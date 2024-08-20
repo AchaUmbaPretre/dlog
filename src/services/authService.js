177d09a2b0565cbc45c2cf10b3d8f60a0c4fb5b0
AchaUmbaPretre <achandambi@gmail.com>
2024-08-20 12:05:39 +0100

    commit

--- src/services/authService.js
+++ src/services/authService.js
@@ -11,3 +11,15 @@ export const loginUser = async (user) => {
 export const registerUser = async (user) => {
   return axios.post(`${DOMAIN}/api/auth/register`, user);
 };
+
+export const logout = async (user) => {
+    return axios.post(`${DOMAIN}/api/auth/logout`, user);
+  };
+
+export const detailForgot  = async (user) => {
+    return axios.post(`${DOMAIN}/api/auth/detail_forgot`, user);
+  };
+
+export const passwordReset  = async (user) => {
+    return axios.post(`${DOMAIN}/api/auth/password_reset`, user);
+  };
