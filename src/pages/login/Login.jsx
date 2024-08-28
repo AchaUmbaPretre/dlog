0d807338a5ed539ece21981c367ef231e558a4c4
AchaUmbaPretre <achandambi@gmail.com>
2024-08-28 16:14:45 +0100

    commit

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,16 +1,65 @@
-import React from 'react'
-import './login.scss'
+import React from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import './login.scss';
 
 const Login = () => {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
   return (
-    <>
-      <div className="login">
-        <div className="login_wrapper">
-          Login
+    <div className="login">
+      <div className="login_wrapper">
+        <div className="login_header">
+          <h2>Se connecter</h2>
+        </div>
+        <Form
+          name="login_form"
+          className="login_form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: 'Veuillez entrer votre email!' }]}
+          >
+            <Input
+              prefix={<UserOutlined className="site_form_item_icon" />}
+              placeholder="Email Address"
+              size="large"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Veuillez entrer votre mot de passe !' }]}
+          >
+            <Input.Password
+              prefix={<LockOutlined className="site_form_item_icon" />}
+              placeholder="Mot de passe"
+              size="large"
+            />
+          </Form.Item>
+          <Form.Item className="login_footer">
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox className="login_remember_me">Souviens-toi de moi</Checkbox>
+            </Form.Item>
+            <a className="login_form_forgot" href="/">
+              Mot de passe oublié ?
+            </a>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login_form_button" size="large" block>
+              Se connecter
+            </Button>
+          </Form.Item>
+        </Form>
+        <div className="login_footer_note">
+          Vous n'avez pas de compte ? <a href="/register">Inscrivez-vous ici</a>
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React from 'react';
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined, MailOutlined } from '@ant-design/icons';
+import './register.scss';
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
 
-export default Register
+  return (
+    <div className="register">
+      <div className="register_wrapper">
+        <div className="register_header">
+          <h2>Créer un compte</h2>
+        </div>
+        <Form
+          name="register_form"
+          className="register_form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: 'Veuillez entrer votre nom !' }]}
+          >
+            <Input
+              prefix={<UserOutlined className="site_form_item_icon" />}
+              placeholder="Nom"
+              size="large"
+            />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: 'Veuillez entrer votre email!' }]}
+          >
+            <Input
+              prefix={<MailOutlined className="site_form_item_icon" />}
+              placeholder="Adresse email"
+              size="large"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Veuillez entrer votre mot de passe !' }]}
+          >
+            <Input.Password
+              prefix={<LockOutlined className="site_form_item_icon" />}
+              placeholder="Password"
+              size="large"
+            />
+          </Form.Item>
+          <Form.Item
+            name="confirm"
+            dependencies={['password']}
+            rules={[
+              { required: true, message: 'Veuillez confirmer votre mot de passe !' },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject('Les deux mots de passe que vous avez saisis ne correspondent pas !');
+                },
+              }),
+            ]}
+          >
+            <Input.Password
+              prefix={<LockOutlined className="site_form_item_icon" />}
+              placeholder="Confirm Password"
+              size="large"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="register_form_button" size="large" block>
+              Registre
+            </Button>
+          </Form.Item>
+        </Form>
+        <div className="register_footer_note">
+          Vous avez déjà un compte ? <a href="/login">Connectez-vous ici</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
