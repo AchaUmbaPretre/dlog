17af13bb87e969d3ffbc532f1db2c15b42f1f904
AchaUmbaPretre <achandambi@gmail.com>
2024-09-12 09:47:32 +0100

    commit

--- src/config.js
+++ src/config.js
@@ -5,8 +5,9 @@ const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
 
 export default {
-  REACT_APP_SERVER_DOMAIN : 'https://apidlog.loginsmart-cd.com' 
+  REACT_APP_SERVER_DOMAIN : 'http://localhost:8080' 
 };
+
 
 export const userRequest = axios.create({
   baseURL: 'http://localhost:8080',
