ae5ef409ef11b18859e87e514168ed735a7f8e1f
AchaUmbaPretre <achandambi@gmail.com>
2024-09-20 15:52:27 +0100

    commit

--- src/pages/batiment/uploadBatimentForm/detailUpload/DetailUpload.jsx
+++ src/pages/batiment/uploadBatimentForm/detailUpload/DetailUpload.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { getPlansOne } from '../../../../services/batimentService';
 import { Spin, notification, Card, Col, Row, Modal, Button, Typography } from 'antd';
 import { EyeOutlined, DownloadOutlined } from '@ant-design/icons';
-import { getPlansOne } from '../../../../services/batimentService';
-import config from '../../../../config';
 import './detailUpload.scss';
+import config from '../../../../config';
 
 const { Meta } = Card;
 const { Title } = Typography;
@@ -41,69 +41,81 @@ const DetailUpload = ({ idBatiment }) => {
     setIsModalVisible(false);
   };
 
-  const downloadImage = (imagePath) => {
-    const link = document.createElement('a');
-    link.href = `${DOMAIN}/${imagePath}`;
-    link.download = imagePath.split('/').pop();
-    link.click();
+  const downloadImage = async (imagePath) => {
+    const url = `${DOMAIN}/${imagePath}`;
+    try {
+      const response = await fetch(url);
+      const blob = await response.blob();
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = imagePath.split('/').pop();
+      link.click();
+      URL.revokeObjectURL(link.href);
+    } catch (error) {
+      notification.error({
+        message: 'Erreur de téléchargement',
+        description: 'Impossible de télécharger l\'image. Veuillez réessayer plus tard.',
+      });
+    }
   };
+
+  if (loading) {
+    return (
+      <div className="loading-container">
+        <Spin tip="Chargement des images..." size="large" />
+      </div>
+    );
+  }
 
   return (
     <div className="detail-upload">
-      <Title level={2} className="gallery-title">
-        Galerie des plans
-      </Title>
-
-      {loading ? (
-        <div className="loading-container">
-          <Spin tip="Chargement des images..." size="large" />
-        </div>
-      ) : (
-        <Row gutter={[16, 16]} className="gallery-row">
-          {data.map((item) => (
-            <Col key={item.id_batiment_plans} xs={24} sm={12} md={8} lg={6}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt={item.nom_document}
-                    src={`${DOMAIN}/${item.chemin_document}`}
-                    className="gallery-image"
-                    onClick={() => showImageModal(item)}
-                  />
-                }
-                actions={[
-                  <Button
-                    type="link"
-                    icon={<EyeOutlined />}
-                    onClick={() => showImageModal(item)}
-                    className="view-button"
-                  />,
-                  <Button
-                    type="link"
-                    icon={<DownloadOutlined />}
-                    onClick={() => downloadImage(item.chemin_document)}
-                    className="download-button"
-                  />,
-                ]}
-                className="gallery-card"
-              >
-                <Meta
-                  title={item.nom_document}
-                  description={`Ajouté le ${new Date(item.date_ajout).toLocaleDateString()}`}
+      <Title level={2} className="gallery-title">Galerie des plans</Title>
+      
+      <Row gutter={[16, 16]} className="gallery-row">
+        {data.map((item) => (
+          <Col key={item.id_batiment_plans} xs={24} sm={12} md={8} lg={6}>
+            <Card
+              hoverable
+              cover={
+                <img
+                  alt={item.nom_document}
+                  src={`${DOMAIN}/${item.chemin_document}`}
+                  className="gallery-image"
+                  onClick={() => showImageModal(item)}
                 />
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      )}
+              }
+              actions={[
+                <Button
+                  type="link"
+                  icon={<EyeOutlined />}
+                  onClick={() => showImageModal(item)}
+                  className="view-button"
+                >
+                  Voir
+                </Button>,
+                <Button
+                  type="link"
+                  icon={<DownloadOutlined />}
+                  onClick={() => downloadImage(item.chemin_document)}
+                  className="download-button"
+                >
+                  Télécharger
+                </Button>,
+              ]}
+              className="gallery-card"
+            >
+              <Meta title={item.nom_document} description={`Ajouté le ${new Date(item.date_ajout).toLocaleDateString()}`} />
+            </Card>
+          </Col>
+        ))}
+      </Row>
 
       <Modal
         visible={isModalVisible}
         footer={null}
         onCancel={handleCancel}
         className="image-modal"
-        width={1000}
+        width="80%"
         centered
       >
         {selectedImage && (
