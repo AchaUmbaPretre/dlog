a33dde744f72e611a2cfbe6c9c1eba7d9204fa95
AchaUmbaPretre <achandambi@gmail.com>
2024-09-20 16:30:06 +0100

    commit

---
+++ src/pages/batiment/listeEquipement/ListeEquipement.jsx
@@ -0,0 +1,230 @@
+import React, { useEffect, useState } from 'react';
+import { Table, Button, Input, message, Dropdown, Menu, notification, Popconfirm, Space, Tooltip, Tag } from 'antd';
+import { ExportOutlined,HomeOutlined,MailOutlined,UserOutlined,PhoneOutlined, PrinterOutlined, PlusOutlined, TeamOutlined, DeleteOutlined } from '@ant-design/icons';
+import './client.scss';
+import { getClient } from '../../services/clientService';
+import config from '../../config';
+
+const { Search } = Input;
+
+const ListeEquipement = () => {
+  const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const scroll = { x: 400 };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await getClient();
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        notification.error({
+          message: 'Erreur de chargement',
+          description: 'Une erreur est survenue lors du chargement des données.',
+        });
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [DOMAIN]);
+
+  const handleAddClient = () => {
+    setIsModalVisible(true);
+  };
+
+
+  const handleExportExcel = () => {
+    message.success('Exporting to Excel...');
+  };
+
+  const handleExportPDF = () => {
+    message.success('Exporting to PDF...');
+  };
+
+  const handlePrint = () => {
+    window.print();
+  };
+
+  const handleEdit = (record) => {
+    message.info(`Editing client: ${record.nom}`);
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      // Uncomment when delete function is available
+      // await deleteClient(id);
+      setData(data.filter((item) => item.id !== id));
+      message.success('Client deleted successfully');
+    } catch (error) {
+      notification.error({
+        message: 'Erreur de suppression',
+        description: 'Une erreur est survenue lors de la suppression du client.',
+      });
+    }
+  };
+
+  const handleViewDetails = (record) => {
+    message.info(`Viewing details of client: ${record.nom}`);
+  };
+
+  const menu = (
+    <Menu>
+      <Menu.Item key="1" onClick={handleExportExcel}>
+        <Tag icon={<ExportOutlined />} color="green">Export to Excel</Tag>
+      </Menu.Item>
+      <Menu.Item key="2" onClick={handleExportPDF}>
+        <Tag icon={<ExportOutlined />} color="blue">Export to PDF</Tag>
+      </Menu.Item>
+    </Menu>
+  );
+
+  const columns = [
+    {
+      title: '#',
+      dataIndex: 'id',
+      key: 'id',
+      render: (text, record, index) => index + 1,
+      width: "3%",
+    },
+    {
+      title: 'Nom',
+      dataIndex: 'nom',
+      key: 'nom',
+      render: (text) => (
+        <Tag icon={<UserOutlined />} color="blue">{text ?? 'Aucun'}</Tag>
+      ),
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+      render: (text) => (
+        <Tag icon={<MailOutlined />} color="blue">{text ?? 'Aucun'}</Tag>
+      ),
+    },
+    {
+      title: 'Téléphone',
+      dataIndex: 'telephone',
+      key: 'telephone',
+      render: (text) => (
+        <Tag icon={<PhoneOutlined />} color="blue">{text ?? 'Aucun'}</Tag>
+      ),
+    },
+    {
+      title: 'Adresse',
+      dataIndex: 'adresse',
+      key: 'adresse',
+      render: (text) => (
+        <> 
+          <Tag icon={<HomeOutlined />} color='cyan'>
+            {text ?? 'Aucune'}
+          </Tag>
+        </>
+      ),
+    },
+    {
+      title: 'Type',
+      dataIndex: 'nom_type',
+      key: 'nom_type',
+      render: (text) => (
+        <Tag color={ text === 'Interne' ? 'green' : "magenta"}>{text}</Tag>
+      ),
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      width: '10%',
+      render: (text, record) => (
+        <Space size="middle">
+{/*           <Tooltip title="View Details">
+            <Button
+              icon={<EyeOutlined />}
+              onClick={() => handleViewDetails(record)}
+              type="link"
+              aria-label="View client details"
+            />
+          </Tooltip> */}
+{/*           <Tooltip title="Edit">
+            <Popover title="Modifier" trigger="hover">
+              <Button
+                icon={<EditOutlined />}
+                style={{ color: 'green' }}
+                onClick={() => handleEdit(record)}
+                type="link"
+                aria-label="Edit client"
+              />
+            </Popover>
+          </Tooltip> */}
+          <Tooltip title="Delete">
+            <Popconfirm
+              title="Êtes-vous sûr de vouloir supprimer ce client?"
+              onConfirm={() => handleDelete(record.id)}
+              okText="Oui"
+              cancelText="Non"
+            >
+              <Button
+                icon={<DeleteOutlined />}
+                style={{ color: 'red' }}
+                aria-label="Delete client"
+              />
+            </Popconfirm>
+          </Tooltip>
+        </Space>
+      ),
+    },
+  ]
+
+  return (
+    <>
+      <div className="client">
+        <div className="client-wrapper">
+          <div className="client-row">
+            <div className="client-row-icon">
+              <TeamOutlined className='client-icon' />
+            </div>
+            <h2 className="client-h2">Client</h2>
+          </div>
+          <div className="client-actions">
+            <div className="client-row-left">
+              <Search placeholder="Search clients..." enterButton />
+            </div>
+            <div className="client-rows-right">
+              <Button
+                type="primary"
+                icon={<PlusOutlined />}
+                onClick={handleAddClient}
+              >
+                Ajouter un Client
+              </Button>
+              <Dropdown overlay={menu} trigger={['click']}>
+                <Button icon={<ExportOutlined />}>Export</Button>
+              </Dropdown>
+              <Button
+                icon={<PrinterOutlined />}
+                onClick={handlePrint}
+              >
+                Print
+              </Button>
+            </div>
+          </div>
+          <Table
+            columns={columns}
+            dataSource={data}
+            loading={loading}
+            pagination={{ pageSize: 10 }}
+            rowKey="id"
+            bordered
+            size="middle"
+            scroll={scroll}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ListeEquipement;
