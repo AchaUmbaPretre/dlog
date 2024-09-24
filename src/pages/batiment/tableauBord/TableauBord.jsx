67a9487dbf57fc438468551ba8a790a8d2ad5e11
AchaUmbaPretre <achandambi@gmail.com>
2024-09-24 13:40:40 +0100

    commit

--- src/pages/batiment/tableauBord/TableauBord.jsx
+++ src/pages/batiment/tableauBord/TableauBord.jsx
@@ -1,20 +1,37 @@
-import { Tabs } from 'antd'
-import React from 'react'
+import { Tabs } from 'antd';
+import { DashboardOutlined, FileTextOutlined } from '@ant-design/icons'; // Importer les icônes appropriées
+import React from 'react';
 
 const TableauBord = () => {
   return (
     <div>
-    <Tabs defaultActiveKey="0">
-        <Tabs.TabPane tab='Tableau de bord' key="0">
-
+      <Tabs defaultActiveKey="0">
+        <Tabs.TabPane
+          tab={
+            <span>
+              <DashboardOutlined /> {/* Icône du tableau de bord */}
+              Tableau de bord
+            </span>
+          }
+          key="0"
+        >
+          {/* Contenu du tableau de bord */}
         </Tabs.TabPane>
-        <Tabs.TabPane tab="les rapports d'entretien" key="1">
-            
-        </Tabs.TabPane>
-    </Tabs>
 
+        <Tabs.TabPane
+          tab={
+            <span>
+              <FileTextOutlined /> {/* Icône des rapports */}
+              Les rapports d'entretien
+            </span>
+          }
+          key="1"
+        >
+          
+        </Tabs.TabPane>
+      </Tabs>
     </div>
-  )
-}
+  );
+};
 
-export default TableauBord
+export default TableauBord;
