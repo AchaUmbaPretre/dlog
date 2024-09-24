2389b3cf2d843861e0962fd3874597ffd943f27b
AchaUmbaPretre <achandambi@gmail.com>
2024-09-24 13:38:32 +0100

    commit

--- src/pages/batiment/tableauBord/TableauBord.jsx
+++ src/pages/batiment/tableauBord/TableauBord.jsx
@@ -1,9 +1,18 @@
+import { Tabs } from 'antd'
 import React from 'react'
 
 const TableauBord = () => {
   return (
     <div>
-        
+    <Tabs defaultActiveKey="0">
+        <Tabs.TabPane tab='Tableau de bord' key="0">
+
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="les rapports d'entretien" key="1">
+            
+        </Tabs.TabPane>
+    </Tabs>
+
     </div>
   )
 }
