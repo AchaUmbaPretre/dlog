ad3903b6453e6543489b98db4f619d7006846dba
AchaUmbaPretre <achandambi@gmail.com>
2024-09-25 13:24:43 +0100

    commit

--- src/pages/taches/detailTacheGlobalOne/DetailTacheGlobalOne.jsx
+++ src/pages/taches/detailTacheGlobalOne/DetailTacheGlobalOne.jsx
@@ -7,21 +7,20 @@ import { getTacheOne } from '../../../services/tacheService';
 const DetailTacheGlobalOne = ({ idTache }) => {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(false);
-  const [nameBatiment, setNameBatiment] = useState('');
 
-
-    const fetchData = async () => {
-      try {
-        const response = await getTacheOne(idTache);
-        setData(response.data[0]);
-      } catch (error) {
-        notification.error({
-          message: 'Erreur de chargement',
-          description: 'Une erreur est survenue lors du chargement des données. Veuillez réessayer plus tard.',
-        });
-      } finally {
-        setLoading(false);
-      }
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await getTacheOne(idTache);
+      setData(response.data[0]);
+    } catch (error) {
+      notification.error({
+        message: 'Erreur de chargement',
+        description: 'Une erreur est survenue lors du chargement des données. Veuillez réessayer plus tard.',
+      });
+    } finally {
+      setLoading(false);
+    }
   }
 
   useEffect(() => {
@@ -31,12 +30,7 @@ const DetailTacheGlobalOne = ({ idTache }) => {
   const renderDataCards = () => (
     <Row gutter={[16, 16]} justify="center" className="data-cards">
       <Col xs={24} sm={12} md={6}>
-        <Card
-          className="data-card"
-          hoverable
-          style={{ textAlign: 'center' }}
-          bodyStyle={{ padding: '20px' }}
-        >
+        <Card className="data-card" hoverable style={{ textAlign: 'center' }} bodyStyle={{ padding: '20px' }}>
           <Badge count={data.nbre_info || 0} showZero>
             <InfoCircleOutlined style={{ fontSize: '40px', color: '#1890ff', marginBottom: '10px' }} />
           </Badge>
@@ -45,12 +39,7 @@ const DetailTacheGlobalOne = ({ idTache }) => {
       </Col>
 
       <Col xs={24} sm={12} md={6}>
-        <Card
-          className="data-card"
-          hoverable
-          style={{ textAlign: 'center' }}
-          bodyStyle={{ padding: '20px' }}
-        >
+        <Card className="data-card" hoverable style={{ textAlign: 'center' }} bodyStyle={{ padding: '20px' }}>
           <Badge count={data.nbre_tracking || 0} showZero>
             <HistoryOutlined style={{ fontSize: '40px', color: '#52c41a', marginBottom: '10px' }} />
           </Badge>
@@ -59,12 +48,7 @@ const DetailTacheGlobalOne = ({ idTache }) => {
       </Col>
 
       <Col xs={24} sm={12} md={6}>
-        <Card
-          className="data-card"
-          hoverable
-          style={{ textAlign: 'center' }}
-          bodyStyle={{ padding: '20px' }}
-        >
+        <Card className="data-card" hoverable style={{ textAlign: 'center' }} bodyStyle={{ padding: '20px' }}>
           <Badge count={data.nbre_documents || 0} showZero>
             <FileTextOutlined style={{ fontSize: '40px', color: '#faad14', marginBottom: '10px' }} />
           </Badge>
@@ -73,12 +57,7 @@ const DetailTacheGlobalOne = ({ idTache }) => {
       </Col>
 
       <Col xs={24} sm={12} md={6}>
-        <Card
-          className="data-card"
-          hoverable
-          style={{ textAlign: 'center' }}
-          bodyStyle={{ padding: '20px' }}
-        >
+        <Card className="data-card" hoverable style={{ textAlign: 'center' }} bodyStyle={{ padding: '20px' }}>
           <Badge count={data.nbre_timing || 0} showZero>
             <ClockCircleOutlined style={{ fontSize: '40px', color: '#f5222d', marginBottom: '10px' }} />
           </Badge>
@@ -90,12 +69,18 @@ const DetailTacheGlobalOne = ({ idTache }) => {
 
   return (
     <div className="dataTableau">
-        <div className="title_row">
-            <h1 className="title_h1"><strong>Titre : </strong> {data.nom_tache}</h1>
-        </div>
-        <div className="title_row">
-            <h1 className="title_h1"><strong>Description : </strong> {data.description}</h1>
-        </div>
+      <div className="title_row">
+        <h1 className="title_h1">
+          <FileTextOutlined style={{ marginRight: '8px' }}/>
+          <strong>Titre : </strong> {data.nom_tache}
+        </h1>
+      </div>
+      <div className="title_row">
+        <h1 className="title_h1">
+          <FileTextOutlined style={{ marginRight: '8px' }}/>
+          <strong>Description : </strong> {data.description}
+        </h1>
+      </div>
       {loading ? (
         <div className="spinner-container">
           <Spin size="large" />
