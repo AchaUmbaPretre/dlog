4830f6c38cb870cdeffe1d41a265f826633f15a5
AchaUmbaPretre <achandambi@gmail.com>
2024-09-30 15:04:19 +0100

    commit

--- src/pages/batiment/Batiment.jsx
+++ src/pages/batiment/Batiment.jsx
@@ -14,6 +14,7 @@ import DocFormBatiment from './document/docFormBatiment/DocFormBatiment';
 import FormEntrepots from './entrepots/formEntrepots/FormEntreports';
 import Entrepots from './entrepots/Entrepots';
 import Bureaux from './bureaux/Bureaux';
+import BureauForm from './bureaux/bureauForm/BureauForm';
 
 const { Search } = Input;
 
@@ -441,6 +442,17 @@ const Batiment = () => {
         centered
       >
         <Bureaux idBatiment={idBatiment} closeModal={()=>setModalType(null)}/>
+      </Modal>
+
+      <Modal
+        title=""
+        visible={modalType === 'FormBureau'}
+        onCancel={closeAllModals}
+        footer={null}
+        width={1050}
+        centered
+      >
+        <BureauForm idBatiment={idBatiment} closeModal={()=>setModalType(null)}/>
       </Modal>
     </>
   );
