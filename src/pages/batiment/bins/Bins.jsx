a102b86afb29b51bf38a0e15d410be66f6dab569
AchaUmbaPretre <achandambi@gmail.com>
2024-09-30 16:21:35 +0100

    commit

---
+++ src/pages/batiment/bins/Bins.jsx
@@ -0,0 +1,285 @@
+import React, { useEffect, useState } from 'react';
+import { Table, Button, Modal, Input, message, Dropdown, Menu, notification, Space, Tooltip, Popconfirm, Tag } from 'antd';
+import { ExportOutlined, PrinterOutlined,MoreOutlined, ContainerOutlined, MailOutlined ,ApartmentOutlined,EditOutlined, PlusCircleOutlined,DeleteOutlined} from '@ant-design/icons';
+import { getEntrepot } from '../../../services/batimentService';
+import BinForm from '../bins/binsForm/BinForm';
+
+const { Search } = Input;
+
+const Bins = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+  const [idEntrepot, setIdEntrepot] = useState('');
+  const scroll = { x: 400 };
+
+  const handleAddBin = (id) => {
+    setIdEntrepot(id)
+    setIsModalVisible(true)
+  }
+
+  const handleListBin = () => {
+
+  }
+
+  const handleDelete = async (id) => {
+    try {
+      setData(data.filter((item) => item.id_departement !== id));
+      message.success('Departement supprimé avec succès');
+    } catch (error) {
+      notification.error({
+        message: 'Erreur de suppression',
+        description: 'Une erreur est survenue lors de la suppression du client.',
+      });
+    }
+  };
+
+    const fetchData = async () => {
+      try {
+         const { data } = await getEntrepot();
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        notification.error({
+          message: 'Erreur de chargement',
+          description: 'Une erreur est survenue lors du chargement des données.',
+        });
+        setLoading(false);
+      }
+    };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleAddClient = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleExportExcel = () => {
+    message.success('Exporting to Excel...');
+  };
+
+  const handleExportPDF = () => {
+    // Logic to export data to PDF
+    message.success('Exporting to PDF...');
+  };
+
+  const handlePrint = () => {
+    window.print();
+  };
+
+  const menu = (
+    <Menu>
+      <Menu.Item key="1" onClick={handleExportExcel}>
+        Export to Excel
+      </Menu.Item>
+      <Menu.Item key="2" onClick={handleExportPDF}>
+        Export to PDF
+      </Menu.Item>
+    </Menu>
+  );
+
+  const columns = [
+    { 
+      title: '#', 
+      dataIndex: 'id', 
+      key: 'id', 
+      render: (text, record, index) => index + 1, 
+      width: "3%" 
+    },
+    { 
+      title: 'Nom', 
+      dataIndex: 'nom', 
+      key: 'nom',
+      render: text => (
+        <Space>
+          <Tag icon={<ApartmentOutlined />} color='cyan'>{text}</Tag>
+        </Space>
+      ),
+    },
+    { 
+      title: 'Superficie', 
+      dataIndex: 'superficie', 
+      key: 'superficie',
+      render: text => (
+        <Tag color='magenta'>{text ?? "Aucun"}</Tag>
+      ),
+    },
+    { 
+        title: 'Longueur', 
+        dataIndex: 'longueur', 
+        key: 'longueur',
+        render: text => (
+          <Tag color='magenta'>{text ?? "Aucun"}</Tag>
+        ),
+      },
+      { 
+        title: 'Largeur', 
+        dataIndex: 'largeur', 
+        key: 'largeur',
+        render: text => (
+          <Tag color='magenta'>{text ?? "Aucun"}</Tag>
+        ),
+      },
+      { 
+        title: 'Hauteur', 
+        dataIndex: 'hauteur', 
+        key: 'hauteur',
+        render: text => (
+          <Tag color='magenta'>{text ?? "Aucun"}</Tag>
+        ),
+      },
+      { 
+        title: 'Capacite', 
+        dataIndex: 'capacite', 
+        key: 'capacite',
+        render: text => (
+          <Tag color='magenta'>{text ?? "Aucun"}</Tag>
+        ),
+      },
+      { 
+        title: 'statut', 
+        dataIndex: 'statut', 
+        key: 'statut',
+        render: text => (
+          <Tag color='magenta'>{text ?? "Aucun"}</Tag>
+        ),
+      },
+      { 
+        title: 'Type_stockage ', 
+        dataIndex: 'type_stockage ', 
+        key: 'type_stockage ',
+        render: text => (
+          <Tag color='magenta'>{text ?? "Aucun"}</Tag>
+        ),
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      width: '10%',
+      render: (text, record) => (
+        <Space size="middle">
+            <Tooltip title="Modifier">
+            <Button
+              icon={<EditOutlined />}
+              style={{ color: 'green' }}
+              aria-label="Edit entrepot"
+            />
+          </Tooltip>
+          <Dropdown
+        overlay={(
+          <Menu>
+            {/* Actions Équipement */}
+            <Menu.Item onClick={() => handleListBin(record.id)}>
+              <ContainerOutlined /> Liste des bins
+            </Menu.Item>
+            <Menu.Item onClick={() => handleAddBin(record.id)}>
+              <ContainerOutlined /> Nouveau Bin
+            </Menu.Item>
+            <Menu.Divider />
+          </Menu>
+        )}
+        trigger={['click']}
+      >
+        <Button
+          icon={<MoreOutlined />}
+          style={{ color: 'black', padding: '0' }}
+          aria-label="Menu actions"
+        />
+          </Dropdown>
+          <Tooltip title="Supprimer">
+            <Popconfirm
+              title="Etes-vous sûr de vouloir supprimer ?"
+              onConfirm={() => handleDelete(record.id)}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button
+                icon={<DeleteOutlined />}
+                style={{ color: 'red' }}
+                aria-label="Delete department"
+              />
+            </Popconfirm>
+          </Tooltip>
+        </Space>
+      ),
+    },
+  ];
+
+  const filteredData = data.filter(item =>
+    item.nom_departement?.toLowerCase().includes(searchValue.toLowerCase()) ||
+    item.code?.toLowerCase().includes(searchValue.toLowerCase()) || 
+    item.nom?.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  return (
+    <>
+      <div className="client">
+        <div className="client-wrapper">
+          <div className="client-row">
+            <div className="client-row-icon">
+              <ContainerOutlined className='client-icon'/>
+            </div>
+            <h2 className="client-h2">Entrepot</h2>
+          </div>
+          <div className="client-actions">
+            <div className="client-row-left">
+              <Search placeholder="Recherche..." 
+                enterButton 
+                onChange={(e) => setSearchValue(e.target.value)}
+              />
+            </div>
+            <div className="client-rows-right">
+              <Button
+                type="primary"
+                icon={<PlusCircleOutlined />}
+                onClick={handleAddClient}
+              >
+                Entrepot
+              </Button>
+              <Dropdown overlay={menu} trigger={['click']} className='client-export'>
+                <Button icon={<ExportOutlined />}>Export</Button>
+              </Dropdown>
+              <Button
+                icon={<PrinterOutlined />}
+                onClick={handlePrint}
+                className='client-export'
+              >
+                Print
+              </Button>
+            </div>
+          </div>
+          <Table
+            columns={columns}
+            dataSource={filteredData}
+            pagination={{ pageSize: 10 }}
+            rowKey="key"
+            bordered
+            size="middle"
+            scroll={scroll}
+            loading={loading}
+          />
+        </div>
+      </div>
+
+      <Modal
+        title=""
+        visible={isModalVisible}
+        onCancel={handleCancel}
+        footer={null}
+        width={650}
+        centered
+      >
+        <BinForm id_entrepot={idEntrepot} closeModal={() => setIsModalVisible(false)} fetchData={fetchData}/>
+     </Modal>
+    </>
+  );
+};
+
+export default Bins;
