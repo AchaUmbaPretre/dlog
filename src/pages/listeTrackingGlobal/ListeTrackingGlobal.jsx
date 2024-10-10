fb20be895ee599bda342fdefb837acb503e8592f
AchaUmbaPretre <achandambi@gmail.com>
2024-10-10 14:55:41 +0100

    commit

--- src/pages/listeTrackingGlobal/ListeTrackingGlobal.jsx
+++ src/pages/listeTrackingGlobal/ListeTrackingGlobal.jsx
@@ -126,7 +126,7 @@ const ListeTrackingGlobal = ({ idTache }) => {
           <Tooltip title="Delete">
             <Popconfirm
               title="Êtes-vous sûr de vouloir supprimer ce client?"
-              onConfirm={() => handleDelete(record.id)}
+              onConfirm={() => handleDelete(record.id_suivi)}
               okText="Oui"
               cancelText="Non"
             >
