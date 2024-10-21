7b2fdee43f955beff7263078e58e731a84aab3f2
AchaUmbaPretre <achandambi@gmail.com>
2024-10-21 09:38:22 +0100

    commit

--- src/pages/passwordForgot/PasswordForgot.jsx
+++ src/pages/passwordForgot/PasswordForgot.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Form, Input, Button, Typography } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
-import './passwordForgot.scss'
-import forgot from './../../assets/rb_1123.png'
+import './passwordForgot.scss';
+import forgot from './../../assets/rb_1123.png';
 
-const { Title } = Typography;
+const { Title, Paragraph } = Typography;
 
 const PasswordForgot = () => {
   const onFinish = (values) => {
@@ -13,39 +13,45 @@ const PasswordForgot = () => {
 
   return (
     <div className="forgot-password-container">
-        <div className="forgot_password">
-            <div className="forgot_left">
-                <img src={forgot} alt="" className='forgot_img' />
-            </div>
-            <div className="forgot_right">
-                    <Title level={5}>Réinitialiser le mot de passe</Title>
-                    <Form
-                        name="forgot_password"
-                        onFinish={onFinish}
-                        layout="vertical"
-                        style={{ maxWidth: '400px', margin: 'auto' }}
-                    >
-                        <Form.Item
-                        name="email"
-                        rules={[
-                            { required: true, message: 'Veuillez entrer votre email!' },
-                            { type: 'email', message: 'Adresse email invalide!' },
-                        ]}
-                        >
-                        <Input
-                            prefix={<UserOutlined />}
-                            placeholder="Entrez votre email"
-                            size="large"
-                        />
-                        </Form.Item>
-                        <Form.Item>
-                        <Button type="primary" htmlType="submit" block size="large">
-                            Réinitialiser
-                        </Button>
-                        </Form.Item>
-                    </Form>
-            </div>
+      <div className="forgot_password">
+        <div className="forgot_left">
+          <img src={forgot} alt="Password recovery" className='forgot_img' />
         </div>
+        <div className="forgot_right">
+          <div className="forgot_content">
+            <Title level={2} className="forgot_title">Mot de passe oublié?</Title>
+            <Paragraph className="forgot_description">
+              Entrez votre adresse email et nous vous enverrons un lien pour réinitialiser votre mot de passe.
+            </Paragraph>
+            <Form
+              name="forgot_password"
+              onFinish={onFinish}
+              layout="vertical"
+              className="forgot_form"
+            >
+              <Form.Item
+                name="email"
+                rules={[
+                  { required: true, message: 'Veuillez entrer votre email!' },
+                  { type: 'email', message: 'Adresse email invalide!' },
+                ]}
+              >
+                <Input
+                  prefix={<UserOutlined />}
+                  placeholder="Entrez votre email"
+                  size="large"
+                  className="forgot_input"
+                />
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit" block size="large" className="forgot_button">
+                  Envoyer le lien
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
