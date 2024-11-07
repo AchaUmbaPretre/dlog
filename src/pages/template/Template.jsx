7d4a542f5deae30ea8cac45f6dd4d7dbfb154530
AchaUmbaPretre <achandambi@gmail.com>
2024-11-07 11:44:53 +0100

    commit

--- src/pages/template/Template.jsx
+++ src/pages/template/Template.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Table, Button, Modal, Input, message, Dropdown, Menu, notification, Popconfirm, Space, Tooltip, Tag } from 'antd';
-import { ExportOutlined,CheckCircleOutlined, CloseCircleOutlined,CalendarOutlined,ScheduleOutlined,PlusCircleOutlined, UserOutlined, PrinterOutlined, DeleteOutlined } from '@ant-design/icons';
+import { ExportOutlined,FileTextOutlined,TagOutlined,ShopOutlined,OrderedListOutlined,ApartmentOutlined,HomeOutlined,CheckCircleOutlined, CloseCircleOutlined,CalendarOutlined,ScheduleOutlined,PlusCircleOutlined, UserOutlined, PrinterOutlined, DeleteOutlined } from '@ant-design/icons';
 import TemplateForm from './templateForm/TemplateForm';
 import { getTemplate } from '../../services/templateService';
 import moment from 'moment';
@@ -107,7 +107,7 @@ const Template = () => {
       dataIndex: 'nom_type_d_occupation',
       key: 'nom_type_d_occupation',
       render: (text) => (
-        <Tag color="blue">{text ?? 'Aucun'}</Tag>
+        <Tag icon={<ApartmentOutlined />} color="blue">{text ?? 'Aucun'}</Tag>
       ),
     },
     {
@@ -115,7 +115,7 @@ const Template = () => {
       dataIndex: 'nom_batiment',
       key: 'nom_batiment',
       render: (text) => (
-        <Tag color="blue">{text ?? 'Aucun'}</Tag>
+        <Tag icon={<HomeOutlined />} color="blue">{text ?? 'Aucun'}</Tag>
       ),
     },
     {
@@ -123,11 +123,7 @@ const Template = () => {
       dataIndex: 'nom_niveau',
       key: 'nom_niveau',
       render: (text) => (
-        <> 
-          <Tag color='cyan'>
-            {text ?? 'Aucune'}
-          </Tag>
-        </>
+        <Tag icon={<OrderedListOutlined />} color="cyan">{text ?? 'Aucune'}</Tag>
       ),
     },
     {
@@ -135,68 +131,58 @@ const Template = () => {
       dataIndex: 'nom_denomination_bat',
       key: 'nom_denomination_bat',
       render: (text) => (
-        <Tag>{text}</Tag>
+        <Tag icon={<TagOutlined />} color="purple">{text ?? 'Aucune'}</Tag>
       ),
     },
     {
-        title: 'Whse fact',
-        dataIndex: 'nom_whse_fact',
-        key: 'nom_whse_fact',
-        render: (text) => (
-          <Tag>{text}</Tag>
-        ),
-      },
-      {
-        title: 'Objet fact',
-        dataIndex: 'nom_objet_fact',
-        key: 'nom_objet_fact',
-        render: (text) => (
-          <Tag>{text}</Tag>
-        ),
-      },
-      { 
-        title: 'Date active', 
-        dataIndex: 'date_actif', 
-        key: 'date_actif',
-        sorter: (a, b) => moment(a.date_actif).unix() - moment(b.date_actif).unix(), // tri par date
-        render: text => (
-          <Tag icon={<CalendarOutlined />} color='purple'>{moment(text).format('LL')}</Tag>
-        ),
-      },
-      {
-        title: 'Statut',
-        dataIndex: 'id_statut_template',
-        key: 'id_statut_template',
-        render: (text) => (
-          <>
-            {text === 1 ? (
-              <Tag color="cyan" icon={<CheckCircleOutlined style={{ color: 'green' }} />}>
-                Activé
-              </Tag>
-            ) : (
-              <Tag color="gray" icon={<CloseCircleOutlined style={{ color: 'red' }} />}>
-                Désactivé
-              </Tag>
-            )}
-          </>
-        ),
-      },
+      title: 'Whse fact',
+      dataIndex: 'nom_whse_fact',
+      key: 'nom_whse_fact',
+      render: (text) => (
+        <Tag icon={<ShopOutlined />} color="geekblue">{text ?? 'Aucune'}</Tag>
+      ),
+    },
+    {
+      title: 'Objet fact',
+      dataIndex: 'nom_objet_fact',
+      key: 'nom_objet_fact',
+      render: (text) => (
+        <Tag icon={<FileTextOutlined />} color="green">{text ?? 'Aucun'}</Tag>
+      ),
+    },
+    { 
+      title: 'Date active', 
+      dataIndex: 'date_actif', 
+      key: 'date_actif',
+      sorter: (a, b) => moment(a.date_actif).unix() - moment(b.date_actif).unix(),
+      render: (text) => (
+        <Tag icon={<CalendarOutlined />} color='purple'>
+          {text ? moment(text).format('DD-MM-yyyy') : 'Aucune'}
+        </Tag>
+      ),
+    },
+    {
+      title: 'Statut',
+      dataIndex: 'id_statut_template',
+      key: 'id_statut_template',
+      render: (text) => (
+        text === 1 ? (
+          <Tag color="cyan" icon={<CheckCircleOutlined style={{ color: 'green' }} />}>
+            Activé
+          </Tag>
+        ) : (
+          <Tag color="gray" icon={<CloseCircleOutlined style={{ color: 'red' }} />}>
+            Désactivé
+          </Tag>
+        )
+      ),
+    },
     {
       title: 'Action',
       key: 'action',
       width: '10%',
       render: (text, record) => (
         <Space size="middle">
-{/*            <Tooltip title="Edit">
-            <Popover title="Modifier" trigger="hover">
-              <Button
-                icon={<EditOutlined />}
-                style={{ color: 'green' }}
-                onClick={() => handlEditClient(record.id_client)}
-                aria-label="Edit client"
-              />
-            </Popover>
-          </Tooltip> */}
           <Tooltip title="Delete">
             <Popconfirm
               title="Êtes-vous sûr de vouloir supprimer ce client?"
@@ -214,7 +200,7 @@ const Template = () => {
         </Space>
       ),
     },
-  ]
+  ];
 
   const filteredData = data.filter(item =>
     item.nom_client?.toLowerCase().includes(searchValue.toLowerCase()) || 
