849af70abdb3814d6150964ccacd2e2f02584902
AchaUmbaPretre <achandambi@gmail.com>
2024-11-07 13:26:56 +0100

    commit

--- src/pages/declaration/declarationFiltre/DeclarationFiltre.jsx
+++ src/pages/declaration/declarationFiltre/DeclarationFiltre.jsx
@@ -2,9 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Select, DatePicker, Button } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import 'antd/dist/reset.css';
-import { getUser } from '../../../services/userService';
 import { getClient, getProvince } from '../../../services/clientService';
-import { getTypes } from '../../../services/typeService';
+import { getBatiment, getTypes } from '../../../services/typeService';
 import { getPriorityIcon } from '../../../utils/prioriteIcons';
 
 const { Option } = Select;
@@ -14,8 +13,7 @@ const DeclarationFiltre = ({ onFilter }) => {
     const [province, setProvince] = useState([]);
     const [client, setClient] = useState([]);
     const [dateRange, setDateRange] = useState([]);
-    const [owners, setOwners] = useState([]);
-    const [type, setType] = useState([]);
+    const [batiment, setBatiment] = useState([]);
     const [selectedDepartement, setSelectedDepartement] = useState('');
     const [selectedClients, setSelectedClients] = useState('');
     const [selectedStatut, setSelectedStatut] = useState('');
@@ -36,16 +34,14 @@ const DeclarationFiltre = ({ onFilter }) => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const [usersData, clientData, typeData, provinceData] = await Promise.all([
-                    getUser(),
+                const [batimentData, clientData, provinceData] = await Promise.all([
+                    getBatiment(),
                     getClient(),
-                    getTypes(),
                     getProvince()
                 ]);
 
-                setOwners(usersData.data);
+                setBatiment(batimentData.data);
                 setClient(clientData.data);
-                setType(typeData.data);
                 setProvince(provinceData.data)
             } catch (error) {
                 console.error(error);
@@ -73,7 +69,7 @@ const DeclarationFiltre = ({ onFilter }) => {
                 />
             </div>
             <div className="filter_row">
-                <label>Clients:</label>
+                <label>Clients :</label>
                 <Select
                     mode="multiple"
                     style={{ width: '100%' }}
@@ -93,31 +89,16 @@ const DeclarationFiltre = ({ onFilter }) => {
                     mode="multiple"
                     style={{ width: '100%' }}
                     showSearch
-                    options={client.map((item) => ({
-                        value: item.id_client,
-                        label: item.nom,
+                    options={batiment.map((item) => ({
+                        value: item.id_batiment,
+                        label: item.nom_batiment,
                     }))}
                     placeholder="Sélectionnez un client..."
                     optionFilterProp="label"
                     onChange={setSelectedClients} // Met à jour les clients sélectionnés
                 />
             </div>
-            <div className="filter_row">
-                <label>Priorité:</label>
-                <Select
-                    mode="multiple"
-                    placeholder="Sélectionnez..."
-                    optionFilterProp="label"
-                    options={[
-                        { value: 1, label: <span>{getPriorityIcon(1)} Très faible</span> },
-                        { value: 2, label: <span>{getPriorityIcon(2)} Faible</span> },
-                        { value: 3, label: <span>{getPriorityIcon(3)} Moyenne</span> },
-                        { value: 4, label: <span>{getPriorityIcon(4)} Haute</span> },
-                        { value: 5, label: <span>{getPriorityIcon(5)} Très haute</span> },
-                    ]}
-                    onChange={setSelectedPriorite}
-                />
-            </div>
+
             <div className="filter_row">
                 <label>Periode :</label>
                 <DatePicker
@@ -125,21 +106,6 @@ const DeclarationFiltre = ({ onFilter }) => {
                     placeholder="Sélectionnez le mois"
                     format="YYYY-MM-DD"
                     style={{ width: '100%' }}
-                />
-            </div>
-            <div className="filter_row">
-                <label>Owner:</label>
-                <Select
-                    showSearch
-                    mode="multiple"
-                    style={{ width: '100%' }}
-                    options={owners.map((item) => ({
-                        value: item.id_utilisateur,
-                        label: `${item.nom}`,
-                    }))}
-                    placeholder="Sélectionnez..."
-                    optionFilterProp="label"
-                    onChange={setSelectedOwners} // Met à jour les propriétaires sélectionnés
                 />
             </div>
             <Button type="primary" icon={<SearchOutlined />} onClick={handleFilter}>
