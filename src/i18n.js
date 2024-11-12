eea8a386ced4024dfee7694f2da67a91d2900216
AchaUmbaPretre <achandambi@gmail.com>
2024-11-12 09:44:14 +0100

    commit

--- src/i18n.js
+++ src/i18n.js
@@ -1,25 +1,20 @@
-// i18n.js
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 i18n
-  .use(initReactI18next) // passe le module i18next à react-i18next
+  .use(initReactI18next)
   .init({
     resources: {
       en: {
         translation: {
-          // Ajoute tes traductions en anglais ici
           "welcome": "Welcome",
           "logout": "Logout",
-          // ... autres traductions
         },
       },
       fr: {
         translation: {
-          // Ajoute tes traductions en français ici
           "welcome": "Bienvenue",
           "logout": "Déconnexion",
-          // ... autres traductions
         },
       },
     },
