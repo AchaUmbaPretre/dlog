4c4ea6b229e8186bbb3de379a1c385b0fa1da152
AchaUmbaPretre <achandambi@gmail.com>
2024-11-22 11:27:04 +0100

    commit

--- src/pages/instructions/inspectionTache/InspectionTache.jsx
+++ src/pages/instructions/inspectionTache/InspectionTache.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Form, Input, Button, notification, Select } from 'antd';
+import React, { useEffect, useState } from 'react'; 
+import { Form, Button, notification, Select } from 'antd';
 import { putInspectionTache } from '../../../services/batimentService';
 import { getTache } from '../../../services/tacheService';
 
@@ -10,30 +10,38 @@ const InspectionTache = ({ closeModal, fetchData, idInspection }) => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
 
-
+  // Fonction pour récupérer les tâches et éliminer les doublons par id_tache
   const fetchDataTache = async () => {
     try {
       const { data } = await getTache();
-      setData(data.taches);
+      
+      // Eliminer les doublons en utilisant un Set basé sur id_tache
+      const uniqueTasks = data.taches.reduce((acc, task) => {
+        if (!acc.some(existingTask => existingTask.id_tache === task.id_tache)) {
+          acc.push(task);
+        }
+        return acc;
+      }, []);
+      
+      setData(uniqueTasks);
       setLoading(false);
     } catch (error) {
       notification.error({
         message: 'Erreur de chargement',
         description: 'Une erreur est survenue lors du chargement des données.',
-      })
-
+      });
       setLoading(false);
     }
   };
 
-useEffect(() => {
-  fetchDataTache();
-}, []);
+  useEffect(() => {
+    fetchDataTache();
+  }, []);
 
   const onFinish = async (values) => {
     setLoading(true);
     try {
-        await putInspectionTache( idInspection, values);
+      await putInspectionTache(idInspection, values);
       notification.success({
         message: 'Succès',
         description: 'Le formulaire a été soumis avec succès.',
@@ -44,7 +52,7 @@ useEffect(() => {
     } catch (error) {
       notification.error({
         message: 'Erreur',
-        description: "Erreur lors de l'enregistrement de la fréquence.",
+        description: "Erreur lors de l'enregistrement de la tâche.",
       });
     } finally {
       setLoading(false);
@@ -60,34 +68,42 @@ useEffect(() => {
   };
 
   return (
-    <Form
-      form={form}
-      name="inspectionTache_form"
-      layout="vertical"
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      style={{ maxWidth: 600, margin: '0 auto' }}
-    >
-      <Form.Item
-        label="Tache"
-        name="id_tache"
-        rules={[{ required: true, message: 'Veuillez selectionner une tache' }]}
-      >
-            <Select placeholder="Sélectionnez une tache">
-                {data?.map((dd) => (
-                    <Option key={dd.id_tache} value={dd.id_tache}>
-                        {dd.nom_tache}
-                    </Option>
-                ))}
+    <div className="controle_from">
+        <div className="controle_title_rows">
+            <h2 className="controle_h2">Relier l'inspection à une tache</h2>
+        </div>
+        <div className="controle_wrapper">
+            <Form
+        form={form}
+        name="inspectionTache_form"
+        layout="vertical"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        style={{ maxWidth: 600, margin: '0 auto' }}
+        >
+        <Form.Item
+            label="Tâche"
+            name="id_tache"
+            rules={[{ required: true, message: 'Veuillez sélectionner une tâche' }]}
+        >
+            <Select placeholder="Sélectionnez une tâche">
+            {/* Affichage des tâches sans doublons */}
+            {data?.map((dd) => (
+                <Option key={dd.id_tache} value={dd.id_tache}>
+                {dd.nom_tache}
+                </Option>
+            ))}
             </Select>
-      </Form.Item>
+        </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit" loading={loading} disabled={loading}>
-          Soumettre
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+            <Button type="primary" htmlType="submit" loading={loading} disabled={loading}>
+            Soumettre
+            </Button>
+        </Form.Item>
+            </Form>
+        </div>
+    </div>
   );
 };
 
