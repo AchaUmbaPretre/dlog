d23d50d102af9db54d32cd0897be1f0805bb2fa7
AchaUmbaPretre <achandambi@gmail.com>
2025-01-09 15:48:38 +0100

    commit

--- src/pages/declaration/rapportDeclaration/RapportDeclaration.jsx
+++ src/pages/declaration/rapportDeclaration/RapportDeclaration.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Tabs } from 'antd';
 import './rapportDeclaration.scss'
 
+const { TabPane } = Tabs;
+
 const RapportDeclaration = () => {
+    const [activeKey, setActiveKey] = useState(['1', '2']);
+
+    const handleTabChange = (key) => {
+        setActiveKey(key);
+      };
+
   return (
-    <div>RapportDeclaration</div>
+    <>
+        <div className="rapport_declaration">
+            <Tabs
+                activeKey={activeKey[0]}
+                onChange={handleTabChange}
+                type="card"
+                tabPosition="top"
+                renderTabBar={(props, DefaultTabBar) => (
+                    <DefaultTabBar {...props} />
+                )}
+            >
+
+                <TabPane tab="M² Facturé" key="1">
+                    <div>
+                        11111
+                    </div>
+                </TabPane>
+
+                <TabPane tab="Total Entrepo et Manut" key="2">
+                </TabPane>
+
+                <TabPane tab="Total Manutention" key="3">
+                </TabPane>
+
+                <TabPane tab="Total Entreposage" key="4">
+                </TabPane>
+
+                <TabPane tab="Vue d'ensemble" key="5">
+                </TabPane>
+
+            </Tabs>
+        </div>
+    </>
   )
 }
 
