92e21f59a81251eef7717264c8c82d8869814aa2
AchaUmbaPretre <achandambi@gmail.com>
2025-01-10 08:55:45 +0100

    commit

--- src/pages/declaration/rapportDeclaration/rapportFacture/RapportFacture.jsx
+++ src/pages/declaration/rapportDeclaration/rapportFacture/RapportFacture.jsx
@@ -1,73 +1,115 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './rapportFacture.scss'
-import { Table } from 'antd';
+import { notification, Space, Table, Tag } from 'antd';
+import { getRapportFacture } from '../../../../services/templateService';
+import moment from 'moment';
 
 const RapportFacture = () => {
-    const [columnss, setColumnss] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [columns, setColumns] = useState([]);
     const [dataSource, setDataSource] = useState([]);
+    const [data, setData] = useState([]);
+    const scroll = { x: 400 };
 
-    const data = [
-        {
-          key: "1",
-          client: "BRACONGO",
-          "janv-24": 2000,
-          "févr-24": 2000,
-          "mars-24": 2000,
-          "avr-24": 2000,
-        },
-        {
-          key: "2",
-          client: "CLIENT. DIVERS",
-          "janv-24": 1393.5,
-          "févr-24": 1371.5,
-          "mars-24": 1351.5,
-          "avr-24": 1370.3,
-        },
-        // Ajoutez d'autres lignes...
-      ];
 
-      const columns = [
-        {
-          title: "Client",
-          dataIndex: "client",
-          key: "client",
-          fixed: "left",
-        },
-        {
-          title: "Janv-24",
-          dataIndex: "janv-24",
-          key: "janv-24",
-        },
-        {
-          title: "Févr-24",
-          dataIndex: "févr-24",
-          key: "févr-24",
-        },
-        {
-          title: "Mars-24",
-          dataIndex: "mars-24",
-          key: "mars-24",
-        },
-        {
-          title: "Avr-24",
-          dataIndex: "avr-24",
-          key: "avr-24",
-        },
-        // Ajoutez d'autres colonnes...
-      ];
-    
+
+    const fetchData = async () => {
+        try {
+          const { data } = await getRapportFacture();
+         
+          const uniqueMonths = Array.from(
+            new Set(
+              data.map((item) => `${item.Mois}-${item.Année}`)
+            )
+          ).sort((a, b) => {
+            // Trier par année et mois
+            const [monthA, yearA] = a.split("-");
+            const [monthB, yearB] = b.split("-");
+            return yearA - yearB || monthA - monthB;
+          });
+  
+          // Générer dynamiquement les colonnes avec le nom abrégé du mois
+          const generatedColumns = [
+            {
+                title: '#',
+                dataIndex: 'id',
+                key: 'id',
+                render: (text, record, index) => index + 1,
+                width: "3%",
+            },
+            {
+              title: "Client",
+              dataIndex: "Client",
+              key: "Client",
+              fixed: "left",
+            },
+            ...uniqueMonths.map((month) => {
+              const [numMonth, year] = month.split("-");
+              const monthName = moment(`${year}-${numMonth}-01`).format("MMM-YYYY");
+  
+              return {
+                title: monthName,
+                dataIndex: monthName,
+                key: monthName,
+                render: text => (
+                    <Space>
+                      <Tag color={text === null ? 'red' : 'blue'}>
+                        {text === null ? "Aucun" : `${text} $`}
+                      </Tag>
+                    </Space>
+                  ),
+              };
+            }),
+          ];
+  
+          // Transformer les données pour correspondre aux colonnes
+          const groupedData = data.reduce((acc, curr) => {
+            const client = acc.find((item) => item.Client === curr.Client);
+            const [numMonth, year] = [curr.Mois, curr.Année];
+            const monthName = moment(`${year}-${numMonth}-01`).format("MMM-YYYY");
+  
+            if (client) {
+              client[monthName] = curr.Montant;
+            } else {
+              acc.push({
+                Client: curr.Client,
+                [monthName]: curr.Montant,
+              });
+            }
+            return acc;
+          }, []);
+  
+          setColumns(generatedColumns);
+          setDataSource(groupedData);
+          setLoading(false);
+        } catch (error) {
+          notification.error({
+            message: 'Erreur de chargement',
+            description: 'Une erreur est survenue lors du chargement des données.',
+          });
+          setLoading(false);
+        }
+      };
+  
+    useEffect(() => {
+      fetchData();
+    }, []);
+
+
   return (
     <>
         <div className="rapport_facture">
             <h2 className="rapport_h2">CLIENT DIVERS M² FACTURE</h2>
             <div className="rapport_wrapper_facture">
-            <Table
-      dataSource={data}
-      columns={columns}
-      bordered
-      pagination={false} // Désactive la pagination pour afficher tout le tableau
-      scroll={{ x: "max-content" }} // Active le défilement horizontal si nécessaire
-    />
+                <Table
+                    dataSource={dataSource}
+                    columns={columns}
+                    bordered
+                    pagination={false}
+                    scroll={scroll}
+                    loading={loading}
+                    size="small"
+                />
             </div>
         </div>
     </>
