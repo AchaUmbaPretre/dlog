561050a5ec0f121f7e1b79fc1b04f81cdc6ce83b
AchaUmbaPretre <achandambi@gmail.com>
2025-01-15 13:30:24 +0100

    commit

--- src/pages/declaration/rapportDeclaration/rapportFacture/rapportFactureChart/RapportFactureChart.jsx
+++ src/pages/declaration/rapportDeclaration/rapportFacture/rapportFactureChart/RapportFactureChart.jsx
@@ -6,7 +6,6 @@ const RapportFactureChart = ({ groupedData, uniqueMonths }) => {
   console.log(groupedData, uniqueMonths);
 
   const prepareChartData = (groupedData, uniqueMonths) => {
-    // Formater les mois dans un format lisible
     const formattedMonths = uniqueMonths.map((month) => {
       const [numMonth, year] = month.split("-");
       return moment(`${year}-${numMonth}-01`).format("MMM-YYYY");
@@ -18,9 +17,8 @@ const RapportFactureChart = ({ groupedData, uniqueMonths }) => {
         Total: client.Total,
       };
 
-      // Ajouter les données pour chaque mois
       formattedMonths.forEach((month) => {
-        clientData[month] = client[month] || 0; // S'assurer que la donnée est définie, sinon 0
+        clientData[month] = client[month] || 0;
       });
 
       return clientData;
@@ -38,35 +36,68 @@ const RapportFactureChart = ({ groupedData, uniqueMonths }) => {
   }, [groupedData, uniqueMonths]);
 
   return (
-    <div style={{ height: 400 }}>
-      <ResponsiveBar
-        data={chartData}
-        keys={uniqueMonths.map((month) => {
-          const [numMonth, year] = month.split("-");
-          return moment(`${year}-${numMonth}-01`).format("MMM-YYYY");
-        })}
-        indexBy="client"
-        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-        padding={0.3}
-        colors={{ scheme: 'nivo' }}
-        enableLabel={true}
-        axisTop={{ tickSize: 5 }}
-        axisRight={{ tickSize: 5 }}
-        axisBottom={{
-          tickSize: 5,
-          tickPadding: 5,
-        }}
-        axisLeft={{
-          tickSize: 5,
-          tickPadding: 5,
-          legend: 'Montant',
-          legendPosition: 'middle',
-          legendOffset: -40
-        }}
-        tooltip={(data) => (
-          <strong>{`${data.id}: ${data.value} $`}</strong>
-        )}
-      />
+    <div style={{ width: '100%', textAlign: 'center' }}>
+      {/* Titre du graphique */}
+      <h2 style={{ fontSize: '1rem', fontWeight: '300', marginBottom: '15px', borderBottom:'2px solid #e8e8e8', paddingBottom:'10px' }}>
+        Chart DIVERS M² FACTURE
+      </h2>
+
+      <div style={{ height: 400 }}>
+        <ResponsiveBar
+          data={chartData}
+          keys={uniqueMonths.map((month) => {
+            const [numMonth, year] = month.split("-");
+            return moment(`${year}-${numMonth}-01`).format("MMM-YYYY");
+          })}
+          indexBy="client"
+          margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+          padding={0.3}
+          colors={{ scheme: 'nivo' }}
+          enableLabel={true}
+          axisTop={{ tickSize: 5 }}
+          axisRight={{ tickSize: 5 }}
+          axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: -45, // Rotation des ticks pour une meilleure lisibilité
+          }}
+          axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            legend: 'Montant',
+            legendPosition: 'middle',
+            legendOffset: -40
+          }}
+          tooltip={(data) => (
+            <strong>{`${data.id}: ${data.value} $`}</strong>
+          )}
+          theme={{
+            axis: {
+              domain: {
+                line: {
+                  stroke: '#777777',
+                  strokeWidth: 1,
+                },
+              },
+              ticks: {
+                line: {
+                  stroke: '#777777',
+                  strokeWidth: 1,
+                },
+                text: {
+                  fill: '#777777',
+                },
+              },
+            },
+            grid: {
+              line: {
+                stroke: '#dddddd',
+                strokeWidth: 1,
+              },
+            },
+          }}
+        />
+      </div>
     </div>
   );
 };
