4bd93a19c3fa45f84983282e91e236a4d3394aed
AchaUmbaPretre <achandambi@gmail.com>
2025-01-22 12:15:47 +0100

    commit

--- src/pages/declaration/rapportDeclaration/rapportFiltrage/RapportFiltrage.jsx
+++ src/pages/declaration/rapportDeclaration/rapportFiltrage/RapportFiltrage.jsx
@@ -14,16 +14,18 @@ const RapportFiltrage = ({ onFilter, filtraVille }) => {
     const [selectedClients, setSelectedClients] = useState([]);
     const [selectedMonths, setSelectedMonths] = useState([]);
     const [selectedYear, setSelectedYear] = useState(null);
+    const [minMontant, setMinMontant] = useState(null);
+    const [maxMontant, setMaxMontant] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
-    const years = Array.from({ length: 10 }, (_, i) => moment().year() - i); // Génère les années (10 dernières)
-
+    const years = Array.from({ length: 10 }, (_, i) => moment().year() - i);
     const months = Array.from({ length: 12 }, (_, index) => index + 1);
 
     const handleFilter = () => {
         onFilter({
             ville: selectedVille,
             client: selectedClients,
+            montant: { min: minMontant, max: maxMontant },
             dateRange: {
                 months: selectedMonths,
                 year: selectedYear,
@@ -56,42 +58,48 @@ const RapportFiltrage = ({ onFilter, filtraVille }) => {
         setSelectedMonths(months);
     };
 
-    // Gérer les changements de l'année sélectionnée
     const handleYearChange = (year) => {
         setSelectedYear(year);
     };
 
+    const handleMinMontantChange = (e) => {
+        setMinMontant(e.target.value);
+    };
+
+    const handleMaxMontantChange = (e) => {
+        setMaxMontant(e.target.value);
+    };
+
     return (
         <div className="filterTache" style={{ margin: '10px 0' }}>
-        {
-            filtraVille && (
-            <div className="filter_row">
-                <label>Ville :</label>
-                {
-                    isLoading ? <Skeleton.Input active={true} /> :
-                    <Select
-                    mode="multiple"
-                    showSearch
-                    style={{ width: '100%' }}
-                    options={province.map((item) => ({
-                        value: item.id,
-                        label: item.capital,
-                    }))}
-                    placeholder="Sélectionnez..."
-                    optionFilterProp="label"
-                    onChange={setSelectedVille}
-                />
-                }
-            </div>
-            )
-        }
+            {filtraVille && (
+                <div className="filter_row">
+                    <label>Ville :</label>
+                    {isLoading ? (
+                        <Skeleton.Input active={true} />
+                    ) : (
+                        <Select
+                            mode="multiple"
+                            showSearch
+                            style={{ width: '100%' }}
+                            options={province.map((item) => ({
+                                value: item.id,
+                                label: item.capital,
+                            }))}
+                            placeholder="Sélectionnez..."
+                            optionFilterProp="label"
+                            onChange={setSelectedVille}
+                        />
+                    )}
+                </div>
+            )}
 
-        {
-            !filtraVille && 
-            <div className="filter_row">
+            {!filtraVille && (
+                <div className="filter_row">
                     <label>Clients :</label>
-                    {
-                        isLoading ? <Skeleton.Input active={true} /> :
+                    {isLoading ? (
+                        <Skeleton.Input active={true} />
+                    ) : (
                         <Select
                             mode="multiple"
                             style={{ width: '100%' }}
@@ -104,60 +112,70 @@ const RapportFiltrage = ({ onFilter, filtraVille }) => {
                             optionFilterProp="label"
                             onChange={setSelectedClients}
                         />
-                    }
+                    )}
                 </div>
-        }
-                <div className="filter_row">
-                    <label>Montant :</label>
-                    <div style={{ display: 'flex', gap: '1rem' }}>
-                        <Space.Compact>
-                            <Input defaultValue="0" />
-                        </Space.Compact>
+            )}
 
-                        <Space.Compact>
-                            <Input defaultValue="0" />
-                        </Space.Compact>
-                    </div>
+            <div className="filter_row">
+                <label>Montant :</label>
+                <div style={{ display: 'flex', gap: '1rem' }}>
+                    <Input
+                        placeholder="Min"
+                        type="number"
+                        value={minMontant}
+                        onChange={handleMinMontantChange}
+                    />
+                    <Input
+                        placeholder="Max"
+                        type="number"
+                        value={maxMontant}
+                        onChange={handleMaxMontantChange}
+                    />
                 </div>
-            
-                <div className="filter_row">
-                    <label>Période :</label>
-                    <div style={{ display: 'flex', gap: '1rem' }}>
-                        <Select
-                            mode="multiple"
-                            placeholder="Sélectionnez les mois"
-                            value={selectedMonths}
-                            onChange={handleMonthChange}
-                            style={{ width: '60%' }}
-                            showSearch
-                            optionFilterProp="children"
-                        >
-                            {months.map((month) => (
-                                <Option key={month} value={month}>
-                                    {moment().month(month - 1).format('MMMM')} {/* Affiche le mois en texte */}
-                                </Option>
-                            ))}
-                        </Select>
+            </div>
 
-                        <Select
-                            placeholder="Sélectionnez l'année"
-                            value={selectedYear}
-                            onChange={handleYearChange}
-                            style={{ width: '40%' }}
-                            showSearch
-                            optionFilterProp="children"
-                        >
-                            {years.map((year) => (
-                                <Option key={year} value={year}>
-                                    {year}
-                                </Option>
-                            ))}
-                        </Select>
-                    </div>
+            <div className="filter_row">
+                <label>Période :</label>
+                <div style={{ display: 'flex', gap: '1rem' }}>
+                    <Select
+                        mode="multiple"
+                        placeholder="Sélectionnez les mois"
+                        value={selectedMonths}
+                        onChange={handleMonthChange}
+                        style={{ width: '60%' }}
+                        showSearch
+                        optionFilterProp="children"
+                    >
+                        {months.map((month) => (
+                            <Option key={month} value={month}>
+                                {moment().month(month - 1).format('MMMM')}
+                            </Option>
+                        ))}
+                    </Select>
+
+                    <Select
+                        placeholder="Sélectionnez l'année"
+                        value={selectedYear}
+                        onChange={handleYearChange}
+                        style={{ width: '40%' }}
+                        showSearch
+                        optionFilterProp="children"
+                    >
+                        {years.map((year) => (
+                            <Option key={year} value={year}>
+                                {year}
+                            </Option>
+                        ))}
+                    </Select>
                 </div>
+            </div>
 
-
-            <Button style={{padding:'10px', marginTop:'20px'}} type="primary" icon={<SearchOutlined />} onClick={handleFilter}>
+            <Button
+                style={{ padding: '10px', marginTop: '20px' }}
+                type="primary"
+                icon={<SearchOutlined />}
+                onClick={handleFilter}
+            >
             </Button>
         </div>
     );
