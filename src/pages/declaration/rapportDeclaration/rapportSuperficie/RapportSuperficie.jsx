2f1af68df7a5b612f53e4e552cf5841e85fa122b
AchaUmbaPretre <achandambi@gmail.com>
2025-02-04 16:42:15 +0100

    commit

--- src/pages/declaration/rapportDeclaration/rapportSuperficie/RapportSuperficie.jsx
+++ src/pages/declaration/rapportDeclaration/rapportSuperficie/RapportSuperficie.jsx
@@ -179,80 +179,8 @@ const RapportSuperficie = () => {
 
   return (
     <>
-        {
-            loading ? (
-                <Skeleton active paragraph={{ rows: 1 }} />
-            ) : (
-                <div
-                  style={{
-                      boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
-                      borderRadius: '8px',
-                      backgroundColor: '#fff',
-                      width: 'fit-content',
-                      margin: '20px 0',
-                      padding: '15px',
-                  }}
-                >
-                    <span
-                        style={{
-                        display: 'block',
-                        padding: '10px 15px',
-                        fontWeight: 'bold',
-                        fontSize: '1rem',
-                        borderBottom: '1px solid #f0f0f0',
-                        }}
-                    >
-                      Résumé :
-                    </span>
-                <div
-                    style={{
-                    display: 'grid',
-                    gridTemplateColumns: 'repeat(3, 1fr)',
-                    gap: '15px',
-                    padding: '15px',
-                    }}
-                >
-                    <Popover title="Liste des clients" trigger="hover">
-                      <span
-                        style={{
-                          fontSize: '0.9rem',
-                          fontWeight: '400',
-                          cursor: 'pointer',
-                          color: '#1890ff',
-                          }}
-                      >
-                        Nbre de clients : <strong>{detail?.Nbre_de_clients}</strong>
-                      </span>
-                    </Popover>
-                    <span style={{ fontSize: '0.9rem', fontWeight: '400' }}>
-                    Nbre de villes : <strong>{detail.Nbre_de_villes}</strong>
-                    </span>
-                    <span style={{ fontSize: '0.9rem', fontWeight: '400' }}>
-                    Total Entreposage :{' '}
-                    <strong>{Math.round(parseFloat(detail?.total_entreposage))?.toLocaleString()} $</strong>
-                    </span>
-                    <span style={{ fontSize: '0.9rem', fontWeight: '400' }}>
-                    Total Manutention :{' '}
-                    <strong>{Math.round(parseFloat(detail.total_manutation))?.toLocaleString()} $</strong>
-                    </span>
-                    <span style={{ fontSize: '0.9rem', fontWeight: '400' }}>
-                    Total Entre. & Manu. :{' '}
-                    <strong>{Math.round(parseFloat(detail.total))?.toLocaleString()} $</strong>
-                    </span>
-                    <span style={{ fontSize: '0.9rem', fontWeight: '400' }}>
-                    Total Extérieur :{' '}
-                    <strong>{detail.Total_Extérieur?.toLocaleString()} $</strong>
-                    </span>
-                    <span style={{ fontSize: '0.9rem', fontWeight: '400' }}>
-                    Total Intérieur :{' '}
-                    <strong>{detail.Total_Intérieur?.toLocaleString()} $</strong>
-                    </span>
-                </div>
-              </div>
-            )
-        }
       <div className="rapport_facture">
-        <h2 className="rapport_h2">RAPPORT VILLE</h2>
+        <h2 className="rapport_h2">RAPPORT SUPERFICIE</h2>
         <Button
           type={filterVisible ? 'primary' : 'default'}
           onClick={() => setFilterVisible(!filterVisible)}
@@ -262,7 +190,6 @@ const RapportSuperficie = () => {
         </Button>
         {filterVisible && <RapportFiltrage onFilter={handleFilterChange} filtraVille={true} />}
 
-        {/* Menu pour afficher/masquer les villes */}
         <Dropdown
           overlay={
             <Menu>
