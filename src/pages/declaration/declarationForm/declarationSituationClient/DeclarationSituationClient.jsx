9dfc1b4873d72ecf92cf2c3894798f4435f90b54
AchaUmbaPretre <achandambi@gmail.com>
2025-02-10 14:27:20 +0100

    commit

--- src/pages/declaration/declarationForm/declarationSituationClient/DeclarationSituationClient.jsx
+++ src/pages/declaration/declarationForm/declarationSituationClient/DeclarationSituationClient.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {  RightCircleFilled, DownOutlined,EnvironmentOutlined, FileTextOutlined, DollarOutlined, BarcodeOutlined,ScheduleOutlined } from '@ant-design/icons';
 
-const DeclarationSituationClient = () => {
+const DeclarationSituationClient = ({idClients}) => {
+    const [loading, setLoading] = useState(true);
+    const [idClient, setidClient] = useState('');
+      
+    
   return (
     <>
-        
+        <div className="clients">
+            <div className="client-wrapper">
+                <div className="client-row">
+                    <div className="client-row-icon">
+                        <ScheduleOutlined className='client-icon' />
+                    </div>
+                    <h2 className="client-h2">Déclarations {titre}</h2>
+                </div>
+            </div>
+        </div>
     </>
   )
 }
