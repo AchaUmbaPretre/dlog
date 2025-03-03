e9237d344410cab132b3ac640e17ced581bc2531
AchaUmbaPretre <achandambi@gmail.com>
2025-03-03 09:38:56 +0100

    commit

--- src/pages/declaration/declarationStatut/declarationStatutCloture/DeclarationStatutCloture.jsx
+++ src/pages/declaration/declarationStatut/declarationStatutCloture/DeclarationStatutCloture.jsx
@@ -1,5 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import moment from 'moment';
 import { getAnnee, getMois } from '../../../../services/templateService';
+import { Select, Skeleton, Input, Checkbox, Collapse } from 'antd';
+
+const { Panel } = Collapse;
 
 const DeclarationStatutCloture = () => {
         const [mois, setMois] = useState([]);
@@ -7,13 +11,21 @@ const DeclarationStatutCloture = () => {
         const [selectedMois, setSelectedMois] = useState([]);
         const [selectedAnnees, setSelectedAnnees] = useState([]);
 
+        const fetchData = async() => {
+            const [yearData] = await Promise.all([
+                getAnnee()
+            ])
+            setAnnee(yearData.data);
+        }
+            useEffect(() => {
+                fetchData();
+            }, []);
+
         const fetchMoisParAnnee = async (annee) => {
 
             try {
                 const response = await getMois(annee);
-                const res = await getAnnee();
 
-                setAnnee(res)
                 setMois((prev) => ({
                     ...prev,
                     [annee]: response.data,
@@ -23,10 +35,63 @@ const DeclarationStatutCloture = () => {
             }
         };
 
+        const handleMoisChange = (checkedValues, annee) => {
+            setSelectedMois((prev) => ({
+                ...prev,
+                [annee]: checkedValues,
+            }));
+        };
+    
+        const handleAnneeChange = (checkedValues) => {
+            setSelectedAnnees(checkedValues);
+            checkedValues.forEach((annee) => {
+                if (!mois[annee]) {
+                    fetchMoisParAnnee(annee);
+                }
+            });
+        };
+        
+        const renderMoisParAnnee = () => {
+            return selectedAnnees.map((year) => (
+                <Panel header={year} key={year}>
+                    <Checkbox.Group
+                        options={(mois[year] || []).map((item) => ({
+                            label: moment().month(item.mois - 1).format('MMMM'),
+                            value: `${item.mois}-${year}`,
+                        }))}
+                        value={selectedMois[year] || []}
+                        onChange={(checkedValues) => handleMoisChange(checkedValues, year)}
+                    />
+                </Panel>
+            ));
+        };
+
   return (
     <>
         <div className="declaration_statut">
+            <div className="declaration_statut_home">
+                <div className="declaration_statut_row">
+                    <label>Année :</label>
+                    <Checkbox.Group
+                        options={annee.map((item) => ({
+                                label: item.annee,
+                                value: item.annee,
+                            }))}
+                        value={selectedAnnees}
+                        onChange={handleAnneeChange}
+                    />
+                </div>
+                { selectedAnnees.length > 0 && (
+                    <div className="declaration_statut_row">
+                        <label>Mois :</label>
+                        <Collapse defaultActiveKey={selectedAnnees}>
+                            {renderMoisParAnnee()}
+                        </Collapse>
+                    </div>
+                )
 
+                }
+            </div>
         </div>
     </>
   )
