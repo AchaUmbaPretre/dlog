3141620e308f1ba3551a3f1e881d17343af2c6c2
AchaUmbaPretre <achandambi@gmail.com>
2025-03-03 09:21:17 +0100

    commit

--- src/pages/declaration/declarationStatut/declarationStatutCloture/DeclarationStatutCloture.jsx
+++ src/pages/declaration/declarationStatut/declarationStatutCloture/DeclarationStatutCloture.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const DeclarationStatutCloture = () => {
   return (
-    <div>DeclarationStatutCloture</div>
+    <>
+        <div className="declaration_statut">
+            
+        </div>
+    </>
   )
 }
 
