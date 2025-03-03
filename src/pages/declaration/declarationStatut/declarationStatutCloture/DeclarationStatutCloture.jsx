647a820df38e6b4033a996a96fbeea6268b0050d
AchaUmbaPretre <achandambi@gmail.com>
2025-03-03 09:26:10 +0100

    commit

--- src/pages/declaration/declarationStatut/declarationStatutCloture/DeclarationStatutCloture.jsx
+++ src/pages/declaration/declarationStatut/declarationStatutCloture/DeclarationStatutCloture.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { getAnnee, getMois } from '../../../../services/templateService';
 
 const DeclarationStatutCloture = () => {
+        const [mois, setMois] = useState([]);
+        const [annee, setAnnee] = useState([]);
+        const [selectedMois, setSelectedMois] = useState([]);
+        const [selectedAnnees, setSelectedAnnees] = useState([]);
+
+        const fetchMoisParAnnee = async (annee) => {
+
+            try {
+                const response = await getMois(annee);
+                const res = await getAnnee();
+
+                setAnnee(res)
+                setMois((prev) => ({
+                    ...prev,
+                    [annee]: response.data,
+                }));
+            } catch (error) {
+                console.error("Erreur lors du chargement des mois :", error);
+            }
+        };
+
   return (
     <>
         <div className="declaration_statut">
-            
+
         </div>
     </>
   )
