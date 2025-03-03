b1ea9877bad0a3fc0eeac2aa5ffbe286693cb306
AchaUmbaPretre <achandambi@gmail.com>
2025-03-03 15:05:01 +0100

    commit

--- src/pages/declaration/declarationDetail/DeclarationDetail.jsx
+++ src/pages/declaration/declarationDetail/DeclarationDetail.jsx
@@ -2,10 +2,13 @@ import { Card, Divider, Descriptions, notification } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { getDeclarationOne } from '../../../services/templateService';
 import { HomeOutlined, InfoCircleOutlined } from '@ant-design/icons';
+import { useSelector } from 'react-redux';
 
 const DeclarationDetail = ({ idDeclaration }) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const role = useSelector((state) => state.user?.currentUser.role);
+  const userId = useSelector((state) => state.user?.currentUser?.id_utilisateur);
 
   const fetchDatas = async () => {
     try {
@@ -46,32 +49,39 @@ const DeclarationDetail = ({ idDeclaration }) => {
             </Descriptions>
 
             <Divider />
-
-            <Descriptions title={<><HomeOutlined /> Entreposage</>} column={1} bordered>
+            {
+              role === 'Admin' && (
+              <Descriptions title={<><HomeOutlined /> Entreposage</>} column={1} bordered>
               <Descriptions.Item label="Débours Entreposage">{data.debours_entreposage}</Descriptions.Item>
               <Descriptions.Item label="Description">{data.desc_entreposage}</Descriptions.Item>
               <Descriptions.Item label="Entreposage">{data.entreposage}</Descriptions.Item>
               <Descriptions.Item label="Tarif Entreposage">{data.tarif_entreposage}</Descriptions.Item>
               <Descriptions.Item label="Total Entreposage">{data.total_entreposage} $</Descriptions.Item>
               <Descriptions.Item label="TTC Entreposage">{data.ttc_entreposage} $</Descriptions.Item>
-            </Descriptions>
+              </Descriptions>
+              )
+            }
 
             <Divider />
 
-            <Descriptions title={<><InfoCircleOutlined /> Manutention</>} column={1} bordered>
-              <Descriptions.Item label="Débours Manutention">{data.debours_manutation}</Descriptions.Item>
-              <Descriptions.Item label="Description">{data.desc_manutation}</Descriptions.Item>
-              <Descriptions.Item label="Manutention">{data.manutation}</Descriptions.Item>
-              <Descriptions.Item label="Tarif Manutention">{data.tarif_manutation}</Descriptions.Item>
-              <Descriptions.Item label="Total Manutention">{data.total_manutation} $</Descriptions.Item>
-              <Descriptions.Item label="TTC Manutention">{data.ttc_manutation} $</Descriptions.Item>
-            </Descriptions>
+            {
+              role === 'Admin' && (
+              <Descriptions title={<><InfoCircleOutlined /> Manutention</>} column={1} bordered>
+                <Descriptions.Item label="Débours Manutention">{data.debours_manutation}</Descriptions.Item>
+                <Descriptions.Item label="Description">{data.desc_manutation}</Descriptions.Item>
+                <Descriptions.Item label="Manutention">{data.manutation}</Descriptions.Item>
+                <Descriptions.Item label="Tarif Manutention">{data.tarif_manutation}</Descriptions.Item>
+                <Descriptions.Item label="Total Manutention">{data.total_manutation} $</Descriptions.Item>
+                <Descriptions.Item label="TTC Manutention">{data.ttc_manutation} $</Descriptions.Item>
+              </Descriptions>
+              )
+            }
 
             <Divider />
 
             <Descriptions title="Informations Supplémentaires" column={1} bordered>
-              <Descriptions.Item label="Nom Bâtiment">{data.nom_batiment || 'N/A'}</Descriptions.Item>
-              <Descriptions.Item label="Nom Objet Facturé">{data.nom_objet_fact}</Descriptions.Item>
+              <Descriptions.Item label="M2 facture">{data.m2_facture}</Descriptions.Item>
+              <Descriptions.Item label="M2 occupé">{data.m2_occupe}</Descriptions.Item>
               <Descriptions.Item label="Période">{formatDate(data.periode)}</Descriptions.Item>
             </Descriptions>
           </>
