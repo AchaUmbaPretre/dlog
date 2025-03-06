a166602245cd55fca875593e0b96113cd6aea735
AchaUmbaPretre <achandambi@gmail.com>
2025-03-06 09:24:29 +0100

    commit

--- src/pages/declaration/rapportDeclaration/rapportFacture/rapportFactureClientOne/RapportFactureClientOne.jsx
+++ src/pages/declaration/rapportDeclaration/rapportFacture/rapportFactureClientOne/RapportFactureClientOne.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Button, notification, Space, Table, Tag, Tooltip } from 'antd';
+import { MenuOutlined, EditOutlined, PieChartOutlined, EyeOutlined, DeleteOutlined, CalendarOutlined, DownOutlined, EnvironmentOutlined, HomeOutlined, FileTextOutlined, DollarOutlined, BarcodeOutlined, ScheduleOutlined, PlusCircleOutlined, UserOutlined } from '@ant-design/icons';
 import moment from 'moment';
 import jsPDF from "jspdf";
 import "jspdf-autotable";
@@ -9,11 +10,10 @@ import {
 } from '@ant-design/icons';
 import getColumnSearchProps from '../../../../../utils/columnSearchUtils';
 import RapportFiltrage from '../../rapportFiltrage/RapportFiltrage';
-import { getRapportFactureClientOne } from '../../../../../services/templateService';
+import { getDeclarationOneClient } from '../../../../../services/templateService';
 
 const RapportFactureClientOne = ({id_client}) => {
     const [loading, setLoading] = useState(true);
-    const [columns, setColumns] = useState([]);
     const [dataSource, setDataSource] = useState([]);
     const scroll = { x: 'max-content' };
     const [searchText, setSearchText] = useState('');
@@ -30,7 +30,7 @@ const RapportFactureClientOne = ({id_client}) => {
     const [ clientdetail, setClientDetail] = useState([]);
     const [title, setTitle] = useState([]);
     
-      const fetchData = async () => {
+/*       const fetchData = async () => {
         try {
           const { data } = await getRapportFactureClientOne(id_client, filteredDatas);
 
@@ -155,8 +155,23 @@ const RapportFactureClientOne = ({id_client}) => {
             }
             setLoading(false);
         }
-      };
+      }; */
   
+    const fetchData = async () => {
+        try {
+            const {data} = getDeclarationOneClient(id_client)
+            setDataSource(data)
+            setLoading(false);
+        } catch (error) {
+            notification.error({
+                message: 'Erreur de chargement',
+                description: 'Une erreur est survenue lors du chargement des données.',
+            });
+        } finally{
+            setLoading(false);
+        }
+      }
+
     useEffect(() => {
       fetchData();
     }, [filteredDatas, id_client]);
@@ -169,18 +184,7 @@ const RapportFactureClientOne = ({id_client}) => {
         fetchData()
         setFilterVisible(!filterVisible)
       }
-    
-      const clientListContent = (
-        <div style={{ maxHeight: "200px", overflowY: "auto" }}>
-          <ul style={{ margin: 0, padding: 0, listStyle: "none" }}>
-            {clientdetail.map((client, index) => (
-              <li key={index} style={{ padding: "5px 0", borderBottom: "1px solid #f0f0f0", fontSize:'12px' }}>
-                {index + 1}. {client.nom}
-              </li>
-            ))}
-          </ul>
-        </div>
-      );
+
       
     const exportToExcelHTML = () => {
         let tableHTML = `
@@ -296,6 +300,355 @@ const RapportFactureClientOne = ({id_client}) => {
     const handleTableChange = (pagination, filters, sorter) => {
       setPagination(pagination);
     };
+
+    const columnStyles = {
+        title: {
+          maxWidth: '220px',
+          whiteSpace: 'nowrap',
+          overflowX: 'scroll', 
+          overflowY: 'hidden',
+          textOverflow: 'ellipsis',
+          scrollbarWidth: 'none',
+          '-ms-overflow-style': 'none', 
+        },
+        titleClient: {
+          maxWidth: '150px',
+          whiteSpace: 'nowrap',
+          overflowX: 'scroll', 
+          overflowY: 'hidden',
+          textOverflow: 'ellipsis',
+          scrollbarWidth: 'none',
+          '-ms-overflow-style': 'none', 
+        },
+        hideScroll: {
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          },
+        },
+      };
+
+    const columns = [
+        {
+          title: '#',
+          dataIndex: 'id',
+          key: 'id',
+          render: (text, record, index) => {
+            const pageSize = pagination.pageSize || 10;
+            const pageIndex = pagination.current || 1;
+            return (pageIndex - 1) * pageSize + index + 1;
+          },
+          width: "4%",    
+        },
+        // Groupe Entreposage
+        {
+          title: 'Entreposage',
+          children: [
+            {
+              title: 'Template',
+              dataIndex: 'desc_template',
+              key: 'desc_template',
+              render: (text, record) => (
+                <Space style={columnStyles.title} className={columnStyles.hideScroll}>
+                  <Tag icon={<FileTextOutlined />} color="geekblue">{text ?? 'Aucun'}</Tag>
+                </Space>
+              ),
+            },
+            {
+              title: 'Client',
+              dataIndex: 'nom',
+              key: 'nom',
+              render: (text, record) => (
+                <Space style={columnStyles.titleClient} className={columnStyles.hideScroll}>
+                  <Tag icon={<UserOutlined />} color="orange">
+                    {text ?? 'Aucun'}
+                  </Tag>
+                </Space>
+              ),
+            },
+            {
+              title: 'Periode',
+              dataIndex: 'periode',
+              key: 'periode',
+              sorter: (a, b) => moment(a.periode) - moment(b.periode),
+              sortDirections: ['descend', 'ascend'],
+              render: (text, record) => {
+                const date = text ? new Date(text) : null;
+                const mois = date ? date.getMonth() + 1 : null; // getMonth() renvoie 0-11, donc +1 pour avoir 1-12
+                const annee = date ? date.getFullYear() : null;
+                
+                const formattedDate = date
+                  ? date.toLocaleString('default', { month: 'long', year: 'numeric' })
+                  : 'Aucun';
+            
+                return (
+                  <Tag 
+                    icon={<CalendarOutlined />} 
+                    color="purple" 
+                  >
+                    {formattedDate}
+                  </Tag>
+                );
+              },
+            },  
+            {
+              title: 'Statut',
+              dataIndex: 'id_statut_decl',
+              key: 'id_statut_decl',
+              render: (text, record) => (
+                <StatutDeclaration initialStatus={text} id={record.id_declaration_super} />
+              ),
+            },
+            {
+              title: 'M² occupe',
+              dataIndex: 'm2_occupe',
+              key: 'm2_occupe',
+              sorter: (a, b) => a.m2_occupe - b.m2_occupe,
+              sortDirections: ['descend', 'ascend'],
+              render: (text) => (
+                <Tag icon={<BarcodeOutlined />} color="cyan">{text ?? '0'}</Tag>
+              ),
+              align: 'right', 
+            },
+            {
+              title: 'M² facture',
+              dataIndex: 'm2_facture',
+              key: 'm2_facture',
+              sorter: (a, b) => a.m2_facture - b.m2_facture,
+              sortDirections: ['descend', 'ascend'],
+              render: (text) => (
+                <Tag icon={<BarcodeOutlined />} color="cyan">{text?.toLocaleString() ?? '0'}</Tag>
+              ),
+              align: 'right', 
+            },
+            {
+              title: 'Tarif Entr',
+              dataIndex: 'tarif_entreposage',
+              key: 'tarif_entreposage',
+              sorter: (a, b) => a.tarif_entreposage - b.tarif_entreposage,
+              sortDirections: ['descend', 'ascend'],
+              render: (text) => (
+                <Tag color="green">
+                  {text
+                        ? `${parseFloat(text)
+                            .toLocaleString("en-US", {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                            })
+                            .replace(/,/g, " ")} $`
+                        : "0.00"}
+                </Tag>
+              ),
+              align: 'right', 
+            },
+            {
+              title: 'Debours Entr',
+              dataIndex: 'debours_entreposage',
+              key: 'debours_entreposage',
+              sorter: (a, b) => a.debours_entreposage - b.debours_entreposage,
+              sortDirections: ['descend', 'ascend'],
+              render: (text) => (
+                <Tag icon={<DollarOutlined />} color="green">
+                  {text
+                        ? `${parseFloat(text)
+                            .toLocaleString("en-US", {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                            })
+                            .replace(/,/g, " ")} $`
+                        : "0.00"}
+                </Tag>
+              ),
+              align: 'right', 
+            },
+            {
+              title: 'Total Entr',
+              dataIndex: 'total_entreposage',
+              key: 'total_entreposage',
+              sorter: (a, b) => a.total_entreposage - b.total_entreposage,
+              sortDirections: ['descend', 'ascend'],
+              render: (text) => (
+                <Tag color="gold">
+                  {text
+                        ? `${parseFloat(text)
+                            .toLocaleString("en-US", {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                            })
+                            .replace(/,/g, " ")} $`
+                        : "0.00"}
+                </Tag>
+              ),
+              align: 'right', 
+            },
+            {
+              title: 'TTC Entr',
+              dataIndex: 'ttc_entreposage',
+              key: 'ttc_entreposage',
+              sorter: (a, b) => a.ttc_entreposage - b.ttc_entreposage,
+              sortDirections: ['descend', 'ascend'],
+              render: (text) => (
+                <Tag color="volcano">
+                  {text
+                        ? `${parseFloat(text)
+                            .toLocaleString("en-US", {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                            })
+                            .replace(/,/g, " ")} $`
+                        : "0.00"}
+                </Tag>
+              ),
+              align: 'right', 
+            },
+          ]
+        },
+      
+        // Groupe Manutention
+        {
+          title: 'Manutention',
+          children: [
+            {
+              title: 'Desc Man',
+              dataIndex: 'desc_manutation',
+              key: 'desc_manutation',
+              render: (text) => (
+                <Tag icon={<FileTextOutlined />} color="geekblue">{text ?? 'Aucun'}</Tag>
+              ),
+            },
+            {
+              title: 'Ville',
+              dataIndex: 'capital',
+              key: 'capital',
+              ...getColumnSearchProps(
+                'capital',
+                searchText,
+                setSearchText,
+                setSearchedColumn,
+                searchInput
+              ),
+              render: (text) => (
+                <Tag icon={<EnvironmentOutlined />} color="blue">{text ?? 'Aucun'}</Tag>
+              ),
+            },
+            {
+              title: 'Bâtiment',
+              dataIndex: 'nom_batiment',
+              key: 'nom_batiment',
+              render: (text) => (
+                <Tag icon={<HomeOutlined />} color="purple">{text ?? 'Aucun'}</Tag>
+              ),
+            },
+            {
+              title: 'Objet fact',
+              dataIndex: 'nom_objet_fact',
+              key: 'nom_objet_fact',
+              render: (text) => (
+                <Tag icon={<FileTextOutlined />} color="magenta">{text ?? 'Aucun'}</Tag>
+              ),
+            },
+            {
+              title: 'Manu.',
+              dataIndex: 'manutation',
+              key: 'manutation',
+              sorter: (a, b) => a.manutation - b.manutation,
+              sortDirections: ['descend', 'ascend'],
+              render: (text) => (
+                <Tag color="cyan">
+                  {text
+                        ? `${parseFloat(text)
+                            .toLocaleString("en-US", {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                            })
+                            .replace(/,/g, " ")} $`
+                        : "0.00"}
+                </Tag>
+              ),
+              align: 'right', 
+            },
+            {
+              title: 'Tarif Manu',
+              dataIndex: 'tarif_manutation',
+              key: 'tarif_manutation',
+              sorter: (a, b) => a.tarif_manutation - b.tarif_manutation,
+              sortDirections: ['descend', 'ascend'],
+              render: (text) => (
+                <Tag color="green">
+                  {text
+                        ? `${parseFloat(text)
+                            .toLocaleString("en-US", {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                            })
+                            .replace(/,/g, " ")} $`
+                        : "0.00"}
+                </Tag>
+              ),
+              align: 'right', 
+            },
+            {
+              title: 'Debours Manu',
+              dataIndex: 'debours_manutation',
+              key: 'debours_manutation',
+              sorter: (a, b) => a.debours_manutation - b.debours_manutation,
+              sortDirections: ['descend', 'ascend'],
+              render: (text) => (
+                <Tag icon={<DollarOutlined />} color="green">
+                  {text
+                        ? `${parseFloat(text)
+                            .toLocaleString("en-US", {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                            })
+                            .replace(/,/g, " ")} $`
+                        : "0.00"}
+                </Tag>
+              ),
+              align: 'right', 
+            },
+            {
+              title: 'Total Manu',
+              dataIndex: 'total_manutation',
+              key: 'total_manutation',
+              sorter: (a, b) => a.total_manutation - b.total_manutation,
+              sortDirections: ['descend', 'ascend'],
+              render: (text) => (
+                <Tag color="gold">
+                  {text
+                        ? `${parseFloat(text)
+                            .toLocaleString("en-US", {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                            })
+                            .replace(/,/g, " ")} $`
+                        : "0.00"}
+                </Tag>
+              ),
+              align: 'right',
+            },
+            {
+              title: 'TTC Manu',
+              dataIndex: 'ttc_manutation',
+              key: 'ttc_manutation',
+              sorter: (a, b) => a.ttc_manutation - b.ttc_manutation,
+              sortDirections: ['descend', 'ascend'],
+              render: (text) => (
+                <Tag color="volcano">
+                  {text
+                        ? `${parseFloat(text)
+                            .toLocaleString("en-US", {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                            })
+                            .replace(/,/g, " ")} $`
+                        : "0.00"}
+                </Tag>
+              ),
+              align: 'right', 
+            },
+          ]
+        }
+      ];
     
   return (
     <>
