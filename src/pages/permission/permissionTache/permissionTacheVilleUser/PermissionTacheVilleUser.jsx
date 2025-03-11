8184149fe82187dd5e065c9837b81c7051ee91ca
[acha umba] <[achandambi@gmail.com]>
2025-03-11 10:19:37 +0100

    commit

--- src/pages/permission/permissionTache/permissionTacheVilleUser/PermissionTacheVilleUser.jsx
+++ src/pages/permission/permissionTache/permissionTacheVilleUser/PermissionTacheVilleUser.jsx
@@ -1,124 +1,130 @@
 import React, { useEffect, useState } from 'react';
 import { getUser } from '../../../../services/userService';
 import { UnlockOutlined, InfoCircleOutlined } from '@ant-design/icons';
-import { Table, Tooltip, Space, Button, Modal} from 'antd';
+import { Table, Tooltip, Space, Button, Modal, Spin } from 'antd';
 import { getProvinceOne } from '../../../../services/clientService';
-import PermissionDeclarationOne from '../../permissionDeclaration/permissionDeclarationOne/PermissionDeclarationOne';
 import PermissionVilleOne from '../../permissionVille/permissionVilleOne/PermissionVilleOne';
 
 const PermissionTacheVilleUser = ({ idVille }) => {
     const scroll = { x: 400 };
     const [data, setData] = useState([]);
-    const [title, setTitle] = useState('')
+    const [title, setTitle] = useState('');
     const [idUser, setIdUser] = useState('');
     const [modalType, setModalType] = useState(null);
-  
-  useEffect(() => {
-    const fetchPermission = async () => {
-      try { 
-        const { data: users } = await getUser();
-        setData(users);
+    const [loading, setLoading] = useState(true); // Ajout de loading
 
-        if(idVille){
-            const {data} = await getProvinceOne(idVille)
-            setTitle(data[0].name)
-        }
+    useEffect(() => {
+        const fetchPermission = async () => {
+            try {
+                setLoading(true); // Début du chargement
+                const { data: users } = await getUser();
+                setData(users);
 
-      } catch (error) {
-        console.log(error);
-      }
+                if (idVille) {
+                    const { data } = await getProvinceOne(idVille);
+                    setTitle(data[0].name);
+                }
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setLoading(false); // Fin du chargement
+            }
+        };
+        fetchPermission();
+    }, [idVille]);
+
+    const handleAddDeclaration = (id) => {
+        openModal('Add', id);
     };
-    fetchPermission();
-  }, [idVille]);
 
-  const handleAddDeclaration = (id) => {
-    openModal('Add', id);
+    const closeAllModals = () => {
+        setModalType(null);
+    };
 
-  }
+    const openModal = (type, idUser = '') => {
+        closeAllModals();
+        setModalType(type);
+        setIdUser(idUser);
+    };
 
-  const closeAllModals = () => {
-    setModalType(null);
-  };
-  
-  const openModal = (type, idUser = '') => {
-    closeAllModals();
-    setModalType(type);
-    setIdUser(idUser);
-  };
+    const columns = [
+        {
+            title: <span>#</span>,
+            dataIndex: 'id',
+            key: 'id',
+            render: (text, record, index) => index + 1,
+            width: '3%',
+        },
+        {
+            title: 'Utilisateur',
+            dataIndex: 'menu_title',
+            key: 'menu_title',
+            render: (text, record) => (
+                <div>{`${record.nom} ${record.prenom}`}</div>
+            ),
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            width: '10%',
+            render: (text, record) => (
+                <Space size="middle">
+                    <Tooltip title="Voir les permissions pour ce client">
+                        <Button
+                            icon={<InfoCircleOutlined />}
+                            onClick={() => handleAddDeclaration(record.id_utilisateur)}
+                            aria-label="Voir les détails"
+                            style={{ color: 'blue' }}
+                        />
+                    </Tooltip>
+                </Space>
+            ),
+        },
+    ];
 
-  // Colonnes du tableau
-  const columns = [
-    {
-      title: <span>#</span>,
-      dataIndex: 'id',
-      key: 'id',
-      render: (text, record, index) => index + 1,
-      width: '3%',
-    },
-    {
-      title: 'Utilisateur',
-      dataIndex: 'menu_title',
-      key: 'menu_title',
-      render: (text, record) => (
-        <div>
-            {`${record.nom} ${record.prenom}`}
-        </div>
-      ),
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      width: '10%',
-      render: (text, record) => (
-        <Space size='middle'>
-            <Tooltip title="Voir les permissions pour ce client">
-              <Button
-                icon={<InfoCircleOutlined />}
-                onClick={() => handleAddDeclaration(record.id_utilisateur)}
-                aria-label="Voir les détails"
-                style={{ color: 'blue' }}
-              />
-            </Tooltip>
-        </Space>
-      )
-    }
-  ];
+    return (
+        <>
+            <div className="client">
+                <div className="client-wrapper">
+                    <div className="client-row">
+                        <div className="client-row-icon">
+                            <UnlockOutlined className="client-icon" />
+                        </div>
+                        <h2 className="client-h2">Gestion des permissions pour la ville de {title}</h2>
+                    </div>
 
-  return (
-    <>
-      <div className="client">
-        <div className="client-wrapper">
-          <div className="client-row">
-            <div className="client-row-icon">
-              <UnlockOutlined className="client-icon" />
+                    {loading ? (
+                        <div style={{ textAlign: 'center', padding: '20px' }}>
+                            <Spin size="large" />
+                        </div>
+                    ) : (
+                        <Table
+                            dataSource={data}
+                            columns={columns}
+                            scroll={scroll}
+                            rowKey="id"
+                            bordered
+                            pagination={false}
+                            className="table_permission"
+                            size="small"
+                            loading={loading} // Affichage du spinner dans le tableau
+                        />
+                    )}
+                </div>
             </div>
-            <h2 className="client-h2">Gestion des permissions pour la ville de {title} </h2>
-          </div>
-          <Table
-            dataSource={data}
-            columns={columns}
-            scroll={scroll}
-            rowKey="id"
-            bordered
-            pagination={false}
-            className="table_permission"
-            size="small"
-          />
-        </div>
-      </div>
 
-      <Modal
-        title=""
-        visible={modalType === 'Add'}
-        onCancel={closeAllModals}
-        footer={null}
-        width={1000}
-        centered
-      >
-         <PermissionVilleOne idVille={idVille} idUser={idUser}/>
-      </Modal>
-    </>
-  );
+            <Modal
+                title=""
+                visible={modalType === 'Add'}
+                onCancel={closeAllModals}
+                footer={null}
+                width={1000}
+                centered
+            >
+                <PermissionVilleOne idVille={idVille} idUser={idUser} />
+            </Modal>
+        </>
+    );
 };
 
 export default PermissionTacheVilleUser;
