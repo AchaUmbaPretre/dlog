fdc2c08d34112e40c1ef85aca90b1eed26628d20
AchaUmbaPretre <achandambi@gmail.com>
2025-03-26 16:49:47 +0100

    commit

--- src/pages/rapportSpecial/RapportSpecial.jsx
+++ src/pages/rapportSpecial/RapportSpecial.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Modal, Button, Input } from 'antd';
+import { Modal, Button, Input, Table } from 'antd';
 import { AuditOutlined, PlusCircleOutlined } from '@ant-design/icons';
 import RapportSpecialForm from './rapportSpecialForm/RapportSpecialForm';
 
@@ -8,7 +8,14 @@ const { Search } = Input;
 const RapportSpecial = () => {
     const [searchValue, setSearchValue] = useState('');
     const [modalType, setModalType] = useState(null);
-    
+    const [pagination, setPagination] = useState({
+        current: 1,
+        pageSize: 25,
+      });
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const scroll = { x: 'max-content' };
+
     const handleAddRapport = (id) => {
         openModal('Add', id);
     }
@@ -21,6 +28,260 @@ const RapportSpecial = () => {
         closeAllModals();
         setModalType(type);
       };
+
+      const columns = [
+        {
+            title: '#',
+            dataIndex: 'id',
+            key: 'id',
+            render: (text, record, index) => {
+              const pageSize = pagination.pageSize || 10;
+              const pageIndex = pagination.current || 1;
+              return (pageIndex - 1) * pageSize + index + 1;
+            },
+            width: "4%"      
+          },
+          {
+            title: 'Superficie',
+            dataIndex: 'superficie',
+            key: 'superficie',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: 'Entreposage',
+            dataIndex: 'entreposage',
+            key: 'entreposage',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: 'TRANSP NRJ',
+            dataIndex: 'transp_nrj',
+            key: 'transp_nrj',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: 'TEU',
+            dataIndex: 'teu',
+            key: 'teu',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "20' LOURDS",
+            dataIndex: 'lourds',
+            key: 'lourds',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "Tonnage",
+            dataIndex: 'tonnage',
+            key: 'tonnage',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "Peage Camion",
+            dataIndex: 'peage_camion',
+            key: 'peage_camion',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "TEU retour",
+            dataIndex: 'teu_retour',
+            key: 'teu_retour',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "MANUT",
+            dataIndex: 'manut',
+            key: 'manut',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "MANUT1",
+            dataIndex: 'manut1',
+            key: 'manut1',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "Camions Manut",
+            dataIndex: 'camions_manut',
+            key: 'camions_manut',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "Sacs manut IN",
+            dataIndex: 'sacs_manut_IN',
+            key: 'sacs_manut_IN',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "Sacs manut OUT",
+            dataIndex: 'sacs_manut_OUT',
+            key: 'sacs_manut_OUT',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "MANUT2",
+            dataIndex: 'manut2',
+            key: 'manut2',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "Bout. /Intrants (T)",
+            dataIndex: 'intrants_t',
+            key: 'intrants_t',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "CAMION charge/décharge",
+            dataIndex: 'charge_decharge',
+            key: 'charge_decharge',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "Sacs ( Tonne)",
+            dataIndex: 'sacs_tonne',
+            key: 'sacs_tonne',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "Palettes (mise en bac)",
+            dataIndex: 'palette_mise',
+            key: 'palette_mise',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "MANUT3",
+            dataIndex: 'manut3',
+            key: 'manut3',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "Bout. (T)",
+            dataIndex: 'bout',
+            key: 'bout',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "Palettes",
+            dataIndex: 'palettes',
+            key: 'palettes',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "LIV LOCALE",
+            dataIndex: 'liv_locale',
+            key: '',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "Camions livrés",
+            dataIndex: 'camions_livres',
+            key: '',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+          {
+            title: "Camions livrés",
+            dataIndex: 'camions_livres',
+            key: '',
+            render: (text, record) => (
+              <div>
+                {text}
+              </div>
+            )
+          },
+      ]
     
   return (
     <>
@@ -58,6 +319,17 @@ const RapportSpecial = () => {
                         </Button>
                     </div>
                 </div>
+
+                <Table
+                  columns={ columns }
+                  dataSource={data}
+                  loading={loading}
+                  rowClassName={(record, index) => (index % 2 === 0 ? 'odd-row' : 'even-row')}
+                  rowKey="id"
+                  bordered
+                  size="small"
+                  scroll={scroll}
+                />
             </div>
             <Modal
               title=""
