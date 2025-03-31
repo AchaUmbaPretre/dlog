ddcf501d61a4949853456e1b800741feddca8179
AchaUmbaPretre <achandambi@gmail.com>
2025-03-31 10:13:40 +0100

    commit

--- src/pages/rapportCloture/RapportCloture.jsx
+++ src/pages/rapportCloture/RapportCloture.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const RapportCloture = () => {
   return (
-    <div>RapportCloture</div>
+    <>
+        <div className="rapport_facture">
+            <h2 className="rapport_h2">RAPPORT CLOTURE</h2>
+            <div className="rapport_wrapper_facture">
+                
+            </div>
+        </div>
+    </>
   )
 }
 
