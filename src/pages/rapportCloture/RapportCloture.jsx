b969f8a51c563cb487df2c84eba63d8bae935184
AchaUmbaPretre <achandambi@gmail.com>
2025-03-31 10:14:49 +0100

    commit

--- src/pages/rapportCloture/RapportCloture.jsx
+++ src/pages/rapportCloture/RapportCloture.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
 const RapportCloture = () => {
+    const [loading, setLoading] = useState(true);
+    const [pagination, setPagination] = useState({
+        current: 1,
+        pageSize: 20,
+      });
+
+    
+
   return (
     <>
         <div className="rapport_facture">
             <h2 className="rapport_h2">RAPPORT CLOTURE</h2>
             <div className="rapport_wrapper_facture">
-                
+
             </div>
         </div>
     </>
