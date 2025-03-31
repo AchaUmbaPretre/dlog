b0129a224f05cec8ab66617d77896131225b68c3
AchaUmbaPretre <achandambi@gmail.com>
2025-03-31 13:00:24 +0100

    commit

--- src/pages/rapportCloture/rapportClotureManueForm/RapportClotureManueForm.jsx
+++ src/pages/rapportCloture/rapportClotureManueForm/RapportClotureManueForm.jsx
@@ -56,6 +56,27 @@ const RapportClotureManueForm = () => {
                                 <InputNumber min={0} style={{ width: '100%' }} placeholder="M² Facturé" parser={(value) => value.replace(/\$\s?|(,*)/g, '')} />
                             </Form.Item>    
                         </Col>
+
+                        <Col span={8}>       
+                            <Form.Item
+                                name="total_manutation"
+                                label="Total Manutention"
+                                rules={[{ required: false, message: "Veuillez entrer le total" }]}
+                            >
+                                <InputNumber min={0} style={{ width: '100%' }} placeholder="Total" />
+                            </Form.Item>         
+                        </Col>
+
+                        <Col span={8}>       
+                            <Form.Item
+                                name="total_entreposage"
+                                label="Total entreposage"
+                                rules={[{ required: false, message: "Veuillez entrer le total" }]}
+                            >
+                                <InputNumber min={0} style={{ width: '100%' }} placeholder="Total" />
+                            </Form.Item>         
+                        </Col>
+
                     </Row>
                 </Form>
             </div>
