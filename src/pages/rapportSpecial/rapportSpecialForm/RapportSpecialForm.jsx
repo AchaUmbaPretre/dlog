af831f1e5c7f6eac3b268e9b40d59d287908e9a9
AchaUmbaPretre <achandambi@gmail.com>
2025-04-02 12:00:09 +0100

    commit

--- src/pages/rapportSpecial/rapportSpecialForm/RapportSpecialForm.jsx
+++ src/pages/rapportSpecial/rapportSpecialForm/RapportSpecialForm.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Form, Col, Select, InputNumber, Button, DatePicker, Row, Divider, Card, notification } from 'antd';
-import { postRapport } from '../../../services/rapportService';
+import { Form, Col, Skeleton, Select, InputNumber, Button, DatePicker, Row, Divider, Card, notification } from 'antd';
+import { getCatRapport, getContratRapport, getElementContrat, postRapport } from '../../../services/rapportService';
 import { getClient } from '../../../services/clientService';
 import { useSelector } from 'react-redux';
 import './rapportSpecialForm.scss'
@@ -10,16 +10,24 @@ const RapportSpecialForm = ({closeModal, fetchData}) => {
     const [isLoading, setIsLoading] = useState(false);
     const [contrat, setContrat] = useState([]);
     const [idContrat, setIdContrat] = useState('');
+    const [cat, setCat] = useState([]);
+    const [idCat, setIdCat] = useState('');
     const [client, setClient] = useState([]);
+    const [element, setElement] = useState([])
+    const [idElement, setIdElement] = useState('')
     const userId = useSelector((state) => state.user?.currentUser?.id_utilisateur);
     
     const fetchDatas = async () => {
         try {
-            const [clientData] = await Promise.all([
-                getClient()
+            const [ contratData, elementData, catData] = await Promise.all([
+                getContratRapport(),
+                getElementContrat(),
+                getCatRapport()
             ]);
 
-            setClient(clientData.data);
+            setContrat(contratData.data)
+            setElement(elementData.data)
+            setCat(catData.data)
             
         } catch (error) {
             console.error('Error fetching data:', error);
@@ -68,7 +76,59 @@ const RapportSpecialForm = ({closeModal, fetchData}) => {
                 <h1 className="h1_rapport">FORM DE RAPPORT</h1>
                 <div className="rapportSpecial_rows">
                     <div className="rapportSpecial_row">
+                        <div>
+                            <Form.Item
+                                name="id_contrat"
+                                label="Contrat"
+                                rules={[{ required: true, message: "Veuillez sélectionner un contrat" }]}
+                            >
+                                { isLoading ? <Skeleton.Input active={true} /> : 
+                                    <Select
+                                        showSearch
+                                        options={contrat.map(item => ({ value: item.id_contrats_rapport , label: item.nom_contrat }))}
+                                        placeholder="Sélectionnez..."
+                                        onChange={setIdContrat}
+                                        optionFilterProp="label"
+                                    />
+                                }
+                            </Form.Item>
+                        </div>
 
+                        <div>
+                            <Form.Item
+                                name="id_cat_rapport "
+                                label="Categorie"
+                                rules={[{ required: true, message: "Veuillez sélectionner une categorie" }]}
+                            >
+                                { isLoading ? <Skeleton.Input active={true} /> : 
+                                    <Select
+                                        showSearch
+                                        options={cat.map(item => ({ value: item.id_cat_rapport  , label: item.nom_cat }))}
+                                        placeholder="Sélectionnez..."
+                                        onChange={setIdCat}
+                                        optionFilterProp="label"
+                                    />
+                                }
+                            </Form.Item>
+                        </div>
+
+                        <div>
+                            <Form.Item
+                                name="id_element_contrat"
+                                label="Element"
+                                rules={[{ required: true, message: "Veuillez sélectionner un element" }]}
+                            >
+                                { isLoading ? <Skeleton.Input active={true} /> : 
+                                    <Select
+                                        showSearch
+                                        options={element.map(item => ({ value: item.id_element_contrat ,label: item.nom_element }))}
+                                        placeholder="Sélectionnez..."
+                                        onChange={setIdElement}
+                                        optionFilterProp="label"
+                                    />
+                                }
+                            </Form.Item>
+                        </div>
                     </div>
                 </div>
             </div>
