88574af02dc7afaa61a18528688f8c577613b436
AchaUmbaPretre <achandambi@gmail.com>
2025-04-04 15:08:55 +0100

    commit

--- src/pages/rapportCloture/rapportClotureBar/RapportClotureBar.jsx
+++ src/pages/rapportCloture/rapportClotureBar/RapportClotureBar.jsx
@@ -21,64 +21,64 @@ const RapportClotureBar = ({ data }) => {
   return (
     <div style={{ height: 400, background:'#fff' }}>
         <ResponsiveBar
-        data={transformedData}
-        keys={['entreposage', 'manutation', 'total']}
-        indexBy="periode"
-        margin={{ top: 50, right: 130, bottom: 60, left: 60 }}
-        padding={0.3}
-        groupMode="grouped"
-        colors={{ scheme: 'set2' }}
-        borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: -45,
-          legend: 'Période',
-          legendPosition: 'middle',
-          legendOffset: 40,
-        }}
-        axisLeft={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'Montant',
-          legendPosition: 'middle',
-          legendOffset: -50,
-          format: formatValue
+            data={transformedData}
+            keys={['entreposage', 'manutation', 'total']}
+            indexBy="periode"
+            margin={{ top: 50, right: 130, bottom: 60, left: 60 }}
+            padding={0.3}
+            groupMode="grouped"
+            colors={{ scheme: 'set2' }}
+            borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: -45,
+            legend: 'Période',
+            legendPosition: 'middle',
+            legendOffset: 40,
+            }}
+            axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'Montant',
+            legendPosition: 'middle',
+            legendOffset: -50,
+            format: formatValue
 
-        }}
-        labelSkipWidth={12}
-        labelSkipHeight={12}
-        labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-        animate={true}
-        motionStiffness={90}
-        motionDamping={15}
-        legends={[
-          {
-            dataFrom: 'keys',
-            anchor: 'bottom-right',
-            direction: 'column',
-            justify: false,
-            translateX: 120,
-            translateY: 0,
-            itemsSpacing: 4,
-            itemWidth: 100,
-            itemHeight: 20,
-            itemDirection: 'left-to-right',
-            itemOpacity: 0.85,
-            symbolSize: 20,
-            effects: [
-              {
-                on: 'hover',
-                style: {
-                  itemOpacity: 1,
+            }}
+            labelSkipWidth={12}
+            labelSkipHeight={12}
+            labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+            animate={true}
+            motionStiffness={90}
+            motionDamping={15}
+            legends={[
+            {
+                dataFrom: 'keys',
+                anchor: 'bottom-right',
+                direction: 'column',
+                justify: false,
+                translateX: 120,
+                translateY: 0,
+                itemsSpacing: 4,
+                itemWidth: 100,
+                itemHeight: 20,
+                itemDirection: 'left-to-right',
+                itemOpacity: 0.85,
+                symbolSize: 20,
+                effects: [
+                {
+                    on: 'hover',
+                    style: {
+                    itemOpacity: 1,
+                    },
                 },
-              },
-            ],
-          },
-        ]}
+                ],
+            },
+            ]}
       />
     </div>
   );
