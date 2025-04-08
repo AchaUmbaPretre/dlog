36cea7aa55557d36a07e099d3e398b00277e738a
AchaUmbaPretre <achandambi@gmail.com>
2025-04-08 10:58:01 +0100

    commit

--- src/pages/charroi/affectations/affectationsForm/AffectationsForm.jsx
+++ src/pages/charroi/affectations/affectationsForm/AffectationsForm.jsx
@@ -14,43 +14,43 @@ const AffectationsForm = () => {
                 <h2 className='controle_h2'>Créer une affectation</h2>                
             </div>
             <div className="controle_wrapper">
-            <Form 
-                layout="vertical" 
-                onFinish={onFinish} 
-                form={form} 
-            >
-                <Row gutter={16}>
-                    <Col span={12}>
-                        <Form.Item
-                            label="Chauffeur"
-                            name="id_chauffeur"
-                            rules={[{ required: true, message: 'Le nom est requis' }]}
-                        >
-                            {loading ? <Skeleton.Input active /> : <Input placeholder="Entrez le titre du projet" />}
-                        </Form.Item>
-                    </Col>
+                <Form 
+                    layout="vertical" 
+                    onFinish={onFinish} 
+                    form={form} 
+                >
+                    <Row gutter={16}>
+                        <Col span={12}>
+                            <Form.Item
+                                label="Chauffeur"
+                                name="id_chauffeur"
+                                rules={[{ required: true, message: 'Le nom est requis' }]}
+                            >
+                                {loading ? <Skeleton.Input active /> : <Input placeholder="Entrez le titre du projet" />}
+                            </Form.Item>
+                        </Col>
 
-                    <Col span={12}>
-                        <Form.Item
-                            label="Site"
-                            name="id_site"
-                            rules={[{ required: true, message: 'Le site est requis' }]}
-                        >
-                            {loading ? <Skeleton.Input active /> : <Input placeholder="Entrez le site..." />}
-                        </Form.Item>
-                    </Col>
+                        <Col span={12}>
+                            <Form.Item
+                                label="Site"
+                                name="id_site"
+                                rules={[{ required: true, message: 'Le site est requis' }]}
+                            >
+                                {loading ? <Skeleton.Input active /> : <Input placeholder="Entrez le site..." />}
+                            </Form.Item>
+                        </Col>
 
-                    <Col span={24}>
-                        <Form.Item
-                            label="Site"
-                            name="id_site"
-                            rules={[{ required: true, message: 'Le site est requis' }]}
-                        >
-                            {loading ? <Skeleton.Input active /> : <Input.TextArea placeholder="Saisir l'adresse...." style={{height:"80px", resize:'none'}} />}
-                        </Form.Item>
-                    </Col>
-                </Row>
-            </Form>
+                        <Col span={24}>
+                            <Form.Item
+                                label="Site"
+                                name="id_site"
+                                rules={[{ required: true, message: 'Le site est requis' }]}
+                            >
+                                {loading ? <Skeleton.Input active /> : <Input.TextArea placeholder="Saisir l'adresse...." style={{height:"80px", resize:'none'}} />}
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                </Form>
             </div>
         </div>
     </>
