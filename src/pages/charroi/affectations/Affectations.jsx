0c2078cfe3d39e3a1bbaf23a74f951dbf5673713
AchaUmbaPretre <achandambi@gmail.com>
2025-04-08 10:08:53 +0100

    commit

--- src/pages/charroi/affectations/Affectations.jsx
+++ src/pages/charroi/affectations/Affectations.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Input, Button } from 'antd';
+import { SwapOutlined, PlusCircleOutlined } from '@ant-design/icons';
+const { Search } = Input;
 
 const Affectations = () => {
+    const [searchValue, setSearchValue] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    const handleAddAffectation = () => {
+
+    }
+
   return (
-    <div>Affectations</div>
+    <>
+        <div className="client">
+            <div className="client-wrapper">
+                <div className="client-row">
+                    <div className="client-row-icon">
+                        <SwapOutlined className='client-icon'/>
+                    </div>
+                    <h2 className="client-h2">Affectation</h2>
+                </div>
+                <div className="client-actions">
+                    <div className="client-row-left">
+                        <Search 
+                            placeholder="Recherche..." 
+                            onChange={(e) => setSearchValue(e.target.value)}
+                            enterButton
+                        />
+                    </div>
+                    <div className="client-rows-right">
+                        <Button
+                            type="primary"
+                            icon={<PlusCircleOutlined />}
+                            onClick={handleAddAffectation}
+                        >
+                            Affectation
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
   )
 }
 
