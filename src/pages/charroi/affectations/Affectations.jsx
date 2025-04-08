38fa3e12dd5f3abee87bbd2cea7cd24cc984a1e3
AchaUmbaPretre <achandambi@gmail.com>
2025-04-08 10:54:43 +0100

    commit

--- src/pages/charroi/affectations/Affectations.jsx
+++ src/pages/charroi/affectations/Affectations.jsx
@@ -1,12 +1,126 @@
 import React, { useState } from 'react'
-import { Input, Button } from 'antd';
-import { SwapOutlined, PlusCircleOutlined } from '@ant-design/icons';
+import { Input, Button, Table, Space, Tooltip, Popconfirm } from 'antd';
+import { SwapOutlined, CalendarOutlined, PlusCircleOutlined, DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import moment from 'moment';
+import AffectationsForm from './affectationsForm/AffectationsForm';
+
 const { Search } = Input;
 
 const Affectations = () => {
     const [searchValue, setSearchValue] = useState('');
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState([]);
+    const scroll = { x: 400 };
+    const [pagination, setPagination] = useState({
+        current: 1,
+        pageSize: 15,
+    });
+
+    const columns = [
+        { 
+          title: '#', 
+          dataIndex: 'id', 
+          key: 'id', 
+          render: (text, record, index) => (
+            <span style={{ fontWeight: 'bold' }}>{index + 1}</span>
+          ),
+          width: "5%", 
+        },
+        {
+          title: 'Chauffeur',
+          dataIndex: 'nom_chauffeur',
+          render: (text, record) => (
+            <div>
+              <span style={{ color: '#1890ff' }}>{`${record.prenom_chauffeur}`}</span> - <span style={{ fontWeight: 'bold' }}>{record.nom_chauffeur}</span>
+            </div>
+          ),
+        },
+        {
+          title: 'Nom site',
+          dataIndex: 'nom_site',
+          render: (text) => (
+            <div>
+              <span style={{ color: '#52c41a', fontStyle: 'italic' }}>{text}</span>
+            </div>
+          ),
+        },
+        {
+          title: 'Description',
+          dataIndex: 'commentaire',
+          render: (text) => (
+            <div>
+              <span style={{ fontStyle: 'italic' }}>{text}</span>
+            </div>
+          ),
+        },
+        {
+          title: "Date d'affectation",
+          dataIndex: 'created_at',
+          render: (text) => (
+            <Tooltip title="Date d'affectation">
+              <div>
+                <CalendarOutlined style={{ color: '#fa8c16', marginRight: 8 }} /> 
+                <span>{moment(text).format('DD-MM-yyyy')}</span>
+              </div>
+            </Tooltip>
+          ),
+        },
+        {
+          title: 'Créé par',
+          dataIndex: 'affectation',
+          render: (text, record) => (
+            <div>
+              <span style={{ color: '#722ed1' }}>{record.prenom}</span> - <span style={{ fontWeight: 'bold' }}>{record.nom}</span>
+            </div>
+          ),
+        },
+        {
+          title: 'Actions',
+          dataIndex: 'actions',
+          key: 'actions',
+          width: '10%',
+          render: (text, record) => (
+            <Space size="middle">
+                          <Tooltip title="Modifier" placement="top">
+                  <Button
+                    icon={<EditOutlined />}
+                    style={{
+                      color: '#fff',
+                      backgroundColor: '#52c41a',
+                      borderColor: '#52c41a',
+                      transition: 'all 0.3s ease',
+                    }}
+                    aria-label="Modifier"
+                    onMouseEnter={(e) => e.target.style.backgroundColor = '#45b22d'}
+                    onMouseLeave={(e) => e.target.style.backgroundColor = '#52c41a'}
+                  />
+                </Tooltip>
+        
+                <Tooltip title="Supprimer" placement="top">
+                  <Popconfirm
+                    title="Êtes-vous sûr de vouloir supprimer ce client ?"
+                    okText="Oui"
+                    cancelText="Non"
+                    onConfirm={() => {}}
+                  >
+                    <Button
+                      icon={<DeleteOutlined />}
+                      style={{
+                        color: '#fff',
+                        backgroundColor: '#ff4d4f',
+                        borderColor: '#ff4d4f',
+                        transition: 'all 0.3s ease',
+                      }}
+                      aria-label="Supprimer"
+                      onMouseEnter={(e) => e.target.style.backgroundColor = '#e10000'}
+                      onMouseLeave={(e) => e.target.style.backgroundColor = '#ff4d4f'}
+                    />
+                  </Popconfirm>
+                </Tooltip>
+            </Space>
+          )
+        }
+      ];
 
     const handleAddAffectation = () => {
 
@@ -36,12 +150,33 @@ const Affectations = () => {
                             icon={<PlusCircleOutlined />}
                             onClick={handleAddAffectation}
                         >
-                            Affectation
+                            Ajouter une affectation
                         </Button>
                     </div>
                 </div>
+                <Table
+                    columns={columns}
+                    dataSource={data}
+                    rowKey="id_projet"
+                    loading={loading}
+                    scroll={scroll}
+                    size="small"
+                    bordered
+                    rowClassName={(record, index) => (index % 2 === 0 ? 'odd-row' : 'even-row')}
+                />
             </div>
         </div>
+
+        <Modal
+            title="Liste des documents"
+            visible={modalType === 'ListeDoc'}
+            onCancel={closeAllModals}
+            footer={null}
+            width={900}
+            centered
+        >
+            <AffectationsForm />
+      </Modal>
     </>
   )
 }
--- src/pages/charroi/affectations/affectationsForm/AffectationsForm.jsx
+++ src/pages/charroi/affectations/affectationsForm/AffectationsForm.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import { Form, Input, Skeleton, Row, Col, Modal, Select } from 'antd';
+
+const { Option } = Select;
+
 
 const AffectationsForm = () => {
+    const [form] = Form.useForm();
+    
   return (
-    <div>AffectationsForm</div>
+    <>
+        <div className="controle_form">
+            <div className="controle_title_rows">
+                <h2 className='controle_h2'>Créer une affectation</h2>                
+            </div>
+            <div className="controle_wrapper">
+            <Form 
+                layout="vertical" 
+                onFinish={onFinish} 
+                form={form} 
+            >
+                <Row gutter={16}>
+                    <Col span={12}>
+                        <Form.Item
+                            label="Chauffeur"
+                            name="id_chauffeur"
+                            rules={[{ required: true, message: 'Le nom est requis' }]}
+                        >
+                            {loading ? <Skeleton.Input active /> : <Input placeholder="Entrez le titre du projet" />}
+                        </Form.Item>
+                    </Col>
+
+                    <Col span={12}>
+                        <Form.Item
+                            label="Site"
+                            name="id_site"
+                            rules={[{ required: true, message: 'Le site est requis' }]}
+                        >
+                            {loading ? <Skeleton.Input active /> : <Input placeholder="Entrez le site..." />}
+                        </Form.Item>
+                    </Col>
+
+                    <Col span={24}>
+                        <Form.Item
+                            label="Site"
+                            name="id_site"
+                            rules={[{ required: true, message: 'Le site est requis' }]}
+                        >
+                            {loading ? <Skeleton.Input active /> : <Input.TextArea placeholder="Saisir l'adresse...." style={{height:"80px", resize:'none'}} />}
+                        </Form.Item>
+                    </Col>
+                </Row>
+            </Form>
+            </div>
+        </div>
+    </>
   )
 }
 
