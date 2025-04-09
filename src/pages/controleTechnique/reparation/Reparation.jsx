474afa9ad9b4554d9aee254bae0993a358f306e0
AchaUmbaPretre <achandambi@gmail.com>
2025-04-09 13:49:23 +0100

    commit

--- src/pages/controleTechnique/reparation/Reparation.jsx
+++ src/pages/controleTechnique/reparation/Reparation.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Input } from 'antd';
+import { ToolOutlined } from '@ant-design/icons';
+
+const { Search } = Input;
+
 
 const Reparation = () => {
+    const [searchValue, setSearchValue] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    const scroll = { x: 400 };
+    
   return (
-    <div>Reparation</div>
+    <>
+        <div className="client">
+            <div className="client-wrapper">
+                <div className="client-row">
+                    <div className="client-row-icon">
+                        <ToolOutlined className='client-icon'/>
+                    </div>
+                    <h2 className="client-h2">Liste des réparations</h2>
+                </div>
+
+                <div className="client-actions">
+                    <div className="client-row-left">
+                        <Search 
+                            placeholder="Recherche..." 
+                            onChange={(e) => setSearchValue(e.target.value)}
+                            enterButton
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
   )
 }
 
