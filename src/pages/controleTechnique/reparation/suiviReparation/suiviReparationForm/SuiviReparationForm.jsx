338eec7585030830d083c26bde1efb1d19104531
AchaUmbaPretre <achandambi@gmail.com>
2025-04-10 16:14:34 +0100

    commit

--- src/pages/controleTechnique/reparation/suiviReparation/suiviReparationForm/SuiviReparationForm.jsx
+++ src/pages/controleTechnique/reparation/suiviReparation/suiviReparationForm/SuiviReparationForm.jsx
@@ -6,6 +6,7 @@ import { PlusCircleOutlined, MinusCircleOutlined, SendOutlined } from '@ant-desi
 const SuiviReparationForm = () => {
     const [form] = Form.useForm();
     const [tache, setTache] = useState([]);
+    const [evaluation, setEvaluation] = useState([]);
     const [loadingData, setLoadingData] = useState(false);
     
     
@@ -35,94 +36,121 @@ const SuiviReparationForm = () => {
                     onFinish={onFinish}
                 >
                     <Card>
-
-                    </Card>
-                    <Form.List name="reparations">
-                        {(fields, { add, remove }) => (
-                            <>
-                            {fields.map(({ key, name, ...restField }) => (
-                                <Row key={key} gutter={12} align="middle">
-
-                                    <Col xs={24} md={7}>
-                                        <Form.Item
-                                        {...restField}
-                                        name={[name, 'id_tache']}
-                                        label="Tache"
-                                        rules={[
-                                            { required: true, message: 'Veuillez sélectionner une tache...' },
-                                        ]}
-                                        >
-                                        <Select
-                                            allowClear
-                                            showSearch
-                                            options={tache.map((item) => ({
-                                                value: item.id_type_reparation,
-                                                label: `${item.type_rep}`,
-                                            }))}
-                                            placeholder="Sélectionnez un type de réparation..."
-                                            optionFilterProp="label"
-                                        />
-                                        </Form.Item>
-                                    </Col>
-                                    <Col xs={24} md={7}>
-                                        <Form.Item
-                                        {...restField}
-                                        name={[name, 'id_piece']}
-                                        label="Piece"
-                                        rules={[
-                                            { required: true, message: 'Veuillez fournir une réparation...' },
-                                        ]}
-                                        >
-                                        <Select
-                                            allowClear
-                                            showSearch
-                                            options={tache.map((item) => ({
-                                                value: item.id_type_reparation,
-                                                label: `${item.type_rep}`,
-                                            }))}
-                                            placeholder="Sélectionnez un type de réparation..."
-                                            optionFilterProp="label"
-                                        />
-                                        </Form.Item>
-                                    </Col>
-                                    <Col xs={24} md={7}>
-                                        <Form.Item
-                                        {...restField}
-                                        name={[name, 'montant']}
-                                        label="Montant"
-                                        rules={[
-                                            { required: false, message: 'Veuillez fournir le montant...' },
-                                        ]}
-                                        >
-                                            <InputNumber size='large' min={0} placeholder="Saisir le montant..." style={{width:'100%'}}/>
-                                        </Form.Item>
-                                    </Col>
-                                    <Col xs={24} md={2}>
-                                        <Button
-                                        type="text"
-                                        danger
-                                        icon={<MinusCircleOutlined />}
-                                        onClick={() => remove(name)}
-                                        >
-                                        </Button>
-                                    </Col>
-
-                                </Row>
-                            ))}
-                            <Form.Item>
-                                <Button
-                                type="dashed"
-                                onClick={() => add()}
-                                icon={<PlusCircleOutlined />}
-                                style={{ width: '100%' }}
-                                >
-                                Ajouter un suivi
-                                </Button>
+                        <Col xs={24} md={24}>
+                            <Form.Item
+                                name="id_etat"
+                                label="Evaluation"
+                                rules={[
+                                    {
+                                        required: false,
+                                        message: 'Veuillez sélectionner un etat...',
+                                    }
+                                ]}
+                            >
+                                {loadingData ? <Skeleton.Input active={true} /> : 
+                                    <Select
+                                        allowClear
+                                        showSearch
+                                        options={evaluation.map((item) => ({
+                                            value: item.id_type_statut_suivi                                           ,
+                                            label: `${item.nom_type_statut}`,
+                                        }))}
+                                        placeholder="Sélectionnez une évaluation..."
+                                        optionFilterProp="label"
+                                    />}
                             </Form.Item>
-                            </>
-                        )}
-                    </Form.List>
+                        </Col>
+                    </Card>
 
+                    <Card>
+                        <Form.List name="reparations">
+                            {(fields, { add, remove }) => (
+                                <>
+                                {fields.map(({ key, name, ...restField }) => (
+                                    <Row key={key} gutter={12} align="middle">
+
+                                        <Col xs={24} md={7}>
+                                            <Form.Item
+                                            {...restField}
+                                            name={[name, 'id_tache']}
+                                            label="Tache"
+                                            rules={[
+                                                { required: true, message: 'Veuillez sélectionner une tache...' },
+                                            ]}
+                                            >
+                                            <Select
+                                                allowClear
+                                                showSearch
+                                                options={tache.map((item) => ({
+                                                    value: item.id_type_reparation,
+                                                    label: `${item.type_rep}`,
+                                                }))}
+                                                placeholder="Sélectionnez un type de réparation..."
+                                                optionFilterProp="label"
+                                            />
+                                            </Form.Item>
+                                        </Col>
+                                        <Col xs={24} md={7}>
+                                            <Form.Item
+                                            {...restField}
+                                            name={[name, 'id_piece']}
+                                            label="Piece"
+                                            rules={[
+                                                { required: true, message: 'Veuillez fournir une réparation...' },
+                                            ]}
+                                            >
+                                            <Select
+                                                allowClear
+                                                showSearch
+                                                options={tache.map((item) => ({
+                                                    value: item.id_type_reparation,
+                                                    label: `${item.type_rep}`,
+                                                }))}
+                                                placeholder="Sélectionnez un type de réparation..."
+                                                optionFilterProp="label"
+                                            />
+                                            </Form.Item>
+                                        </Col>
+                                        <Col xs={24} md={7}>
+                                            <Form.Item
+                                            {...restField}
+                                            name={[name, 'montant']}
+                                            label="Montant"
+                                            rules={[
+                                                { required: false, message: 'Veuillez fournir le montant...' },
+                                            ]}
+                                            >
+                                                <InputNumber size='large' min={0} placeholder="Saisir le montant..." style={{width:'100%'}}/>
+                                            </Form.Item>
+                                        </Col>
+                                        <Col xs={24} md={2}>
+                                            <Button
+                                            type="text"
+                                            danger
+                                            icon={<MinusCircleOutlined />}
+                                            onClick={() => remove(name)}
+                                            >
+                                            </Button>
+                                        </Col>
+
+                                    </Row>
+                                ))}
+                                <Form.Item>
+                                    <Button
+                                    type="dashed"
+                                    onClick={() => add()}
+                                    icon={<PlusCircleOutlined />}
+                                    style={{ width: '100%' }}
+                                    >
+                                    Ajouter un suivi
+                                    </Button>
+                                </Form.Item>
+                                </>
+                            )}
+                        </Form.List>
+                    </Card>
+
+                    <Card>
                     <Col xs={24} md={24}>
                         <Form.Item
                             name="commentaire"
@@ -142,6 +170,7 @@ const SuiviReparationForm = () => {
                             Soumettre
                         </Button>
                     </div>
+                    </Card>
                 </Form>
             </div>
         </div>
