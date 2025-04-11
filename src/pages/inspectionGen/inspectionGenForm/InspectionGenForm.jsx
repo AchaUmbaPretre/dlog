bd1bd82243d2b9a304015b98afc1a07cec50532b
AchaUmbaPretre <achandambi@gmail.com>
2025-04-11 13:16:44 +0100

    commit

--- src/pages/inspectionGen/inspectionGenForm/InspectionGenForm.jsx
+++ src/pages/inspectionGen/inspectionGenForm/InspectionGenForm.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, Row, Col, Skeleton, Select } from 'antd';
 
 const InspectionGenForm = () => {
+    const [form] = Form.useForm();
+    const [ loading, setLoading ] = useState(false);
+    const [ chauffeur, setChauffeur ] = useState([])
+    const [ vehicule, setVehicule ] = useState([])
+    const [ loadingData, setLoadingData ] = useState(false);
+
+    const handleSubmit = async (values) => {
+
+    }
+
   return (
-    <div>InspectionGenForm</div>
+    <>
+        <div className="controle_form">
+            <div className="controle_title_rows">
+                <div className="controle_h2">Form inspection</div>
+            </div>
+            <div className="controle_wrapper">
+                <Form
+                    form={form}
+                    layout="vertical"
+                    onFinish={handleSubmit}
+                >
+                    <Row gutter={12}>
+                        <Col xs={24} md={12}>
+                            <Form.Item
+                                label="Véhicule"
+                                name="id_vehicule"
+                                rules={[{ required: true, message: 'Veuillez sélectionner un véhicule' }]}
+                            >
+                                { loadingData ? <Skeleton.Input active={true} /> : 
+                                <Select
+                                    allowClear
+                                    showSearch
+                                    options={vehicule.map((item) => ({
+                                        value: item.id_vehicule,
+                                        label: item.nom_vehicule,
+                                    }))}
+                                    optionFilterProp="label"
+                                    placeholder="Sélectionnez un vehicule..."
+                                    size='large'
+                                /> }
+                            </Form.Item>
+                        </Col>
+
+                        <Col xs={24} md={12}>
+                            <Form.Item
+                                label="Catégorie d'Instruction"
+                                name="id_cat_instruction"
+                                rules={[{ required: true, message: 'Veuillez sélectionner une catégorie' }]}
+                            >
+                                { loadingData ? <Skeleton.Input active={true} /> : 
+                                <Select
+                                    allowClear
+                                    showSearch
+                                    options={chauffeur.map((item) => ({
+                                            value: item.id_cat_inspection,
+                                            label: item.nom_cat_inspection,
+                                        }))}
+                                    optionFilterProp="label"
+                                    placeholder="Sélectionnez une categorie..."
+                                    size='large'
+                                /> }
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                </Form>
+            </div>
+        </div>
+    </>
   )
 }
 
