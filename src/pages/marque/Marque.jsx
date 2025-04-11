4e7bd48b77e84e9f272edb25a81e7cf40b0c613d
AchaUmbaPretre <achandambi@gmail.com>
2025-04-11 09:56:41 +0100

    commit

--- src/pages/marque/Marque.jsx
+++ src/pages/marque/Marque.jsx
@@ -1,10 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import { Table, Button, Modal, Input, message, Dropdown, Menu, notification, Space, Tooltip, Popconfirm, Tag, Form, Popover } from 'antd';
+import { CarOutlined, PlusCircleOutlined } from '@ant-design/icons';
+import { getMarque } from '../../services/charroiService';
+
+const { Search } = Input;
 
 
 const Marque = () => {
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(true);
+    const [searchValue, setSearchValue] = useState('');
+    const [pagination, setPagination] = useState({
+        current: 1,
+        pageSize: 15,
+    }); 
+    const scroll = { x: 'max-content' };
+    
+       const fetchData = async() => {
+            try {
+                const { data } = await getMarque();
+                setData(data);
+                setLoading(false);
+    
+            } catch (error) {
+                notification.error({
+                    message: 'Erreur de chargement',
+                    description: 'Une erreur est survenue lors du chargement des données.',
+                  });
+                  setLoading(false);
+            }
+        }
+    
+        useEffect(()=> {
+            fetchData()
+        }, [])
     
     const columns = [
         { 
@@ -38,9 +67,52 @@ const Marque = () => {
           } */
       ];
 
+      const handleAddMarque = () => {
+
+      }
+
   return (
     <>
+        <div className="client">
+            <div className="client-wrapper">
+                <div className="client-row">
+                    <div className="client-row-icon">
+                        <CarOutlined className='client-icon'/>
+                    </div>
+                    <h2 className="client-h2">Liste des marques</h2>
+                </div>
 
+                <div className="client-actions">
+                    <div className="client-row-left">
+                        <Search 
+                            placeholder="Recherche..." 
+                            onChange={(e) => setSearchValue(e.target.value)}
+                            enterButton
+                        />
+                    </div>
+                    <div className="client-rows-right">
+                        <Button
+                            type="primary"
+                            icon={<PlusCircleOutlined />}
+                            onClick={handleAddMarque}
+                        >
+                            Ajouter une marque
+                        </Button>
+                    </div>
+                </div>
+                <Table
+                    columns={columns}
+                    dataSource={data}
+                    rowKey="id_marque"
+                    loading={loading}
+                    scroll={scroll}
+                    size="small"
+                    onChange={(pagination)=> setPagination(pagination)}
+                    bordered
+                    rowClassName={(record, index) => (index % 2 === 0 ? 'odd-row' : 'even-row')}
+                />
+            </div>
+        </div>
     </>
   )
 }
