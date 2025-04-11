c2b05bc0f8f698def627e31276b050c57ce09c94
AchaUmbaPretre <achandambi@gmail.com>
2025-04-11 12:38:54 +0100

    commit

--- src/pages/inspectionGen/InspectionGen.jsx
+++ src/pages/inspectionGen/InspectionGen.jsx
@@ -1,15 +1,44 @@
 import React, { useState } from 'react'
-import { Input, Button } from 'antd';
+import { Input, Button, Table } from 'antd';
 import { FileSearchOutlined, PlusCircleOutlined } from '@ant-design/icons'
 
 const { Search } = Input;
 
 const InspectionGen = () => {
+    const [loading, setLoading] = useState(true);
     const [searchValue, setSearchValue] = useState('');
+    const [data, setData] = useState([]);
+    const [pagination, setPagination] = useState({
+        current: 1,
+        pageSize: 15,
+    });
+    const scroll = { x: 400 };
+
+/*         const fetchData = async() => {
+            try {
+                 const { data } = await getControleTechnique();
+                setData(data.data);
+                setLoading(false);
+    
+            } catch (error) {
+                notification.error({
+                    message: 'Erreur de chargement',
+                    description: 'Une erreur est survenue lors du chargement des données.',
+                  });
+                  setLoading(false);
+            }
+        }
+    
+        useEffect(()=> {
+            fetchData()
+        }, []) */
 
     const handleAddInspection = () => {
 
     }
+
+    const columns = []
+    
 
   return (
     <>
@@ -39,6 +68,17 @@ const InspectionGen = () => {
                         </Button>
                     </div>
                 </div>
+                <Table
+                    columns={columns}
+                    dataSource={data}
+                    rowKey="id_inspection"
+                    loading={loading}
+                    scroll={scroll}
+                    size="small"
+                    onChange={(pagination)=> setPagination(pagination)}
+                    bordered
+                    rowClassName={(record, index) => (index % 2 === 0 ? 'odd-row' : 'even-row')}
+                />
             </div>
         </div>
     </>
