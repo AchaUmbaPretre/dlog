d8bb63bb754a22fba2175bd9cbeb418939dca6d8
AchaUmbaPretre <achandambi@gmail.com>
2025-04-11 10:35:30 +0100

    commit

--- src/pages/modeles/modeleForm/ModeleForm.jsx
+++ src/pages/modeles/modeleForm/ModeleForm.jsx
@@ -1,8 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Col, DatePicker, Form, notification, Input, InputNumber, Row, Select, Skeleton, Button, Divider, message } from 'antd';
+import { SendOutlined } from '@ant-design/icons';
 
 const ModeleForm = () => {
+    const [form] = Form.useForm();
+    const [loadingData, setLoadingData] = useState(false);
+    const [modele, setModele] = useState([]);
+    
+    const onFinish = () => {
+
+    }
+
   return (
-    <div>ModeleForm</div>
+    <>
+        <div className="controle_form">
+            <div className="controle_title_rows">
+                <h2 className="controle_h2">ENREGISTRER UN MODELE</h2>
+            </div>
+            <div className="controle_wrapper">
+                <Form
+                    form={form}
+                    name="chauffeurForm"
+                    layout="vertical"
+                    autoComplete="off"
+                    className="custom-form"
+                    onFinish={onFinish}
+                >
+                    <Row gutter={12}>
+                        <Col xs={24} md={12}>
+                            <Form.Item
+                                name="id_marque"
+                                label="Marque"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir une marque...',
+                                    }
+                                ]}
+                            >
+                                {loadingData ? <Skeleton.Input active={true} /> : <Input size='large' placeholder="Saisir la marque..." style={{width:'100%'}}/>}
+                            </Form.Item>
+                        </Col>
+
+                        <Col xs={24} md={12}>
+                            <Form.Item
+                                name="id_modele"
+                                label="Modèle"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir un modèle...',
+                                    }
+                                ]}
+                            >
+                                {loadingData ? <Skeleton.Input active={true} /> : 
+                                <Select
+                                    allowClear
+                                    size='large'
+                                    showSearch
+                                    options={modele.map((item) => ({
+                                        value: item.id_modele                                           ,
+                                        label: `${item.nom_modele}`,
+                                    }))}
+                                    placeholder="Sélectionnez un modele..."
+                                    optionFilterProp="label"
+                                />}
+                            </Form.Item>
+                        </Col>
+
+                        <Button type="primary" htmlType="submit" icon={<SendOutlined />}>
+                            Soumettre
+                        </Button>
+                    </Row>
+                </Form>
+            </div>
+        </div>
+    </>
   )
 }
 
