bc4756e0734a5268bf0925a2484df6f474cdcdcf
AchaUmbaPretre <achandambi@gmail.com>
2025-04-15 10:56:36 +0100

    commit

--- src/pages/inspectionGen/inspectionGenDetail/InspectionGenDetail.jsx
+++ src/pages/inspectionGen/inspectionGenDetail/InspectionGenDetail.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
-const InspectionGenDetail = () => {
+const InspectionGenDetail = ({ inspectionId }) => {
+    const [data, setData] = useState([]);
+    const scroll = { x: 400 };
+    const [loading, setLoading] = useState(false);
+
+        const fetchDatas = async() => {
+            try {
+                const { data } = await getSubInspection(inspectionId);
+                setData(data)
+    
+            } catch (error) {
+                notification.error({
+                    message: 'Erreur de chargement',
+                    description: 'Une erreur est survenue lors du chargement des données.',
+                });
+                setLoading(false);
+            }
+        }
+    
+        useEffect(()=> {
+            fetchDatas()
+        }, [inspectionId])
+
   return (
-    <div>InspectionGenDetail</div>
+    <>
+
+    </>
   )
 }
 
