297eb2129111ec15073a53ba48428e02472aeb07
AchaUmbaPretre <achandambi@gmail.com>
2025-04-15 11:33:34 +0100

    commit

--- src/pages/inspectionGen/inspectionGenDetail/InspectionGenDetail.jsx
+++ src/pages/inspectionGen/inspectionGenDetail/InspectionGenDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Col, Row, Spin, notification, Image, Typography, Tag } from 'antd';
+import { Card, Col, Row, Spin, notification, Image, Typography, Tag, Divider } from 'antd';
 import { getSubInspection } from '../../../services/charroiService';
 import moment from 'moment';
 import config from '../../../config';
@@ -32,47 +32,66 @@ const InspectionGenDetail = ({ inspectionId }) => {
         }
     }, [inspectionId]);
 
+    const headerInfo = data.length > 0 ? {
+        marque: data[0].nom_marque,
+        immatriculation: data[0].immatriculation
+    } : {};
+
     return (
         <div style={{ padding: 16 }}>
-            {loading ? (
-                <Spin size="large" />
-            ) : (
-                <Row gutter={[16, 16]}>
-                    {data.map((item) => (
-                        <Col xs={24} md={12} lg={8} key={item.id_sub_inspection_gen}>
-                            <Card
-                                hoverable
-                                cover={
-                                    <Image
-                                        className="userImg"
-                                        src={`${DOMAIN}/${item.img}`}
-                                        style={{ height: 200, objectFit: 'cover' }}
-                                        alt="Profil utilisateur"
-                                    />
-                                }
-                            >
-                                <Title level={5}>{item.nom_marque} - {item.immatriculation}</Title>
-                                <div style={{display:'flex', flexDirection:'column'}}>
-                                    <Text strong>Type de réparation:</Text> <Text>{item.type_rep}</Text><br />
-                                    <Text strong>Catégorie:</Text> <Text>{item.nom_cat_inspection}</Text><br />
-                                    <Text strong>Caractéristique:</Text> <Text>{item.nom_carateristique_rep}</Text><br />
-                                    <Text strong>Montant:</Text> <Text>{item.montant} $</Text><br />
-                                    <Text strong>Date inspection:</Text> <Text>{moment(item.date_inspection).format('DD/MM/YYYY')}</Text><br />
-                                    <Text strong>Statut:</Text> <Tag color="orange">{item.nom_type_statut}</Tag><br />
-                                    <Paragraph>
-                                        <Text strong>Commentaire:</Text><br />
-                                        {item.commentaire}
-                                    </Paragraph>
-                                    <Paragraph>
-                                        <Text strong>Avis:</Text><br />
-                                        {item.avis}
-                                    </Paragraph>
-                                </div>
-                            </Card>
-                        </Col>
-                    ))}
-                </Row>
-            )}
+            <Card>
+                {loading ? (
+                    <Spin size="large" />
+                ) : (
+                    <>
+                        {/* En-tête globale */}
+                        {data.length > 0 && (
+                            <>
+                                <Title level={4}>Détails de l’inspection</Title>
+                                <Text strong>Marque :</Text> <Text>{headerInfo.marque}</Text><br />
+                                <Text strong>Immatriculation :</Text> <Text>{headerInfo.immatriculation}</Text>
+                                <Divider />
+                            </>
+                        )}
+
+                        {/* Détails par sous-inspection */}
+                        <Row gutter={[16, 16]}>
+                            {data.map((item) => (
+                                <Col xs={24} md={12} lg={8} key={item.id_sub_inspection_gen}>
+                                    <Card
+                                        hoverable
+                                        cover={
+                                            <Image
+                                                className="userImg"
+                                                src={`${DOMAIN}/${item.img}`}
+                                                style={{ height: 200, objectFit: 'cover' }}
+                                                alt="Illustration inspection"
+                                            />
+                                        }
+                                    >
+                                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                            <Text strong>Type de réparation:</Text> <Text>{item.type_rep}</Text><br />
+                                            <Text strong>Catégorie:</Text> <Text>{item.nom_cat_inspection}</Text><br />
+                                            <Text strong>Caractéristique:</Text> <Text>{item.nom_carateristique_rep}</Text><br />
+                                            <Text strong>Montant:</Text> <Text>{item.montant} $</Text><br />
+                                            <Text strong>Date inspection:</Text> <Text>{moment(item.date_inspection).format('DD/MM/YYYY')}</Text><br />
+                                            <Text strong>Statut:</Text> <Tag color="orange">{item.nom_type_statut}</Tag><br />
+                                            <Paragraph>
+                                                <Text strong>Commentaire:</Text><br />
+                                                {item.commentaire}
+                                            </Paragraph>
+                                            <Paragraph>
+                                                <Text strong>Avis:</Text><br />
+                                                {item.avis}
+                                            </Paragraph>
+                                        </div>
+                                    </Card>
+                                </Col>
+                            ))}
+                        </Row>
+                    </>
+                )}
+            </Card>
         </div>
     );
 };
