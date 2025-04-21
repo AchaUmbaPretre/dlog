f7b8e067658e6ccbc8f79abae193bdf9f6e74fb5
AchaUmbaPretre <achandambi@gmail.com>
2025-04-21 15:56:55 +0100

    commit

--- src/services/charroiService.js
+++ src/services/charroiService.js
@@ -207,6 +207,11 @@ export const postSuiviInspections= async (data) => {
 }
 
 //Suivi reparation
+
+export const getSuiviReparation= async (id) => {
+    return axios.get(`${DOMAIN}/api/charroi/suivi_reparation?id_sud_reparation=${id}`);
+}
+
 export const getSuiviReparationOne = async (id) => {
     return axios.get(`${DOMAIN}/api/charroi/suivi_reparationOne?id_sud_reparation=${id}`);
 }
