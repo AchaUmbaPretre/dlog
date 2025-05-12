5d7b5a4b1e90166e8f53d7c3d192f0c0277df442
AchaUmbaPretre <achandambi@gmail.com>
2025-05-12 16:20:39 +0100

    commit

--- src/pages/inspectionGen/inspectionGenDetail/InspectionGenDetail.jsx
+++ src/pages/inspectionGen/inspectionGenDetail/InspectionGenDetail.jsx
@@ -3,6 +3,7 @@ import { Card, Col, Row, Spin, notification, Image, Typography, Tag, Divider } f
 import { getSubInspection } from '../../../services/charroiService';
 import moment from 'moment';
 import config from '../../../config';
+import './inspectionGenDetail.scss'
 
 const { Title, Text, Paragraph } = Typography;
 
@@ -38,61 +39,23 @@ const InspectionGenDetail = ({ inspectionId }) => {
     } : {};
 
     return (
-        <div style={{ padding: 16 }}>
-            <Card>
-                {loading ? (
-                    <Spin size="large" />
-                ) : (
-                    <>
-                        {/* En-tête globale */}
-                        {data.length > 0 && (
-                            <>
-                                <Title level={4}>Détails de l’inspection</Title>
-                                <Text strong>Marque :</Text> <Text>{headerInfo.marque}</Text><br />
-                                <Text strong>Immatriculation :</Text> <Text>{headerInfo.immatriculation}</Text><br />
-                                <Text strong>Date inspection:</Text> <Text>{moment(headerInfo.date_inspection).format('DD/MM/YYYY')}</Text>
-                                <Divider />
-                            </>
-                        )}
+        <>
+            <div className="inspectionGenDetail">
+                <Card loading={loading}>
+                    <div className="inspectionGen_wrapper">
+                        <div className="inspectionGen_top">
+                            <span className="inspection_span">Marque : <strong>{headerInfo.marque}</strong></span>
+                            <span className="inspection_span">Immatriculation : <strong>{headerInfo.marque}</strong></span>
+                            <span className="inspection_span">Date inspection : <strong>{headerInfo.marque}</strong></span>
+                        </div>
 
-                        {/* Détails par sous-inspection */}
-                        <Row gutter={[16, 16]}>
-                            {data.map((item) => (
-                                <Col xs={24} md={12} lg={8} key={item.id_sub_inspection_gen}>
-                                    <Card
-                                        hoverable
-                                        cover={
-                                            <Image
-                                                className="userImg"
-                                                src={`${DOMAIN}/${item.img}`}
-                                                style={{ height: 200, objectFit: 'cover' }}
-                                                alt="Illustration inspection"
-                                            />
-                                        }
-                                    >
-                                        <div style={{ display: 'flex', flexDirection: 'column' }}>
-                                            <Text strong>Type de réparation:</Text> <Text>{item.type_rep}</Text><br />
-                                            <Text strong>Catégorie:</Text> <Text>{item.nom_cat_inspection}</Text><br />
-                                            <Text strong>Caractéristique:</Text> <Text>{item.nom_carateristique_rep}</Text><br />
-                                            <Text strong>Montant:</Text> <Text>{item.montant} $</Text><br />
-                                            <Text strong>Statut:</Text> <Tag color="orange">{item.nom_type_statut}</Tag><br />
-                                            <Paragraph>
-                                                <Text strong>Commentaire:</Text><br />
-                                                {item.commentaire}
-                                            </Paragraph>
-                                            <Paragraph>
-                                                <Text strong>Avis:</Text><br />
-                                                {item.avis}
-                                            </Paragraph>
-                                        </div>
-                                    </Card>
-                                </Col>
-                            ))}
-                        </Row>
-                    </>
-                )}
-            </Card>
-        </div>
+                        <div className="inspectionGen_bottom">
+                            
+                        </div>
+                    </div>
+                </Card>
+            </div>
+        </>
     );
 };
 
