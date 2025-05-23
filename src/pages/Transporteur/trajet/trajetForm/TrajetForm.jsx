f4cfebd496580232d48ec3bbe5b974bf5dc916bf
AchaUmbaPretre <achandambi@gmail.com>
2025-05-23 12:02:25 +0100

    commit

--- src/pages/Transporteur/trajet/trajetForm/TrajetForm.jsx
+++ src/pages/Transporteur/trajet/trajetForm/TrajetForm.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Form, Row, Divider, Card, Col, message, notification, InputNumber, Skeleton, Select, Button, Input, DatePicker } from 'antd';
 import { getLocalisation, getModeTransport, getTransporteur, getTypeTarif } from '../../../../services/transporteurService';
-import { SendOutlined } from '@ant-design/icons';
+import { SendOutlined, PlusCircleOutlined, MinusCircleOutlined } from '@ant-design/icons';
 
 
 const TrajetForm = () => {
@@ -60,12 +60,12 @@ const TrajetForm = () => {
                 <Form
                     form={form}
                     layout="vertical"
+                    variant={'filled'}
                     onFinish={onFinish}
                 >
-                    <Card>
+                    <Card size="small" type="inner" title='Principaux'>
                         <Row gutter={12}>
-                            <Divider orientation="left" plain>Principaux</Divider>
-                            <Col xs={24} md={8}>
+                            <Col xs={24} md={12}>
                                 <Form.Item
                                     label="Départ"
                                     name="id_depart"
@@ -85,7 +85,7 @@ const TrajetForm = () => {
                                     }
                                 </Form.Item>
                             </Col>
-                            <Col xs={24} md={8}>
+                            <Col xs={24} md={12}>
                                 <Form.Item
                                     label="Destination"
                                     name="id_arrive"
@@ -105,65 +105,168 @@ const TrajetForm = () => {
                                     }
                                 </Form.Item>
                             </Col>
-
-                            <Col xs={24} md={8}>
-                                <Form.Item
-                                    label="Cout principal"
-                                    name="cout_principal"
-                                >
-                                    <InputNumber min={0} placeholder="ex: 100$" style={{width:'100%'}}/>
-                                </Form.Item>
-                            </Col>
-
-                            <Col xs={24} md={8}>
-                                <Form.Item
-                                    label="Manutention départ"
-                                    name="manutention_depart"
-                                >
-                                    <InputNumber min={0} placeholder="ex: 150" style={{width:'100%'}}/>
-                                </Form.Item>
-                            </Col>
-
-                            <Col xs={24} md={8}>
-                                <Form.Item
-                                    label="Manutention arrivée"
-                                    name="manutention_arrivee	"
-                                    rules={[{ required: false, message: 'Veuillez fournir la manutention arrivée' }]}
-                                >
-                                    <InputNumber min={0} placeholder=" ex: 200$" style={{width:'100%'}}/>
-                                </Form.Item>
-                            </Col>
-
-                            <Col xs={24} md={8}>
-                                <Form.Item
-                                    label="Livraison locale"
-                                    name="livraison_locale"
-                                >
-                                    <InputNumber min={0} placeholder="ex: 150" style={{width:'100%'}}/>
-                                </Form.Item>
-                            </Col>
-
-                            <Col xs={24} md={8}>
-                                <Form.Item
-                                    label="Frais"
-                                    name="frais"
-                                >
-                                    <InputNumber min={0} placeholder="ex: 90" style={{width:'100%'}}/>
-                                </Form.Item>
-                            </Col>
-
-                            <Col xs={24} md={8}>
-                                <Form.Item
-                                    label="Total"
-                                    name="total"
-                                >
-                                    <InputNumber min={0} placeholder="ex: 100" style={{width:'100%'}}/>
-                                </Form.Item>
-                            </Col>
-
                         </Row>
                     </Card>
-                    <Button type="primary" size='large' htmlType="submit" icon={<SendOutlined />} style={{marginTop:'20px'}}>
+                    <Form.List name="segment">
+                        {(fields, { add, remove }) => (
+                            <>
+                            <Divider orientation="left" plain>Le(s) segment(s)</Divider>
+                            {fields.map(({ key, name, ...restField }) => (
+                                <Card style={{marginBottom:'10px'}}>
+                                    <Row key={key} gutter={12} align="middle">
+                                        <Col xs={24} md={8}>
+                                            <Form.Item
+                                                {...restField}
+                                                name={[name, 'ordre']}
+                                                label="ordre"
+                                                rules={[
+                                                    { required: true, message: 'Veuillez fournir un numero...' },
+                                                ]}
+                                            >
+                                                <InputNumber min={0} placeholder="Saisir le budget..." style={{width:'100%'}}/>
+                                            </Form.Item>
+                                        </Col>
+
+                                        <Col xs={24} md={8}>
+                                            <Form.Item
+                                                label="Départ"
+                                                {...restField}
+                                                name={[name, "id_depart"]}
+                                                rules={[{ required: true, message: 'Veuillez sélectionner un ' }]}
+                                            >
+                                                { loadingData ? <Skeleton.Input active={true} /> : 
+                                                <Select
+                                                    allowClear
+                                                    showSearch
+                                                    options={local?.map((item) => ({
+                                                            value: item.id_localisation,
+                                                            label: `${item.nom}`,
+                                                    }))}
+                                                    optionFilterProp="label"
+                                                    placeholder="Sélectionnez..."
+                                                />
+                                                }
+                                            </Form.Item>
+                                        </Col>
+                                        
+                                        <Col xs={24} md={8}>
+                                            <Form.Item
+                                                label="Destination"
+                                                {...restField}
+                                                name={[name, "id_arrive"]}
+                                                rules={[{ required: true, message: 'Veuillez sélectionner un' }]}
+                                            >
+                                                { loadingData ? <Skeleton.Input active={true} /> : 
+                                                <Select
+                                                    allowClear
+                                                    showSearch
+                                                    options={local?.map((item) => ({
+                                                            value: item.id_localisation,
+                                                            label: `${item.nom}`,
+                                                    }))}
+                                                    optionFilterProp="label"
+                                                    placeholder="Sélectionnez..."
+                                                />
+                                                }
+                                            </Form.Item>
+                                        </Col>
+
+                                        <Col xs={24} md={8}>
+                                            <Form.Item
+                                                label="Date départ"
+                                                {...restField}
+                                                name={[name, "date_depart"]}
+                                                rules={[{ required: true, message: 'Veuillez sélectionner un' }]}
+                                            >
+                                                <DatePicker format="YYYY-MM-DD" style={{ width: '100%' }} />
+                                            </Form.Item>
+                                        </Col>
+
+                                        <Col xs={24} md={8}>
+                                            <Form.Item
+                                                label="Date arrivée"
+                                                {...restField}
+                                                name={[name, "date_arrivee"]}
+                                                rules={[{ required: true, message: 'Veuillez sélectionner un' }]}
+                                            >
+                                                <DatePicker format="YYYY-MM-DD" style={{ width: '100%' }} />
+                                            </Form.Item>
+                                        </Col>
+
+                                        <Col xs={24} md={8}>
+                                            <Form.Item
+                                                label="Distance Km"
+                                                {...restField}
+                                                name={[name, "distance_km"]}
+                                                rules={[{ required: true, message: 'Veuillez fournir la distance km' }]}
+                                            >
+                                                <InputNumber min={0} placeholder="Saisir..." style={{width:'100%'}}/>
+                                            </Form.Item>
+                                        </Col>
+
+                                        <Col xs={24} md={8}>
+                                            <Form.Item
+                                                label="Mode transport"
+                                                {...restField}
+                                                name={[name, "mode_transport"]}
+                                                rules={[{ required: false, message: 'Veuillez sélectionner un mode' }]}
+                                            >
+                                                { loadingData ? <Skeleton.Input active={true} /> : 
+                                                <Select
+                                                    allowClear
+                                                    showSearch
+                                                    options={mode?.map((item) => ({
+                                                            value: item.id_mode_transport,
+                                                            label: `${item.nom_mode}`,
+                                                    }))}
+                                                    optionFilterProp="label"
+                                                    placeholder="Sélectionnez..."
+                                                />
+                                                }
+                                            </Form.Item>
+                                        </Col>
+
+                                        <Col xs={24} md={8}>
+                                            <Form.Item
+                                                label="Prix"
+                                                {...restField}
+                                                name={[name, "prix"]}
+                                            >
+                                                { loadingData ? <Skeleton.Input active={true} /> : 
+                                                    <InputNumber min={0} placeholder="ex : 100$..." style={{width:'100%'}}/>
+                                                }
+                                            </Form.Item>
+                                        </Col>
+
+                                                                            <Col xs={24} md={2}>
+                                        <Button
+                                            type="text"
+                                            danger
+                                            icon={<MinusCircleOutlined />}
+                                            onClick={() => {
+                                                remove(name);
+                                            }}
+
+                                        >
+                                        </Button>
+                                    </Col>
+                                    </Row>
+                                </Card>
+                            ))}
+                                <Form.Item>
+                                    <Button
+                                        type="dashed"
+                                        onClick={() => add()}
+                                        icon={<PlusCircleOutlined />}
+                                        style={{ width: '100%' }}
+                                    >
+                                       Ajouter un segment
+                                    </Button>
+                                </Form.Item>
+                            </>
+                        )}
+                    </Form.List>
+                    <Button type="primary" size='large' htmlType="submit" icon={<SendOutlined />}>
                         Soumettre
                     </Button>
                 </Form>
