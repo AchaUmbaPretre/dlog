187bae340cccfd79e2213dbd0ac7f302efb3fcd3
AchaUmbaPretre <achandambi@gmail.com>
2025-05-26 10:51:22 +0100

    commit

--- src/pages/Transporteur/trajet/trajetDetail/TrajetDetail.jsx
+++ src/pages/Transporteur/trajet/trajetDetail/TrajetDetail.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react'
 import './trajetDetail.scss'
+import { getTrajetOne } from '../../../../services/transporteurService'
 
-const TrajetDetail = () => {
+const TrajetDetail = ({id_trajet}) => {
+    const [ data, setData ] = useState([]);
+
+    useEffect(()=> {
+        const fetchData = async() => {
+            const { data } = await getTrajetOne(id_trajet);
+            setData(data)
+        }
+
+        fetchData()
+    },[id_trajet]);
+
   return (
     <>
         <div className="trajetDetail">
-            
+
         </div>
     </>
   )
