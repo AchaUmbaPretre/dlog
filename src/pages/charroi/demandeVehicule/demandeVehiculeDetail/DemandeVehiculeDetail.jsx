7e1e58359cb65277e81cefaf57180061a5875a35
AchaUmbaPretre <achandambi@gmail.com>
2025-05-27 13:18:43 +0100

    commit

--- src/pages/charroi/demandeVehicule/demandeVehiculeDetail/DemandeVehiculeDetail.jsx
+++ src/pages/charroi/demandeVehicule/demandeVehiculeDetail/DemandeVehiculeDetail.jsx
@@ -1,18 +1,27 @@
 import { useState } from 'react'
 import { getDemandeVehiculeOne } from '../../../../services/charroiService';
 
-const DemandeVehiculeDetail = ({id_demande}) => {
+const DemandeVehiculeDetail = ({id_demande_vehicule}) => {
     const [ data, setData] = useState([]);
 
     const fetchData = async() => {
         try {
             const [detailData] = await Promise.all([
-                getDemandeVehiculeOne()
+                getDemandeVehiculeOne(id_demande_vehicule)
             ])
         } catch (error) {
-            
+            notification.error({
+                message: 'Erreur de chargement',
+                description: 'Une erreur est survenue lors du chargement des données.',
+            });
+        } finally {
+          setLoading(false);
         }
     }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
 
   return (
     <>
