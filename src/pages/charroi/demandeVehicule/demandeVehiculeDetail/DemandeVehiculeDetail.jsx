2fb0e7057f110e84a7fa0533c3d73647944e912c
AchaUmbaPretre <achandambi@gmail.com>
2025-05-27 13:16:41 +0100

    commit

--- src/pages/charroi/demandeVehicule/demandeVehiculeDetail/DemandeVehiculeDetail.jsx
+++ src/pages/charroi/demandeVehicule/demandeVehiculeDetail/DemandeVehiculeDetail.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import { useState } from 'react'
+import { getDemandeVehiculeOne } from '../../../../services/charroiService';
 
-const DemandeVehiculeDetail = () => {
+const DemandeVehiculeDetail = ({id_demande}) => {
+    const [ data, setData] = useState([]);
+
+    const fetchData = async() => {
+        try {
+            const [detailData] = await Promise.all([
+                getDemandeVehiculeOne()
+            ])
+        } catch (error) {
+            
+        }
+    }
+
   return (
     <>
         
