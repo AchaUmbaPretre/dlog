c95247beb6373b5ac4340a40fc8775f51e61a33c
AchaUmbaPretre <achandambi@gmail.com>
2025-05-27 13:44:46 +0100

    commit

--- src/pages/charroi/demandeVehicule/demandeVehiculeDetail/DemandeVehiculeDetail.jsx
+++ src/pages/charroi/demandeVehicule/demandeVehiculeDetail/DemandeVehiculeDetail.jsx
@@ -10,7 +10,11 @@ const DemandeVehiculeDetail = ({ id_demande_vehicule }) => {
     setLoading(true);
     try {
       const response = await getDemandeVehiculeOne(id_demande_vehicule);
-      setData(response.data[0]);
+      if (response.data.length > 0) {
+        setData(response.data[0]);
+      } else {
+        setData(null);
+      }
     } catch (error) {
       notification.error({
         message: 'Erreur de chargement',
@@ -36,21 +40,29 @@ const DemandeVehiculeDetail = ({ id_demande_vehicule }) => {
   }
 
   return (
-    <Descriptions 
-      title="Détails de la Demande de Véhicule" 
-      bordered 
+    <Descriptions
+      title="Détails de la Demande de Véhicule"
+      bordered
       column={1}
       size="middle"
     >
       <Descriptions.Item label="Client">{data.nom}</Descriptions.Item>
       <Descriptions.Item label="Nom du service">{data.nom_service}</Descriptions.Item>
       <Descriptions.Item label="Type de véhicule">{data.nom_type_vehicule}</Descriptions.Item>
+      <Descriptions.Item label="Immatriculation">{data.immatriculation}</Descriptions.Item>
+      <Descriptions.Item label="Nom du chauffeur">{data.nom_chauffeur}</Descriptions.Item>
       <Descriptions.Item label="Statut de la demande">{data.nom_type_statut}</Descriptions.Item>
       <Descriptions.Item label="Motif de la demande">{data.nom_motif_demande}</Descriptions.Item>
       <Descriptions.Item label="Localisation">{data.localisation}</Descriptions.Item>
-      <Descriptions.Item label="Date de chargement">{new Date(data.date_chargement).toLocaleString()}</Descriptions.Item>
-      <Descriptions.Item label="Date prévue">{new Date(data.date_prevue).toLocaleString()}</Descriptions.Item>
-      <Descriptions.Item label="Date de retour">{new Date(data.date_retour).toLocaleString()}</Descriptions.Item>
+      <Descriptions.Item label="Date de chargement">
+        {new Date(data.date_chargement).toLocaleString()}
+      </Descriptions.Item>
+      <Descriptions.Item label="Date prévue">
+        {new Date(data.date_prevue).toLocaleString()}
+      </Descriptions.Item>
+      <Descriptions.Item label="Date de retour">
+        {new Date(data.date_retour).toLocaleString()}
+      </Descriptions.Item>
       <Descriptions.Item label="Nom de l'utilisateur">{data.nom_user}</Descriptions.Item>
     </Descriptions>
   );
