ca30149aa65b19278270de10d4f1d3daf4ec0bd4
AchaUmbaPretre <achandambi@gmail.com>
2025-05-28 12:06:19 +0100

    commit

--- src/pages/Transporteur/trajet/Trajet.jsx
+++ src/pages/Transporteur/trajet/Trajet.jsx
@@ -267,14 +267,15 @@ const Trajet = () => {
       width: '125px',
       render: (text, record) => (
         <Space size="small">
-          <Tooltip title="Modifier ce trajet">
+{/*           <Tooltip title="Modifier ce trajet">
             <Button
               type="text"
               icon={<EditOutlined />}
               onClick={() => handleEdit(record.id_trajet)}
+              style={{ color: '#1890ff' }}
               aria-label="Modifier"
             />
-          </Tooltip>
+          </Tooltip> */}
           <Dropdown overlay={getActionMenu(record, openModal)} trigger={['click']}>
             <Button type="text" icon={<MoreOutlined />} style={{ color: 'blue' }} />
           </Dropdown>
