b0acd035d8aad3a379da0dd52d6710b0efbbb52b
AchaUmbaPretre <achandambi@gmail.com>
2025-06-03 14:27:15 +0100

    commit

--- src/pages/charroi/securite/bottomNav/BottomNav.jsx
+++ src/pages/charroi/securite/bottomNav/BottomNav.jsx
@@ -27,9 +27,7 @@ const BottomNav = () => {
     <div className="bottom-nav">
       {navItems.map((item) => (
         <button
-          key={item.path}
           className={`nav-item ${location.pathname === item.path ? 'active' : ''}`}
-          onClick={() => navigate(item.path)}
         >
           {item.icon}
         </button>
