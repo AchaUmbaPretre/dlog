707a9576703650ae0d2f65ddd49f7eeb35264b9b
AchaUmbaPretre <achandambi@gmail.com>
2025-06-11 08:56:33 +0100

    commit

--- src/pages/inspectionGen/inspectionGenDetail/InspectionGenDetail.jsx
+++ src/pages/inspectionGen/inspectionGenDetail/InspectionGenDetail.jsx
@@ -223,25 +223,25 @@ const InspectionGenDetail = ({ inspectionId }) => {
 
     return (
         <div className="inspectionGenDetail">
-            <Card loading={loading} className="rows">
-                <div className="inspectionGen_wrapper">
-                    <div className="inspectionGen-arrow">
-                        <Tooltip title="Précédent">
-                            <Button className="row-arrow" onClick={goToPrevious}>
-                                <LeftCircleFilled className="icon-arrow" />
-                            </Button>
-                        </Tooltip>
-                        <h2 className="inspection_h2">
-                            DÉTAILS DE L'INSPECTION N° {`${new Date().getFullYear().toString().slice(2)}${inspectionId?.toString().padStart(4, '0')}`}
-                        </h2>
-                        <Tooltip title="Suivant">
-                            <Button className="row-arrow" onClick={goToNext}>
-                                <RightCircleFilled className="icon-arrow" />
-                            </Button>
-                        </Tooltip>
-                    </div>
+          <Card loading={loading} className="rows">
+            <div className="inspectionGen_wrapper">
+              <div className="inspectionGen-arrow">
+                <Tooltip title="Précédent">
+                  <Button className="row-arrow" onClick={goToPrevious}>
+                    <LeftCircleFilled className="icon-arrow" />
+                  </Button>
+                </Tooltip>
+                <h2 className="inspection_h2">
+                  DÉTAILS DE L'INSPECTION N° {`${new Date().getFullYear().toString().slice(2)}${inspectionId?.toString().padStart(4, '0')}`}
+                </h2>
+                <Tooltip title="Suivant">
+                  <Button className="row-arrow" onClick={goToNext}>
+                    <RightCircleFilled className="icon-arrow" />
+                  </Button>
+                </Tooltip>
+              </div>
 
-                    <div className="inspectionGen_top">
+              <div className="inspectionGen_top">
                       <span className="inspection_span">
                         <DollarCircleOutlined style={{ color: '#13c2c2' }} /> TOTAL : <strong>{total} $</strong>
                       </span>
@@ -258,38 +258,40 @@ const InspectionGenDetail = ({ inspectionId }) => {
                         <CalendarOutlined style={{ color: '#1890ff' }} /> DATE INSPECTION :{' '}
                         <strong>{headerInfo.date_inspection ? moment(headerInfo.date_inspection).format('DD-MM-YYYY') : 'N/A'}</strong>
                       </span>
-                    </div>
+              </div>
 
-                    <div className="inspectionGen_bottom_wrapper">
-                        {datas.map((item) => (
-                            <div className="inspectionGen_bottom" key={item.id || `${item.nom_cat_inspection}-${item.type_rep}`}>
-                                <div className='inspectionGen_row_icones'>
-                                    <Dropdown overlay={getActionMenu(item, openModal)} trigger={['click']}>
-                                        <Button type='text' icon={<MoreOutlined />} style={{ color: 'blue' }} />
-                                    </Dropdown>
-                                </div>
-                                <div className="inspection_rows_img">
-                                    <Image src={item.img ? `${DOMAIN}/${item.img}` : imgDetail} alt="Image" className="img" />
-                                </div>
-                                <Divider style={{margin:'0', margin:'10px'}} />
-                                <div className="inspectionGen_bottom_rows">
-                                    <span className="inspectiongen_txt">Type de réparation : <strong>{item.type_rep}</strong></span>
-                                    <span className="inspectiongen_txt">Catégorie : <strong>{item.nom_cat_inspection}</strong></span>
-                                    <span className="inspectiongen_txt">Caractéristique : {item.nom_carateristique_rep}</span>
-                                    <span className="inspectiongen_txt">Montant : <strong>{item.montant} $</strong></span>
-                                    <span className="inspectiongen_txt">Statut : <Tag color="orange" onClick={()=>handleTracking(item.id_sub_inspection_gen)}>{item.nom_type_statut}</Tag></span>
-                                    <span className="inspectiongen_txt">Date d'enregistrement : <Tag color="green" icon={<CalendarOutlined />}>
-                                      {moment(item.created_at).format('LL [à] HH:mm')}
-                                    </Tag>
-                                    </span>
-                                    <span className="inspectiongen_txt">Date de dernière mise à jour : <Tag color="blue" icon={<CalendarOutlined />}>{moment(item.update_at).format('LL')}</Tag></span>
-                                    <span className="inspectiongen_txt txt">Commentaire : <strong>{item.commentaire}</strong></span>
-                                    <span className="inspectiongen_txt txt">Avis : <strong>{item.avis}</strong></span>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+              <div className="inspectionGen_bottom_wrapper">
+                {datas.map((item) => (
+                <div className="inspectionGen_bottom" key={item.id || `${item.nom_cat_inspection}-${item.type_rep}`}>
+                  <div className='inspectionGen_row_icones'>
+                    <Dropdown overlay={getActionMenu(item, openModal)} trigger={['click']}>
+                      <Button type='text' icon={<MoreOutlined />} style={{ color: 'blue' }} />
+                    </Dropdown>
+                  </div>
+
+                  <div className="inspection_rows_img">
+                    <Image src={item.img ? `${DOMAIN}/${item.img}` : imgDetail} alt="Image" className="img" />
+                  </div>
+
+                  <Divider style={{margin:'0', margin:'10px'}} />
+                  <div className="inspectionGen_bottom_rows">
+                    <span className="inspectiongen_txt">Type de réparation : <strong>{item.type_rep}</strong></span>
+                    <span className="inspectiongen_txt">Catégorie : <strong>{item.nom_cat_inspection}</strong></span>
+                    <span className="inspectiongen_txt">Caractéristique : {item.nom_carateristique_rep}</span>
+                    <span className="inspectiongen_txt">Montant : <strong>{item.montant} $</strong></span>
+                    <span className="inspectiongen_txt">Statut : <Tag color="orange" onClick={()=>handleTracking(item.id_sub_inspection_gen)}>{item.nom_type_statut}</Tag></span>
+                    <span className="inspectiongen_txt">Date d'enregistrement : <Tag color="green" icon={<CalendarOutlined />}>
+                      {moment(item.created_at).format('LL [à] HH:mm')}
+                      </Tag>
+                    </span>
+                    <span className="inspectiongen_txt">Date de dernière mise à jour : <Tag color="blue" icon={<CalendarOutlined />}>{moment(item.update_at).format('LL')}</Tag></span>
+                    <span className="inspectiongen_txt txt">Commentaire : <strong>{item.commentaire}</strong></span>
+                    <span className="inspectiongen_txt txt">Avis : <strong>{item.avis}</strong></span>
+                  </div>
                 </div>
+                ))}
+              </div>
+            </div>
             </Card>
             <Modal
                 title=""
