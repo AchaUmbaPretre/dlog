cc0e4db1d73400bec8c976f9bd4ebb603788a640
AchaUmbaPretre <achandambi@gmail.com>
2025-06-16 12:25:58 +0100

    commit

--- src/pages/batiment/listCroquis/ListCroquis.jsx
+++ src/pages/batiment/listCroquis/ListCroquis.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from 'react'
 import './listCroquis.scss'
+import { getPlans } from '../../../services/batimentService'
 
 const ListCroquis = () => {
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const { data } = await getPlans();
+            setData(data)
+        } catch (error) {
+            notification.error({
+                message: 'Erreur de chargement',
+                description: 'Une erreur est survenue lors du chargement des données.',
+            });
+        } finally {
+            setLoading(false)
+        }
+    }
+    fetchData()
+}, [])
   return (
     <>
         <div className="list_croquis">
