11827c1e34829a78c9d3a00f6bada63c61611911
AchaUmbaPretre <achandambi@gmail.com>
2025-06-23 14:34:41 +0100

    commit

---
+++ src/pages/charroi/securite/securiteSortie/SecuriteSortie.jsx
@@ -0,0 +1,11 @@
+import './securiteSortie.scss'
+
+const SecuriteSortie = () => {
+  return (
+    <>
+        <div className='securiteSortie'>SecuriteSortie</div>
+    </>
+  )
+}
+
+export default SecuriteSortie
