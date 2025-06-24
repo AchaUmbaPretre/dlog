1b8880cf27e54be02e3239962b1c433bf5b81068
AchaUmbaPretre <achandambi@gmail.com>
2025-06-24 12:54:42 +0100

    commit

--- src/pages/charroi/securite/securiteRetour/SecuriteRetour.jsx
+++ src/pages/charroi/securite/securiteRetour/SecuriteRetour.jsx
@@ -1,10 +1,77 @@
-import './securiteRetour.scss'
+import { useEffect, useState } from 'react'
+import { notification, Button } from 'antd';
+import { getSortieVehicule, postSortieVehicule } from '../../../../services/charroiService';
+import { useSelector } from 'react-redux';
 
 const SecuriteRetour = () => {
-    
-  return (
-    <div>SecuriteRetour</div>
-  )
-}
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const userId = useSelector((state) => state.user?.currentUser?.id_utilisateur);
 
-export default SecuriteRetour
+  const fetchData = async () => {
+    try {
+      const { data } = await getSortieVehicule();
+      setData(data);
+    } catch (error) {
+      notification.error({
+        message: 'Erreur de chargement',
+        description: 'Une erreur est survenue lors du chargement des données.',
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const onFinish = async(idBandeSortie) => {
+    const value = {
+        id_bande_sortie: idBandeSortie,
+        id_agent: userId
+    }
+    await postSortieVehicule(value);
+    fetchData();
+    notification.success({
+      message: 'Sortie validée',
+      description: `Le véhicule avec la bande ${idBandeSortie} a été validé pour sortir.`,
+    });
+  };
+
+  return (
+    <div className='securiteSortie'>
+    { data.length === 0 ? (<div className="securite_sortie_empty">🚫 Aucune demande de sortie disponible.</div>) : (
+      <div className="securiteSortie_wrapper">
+        <h2 className="securite_sortie_h2">Liste des véhicules à retourner</h2>
+        <div className="securite_sortie_rows">
+          {data.map((d) => (
+            <div className="securite_sortie_row" key={d.id_bande_sortie}>
+              <div className="row">
+                <strong className="securite_strong">
+                  Véhicule : <span className="securite_desc">{d?.immatriculation}</span>
+                </strong>
+                <strong className="securite_strong">
+                  Chauffeur : <span className="securite_desc">{d?.nom}</span>
+                </strong>
+              </div>
+
+              <Button
+                type='primary'
+                className='securite_btn'
+                onClick={() => onFinish(d.id_bande_sortie)}
+                loading={loading}
+              >
+                Sortie
+              </Button>
+            </div>
+          ))}
+        </div>
+      </div>
+      )
+    }
+    </div>
+  );
+};
+
+export default SecuriteRetour;
