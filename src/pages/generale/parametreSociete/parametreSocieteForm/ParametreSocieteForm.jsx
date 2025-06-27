f9c9d1c50c2ebe30d041e6c07b050e8dbbec84eb
AchaUmbaPretre <achandambi@gmail.com>
2025-06-27 12:38:51 +0100

    commit

--- src/pages/generale/parametreSociete/parametreSocieteForm/ParametreSocieteForm.jsx
+++ src/pages/generale/parametreSociete/parametreSocieteForm/ParametreSocieteForm.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import { Col, DatePicker, Space, Form, Upload, notification, Input, InputNumber, Row, Select, Skeleton, Button, Divider, message, Modal } from 'antd';
+import { Col, Card, Space, Form, Upload, Input, notification, Row, Select, Skeleton, Button, Divider, message, Modal } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import getCroppedImg from '../../../../utils/getCroppedImg';
 import Cropper from 'react-easy-crop';
-
+import { postSociete } from '../../../../services/userService';
 
 const ParametreSocieteForm = () => {
     const [form] = Form.useForm();
@@ -53,8 +53,19 @@ const ParametreSocieteForm = () => {
     };
     
 
-    const onFinish = async() => {
+    const onFinish = async(values) => {
+        setLoading(true)
 
+        try {
+            await postSociete(values);
+            notification.success({
+                message: 'Succès',
+                description: 'La société a été enregistrée avec succès.',
+            });
+
+        } catch (error) {
+            
+        }
     }
 
 
@@ -65,111 +76,112 @@ const ParametreSocieteForm = () => {
                 <h2 className="controle_h2">ENREGISTRER UNE SOCIETE</h2>
             </div>
             <div className="controle_wrapper">
-                <Form
-                    form={form}
-                    name="chauffeurForm"
-                    layout="vertical"
-                    autoComplete="off"
-                    className="custom-form"
-                    onFinish={onFinish}
-                >
-                    <Row gutter={12}>
-                        <Col xs={24} md={8}>
-                            <Form.Item
-                                name="nom_societe"
-                                label="Nom"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Veuillez fournir une nom...',
-                                    }
-                                ]}
-                            >
-                                <Input size='large' placeholder="Saisir le nom de société..." style={{width:'100%'}}/>
-                            </Form.Item>
-                        </Col>
-
-                        <Col xs={24} md={8}>
-                            <Form.Item
-                                name="adresse"
-                                label="Adresse"
-                                rules={[
+                <Card>
+                    <Form
+                        form={form}
+                        name="chauffeurForm"
+                        layout="vertical"
+                        autoComplete="off"
+                        onFinish={onFinish}
+                    >
+                        <Row gutter={12}>
+                            <Col xs={24} md={8}>
+                                <Form.Item
+                                    name="nom_societe"
+                                    label="Nom"
+                                    rules={[
                                         {
                                             required: true,
-                                            message: 'Veuillez fournir une adresse...',
+                                            message: 'Veuillez fournir une nom...',
                                         }
                                     ]}
-                            >
-                                <Input size='large' placeholder="Saisir l'adresse " style={{width:'100%'}}/>
-                            </Form.Item>
-                        </Col>
-
-                        <Col xs={24} md={8}>
-                            <Form.Item
-                                name="rccm"
-                                label="Rccm"
-                            >
-                                <Input size='large' placeholder="CD/KNG/RCCM/13-B-192..." style={{width:'100%'}}/>
-                            </Form.Item>
-                        </Col>
-
-                        <Col xs={24} md={8}>
-                            <Form.Item
-                                name="nif"
-                                label="Nif"
-                            >
-                                <Input size='large' placeholder="B0600029R" style={{width:'100%'}}/>
-                            </Form.Item>
-                        </Col>
-
-                        <Col xs={24} md={8}>
-                            <Form.Item
-                                name="tel"
-                                label="Tél"
-                            >
-                                <Input size='large' placeholder="+243" style={{width:'100%'}}/>
-                            </Form.Item>
-                        </Col>
-
-                        <Col xs={24} md={8}>
-                            <Form.Item
-                                name="email"
-                                label="Email"
-                            >
-                                <Input size='large' placeholder="xxx@gmail.com" style={{width:'100%'}}/>
-                            </Form.Item>
-                        </Col>
-
-                        <Col xs={24} md={8}>
-                            <Form.Item
-                                name="logo"
-                                label="Logo"
-                            >
-                                <Upload  
-                                    accept="image/*"
-                                    listType="picture-card"
-                                    onChange={handleUploadChange}
-                                    beforeUpload={() => false} 
                                 >
-                                    <Button icon={<UploadOutlined />}></Button>
-                                </Upload>
-                            </Form.Item>
-                        </Col>
+                                    <Input size='large' placeholder="Saisir le nom de société..." style={{width:'100%'}}/>
+                                </Form.Item>
+                            </Col>
 
-                        <Col xs={24}>
-                            <Form.Item>
-                                <Space className="button-group">
-                                    <Button type="primary" htmlType="submit" loading={loading} disabled={loading}>
-                                        {'Ajouter'}
-                                    </Button>
-                                    <Button htmlType="reset">
-                                        Réinitialiser
-                                    </Button>
-                                </Space>
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                </Form>
+                            <Col xs={24} md={8}>
+                                <Form.Item
+                                    name="adresse"
+                                    label="Adresse"
+                                    rules={[
+                                            {
+                                                required: true,
+                                                message: 'Veuillez fournir une adresse...',
+                                            }
+                                        ]}
+                                >
+                                    <Input size='large' placeholder="Saisir l'adresse " style={{width:'100%'}}/>
+                                </Form.Item>
+                            </Col>
+
+                            <Col xs={24} md={8}>
+                                <Form.Item
+                                    name="rccm"
+                                    label="Rccm"
+                                >
+                                    <Input size='large' placeholder="CD/KNG/RCCM/13-B-192..." style={{width:'100%'}}/>
+                                </Form.Item>
+                            </Col>
+
+                            <Col xs={24} md={8}>
+                                <Form.Item
+                                    name="nif"
+                                    label="Nif"
+                                >
+                                    <Input size='large' placeholder="B0600029R" style={{width:'100%'}}/>
+                                </Form.Item>
+                            </Col>
+
+                            <Col xs={24} md={8}>
+                                <Form.Item
+                                    name="tel"
+                                    label="Tél"
+                                >
+                                    <Input size='large' placeholder="+243" style={{width:'100%'}}/>
+                                </Form.Item>
+                            </Col>
+
+                            <Col xs={24} md={8}>
+                                <Form.Item
+                                    name="email"
+                                    label="Email"
+                                >
+                                    <Input size='large' placeholder="xxx@gmail.com" style={{width:'100%'}}/>
+                                </Form.Item>
+                            </Col>
+
+                            <Col xs={24} md={8}>
+                                <Form.Item
+                                    name="logo"
+                                    label="Logo"
+                                >
+                                    <Upload  
+                                        accept="image/*"
+                                        listType="picture-card"
+                                        onChange={handleUploadChange}
+                                        beforeUpload={() => false} 
+                                    >
+                                        <Button icon={<UploadOutlined style={{margin:0}} />}></Button>
+                                    </Upload>
+                                </Form.Item>
+                            </Col>
+
+                            <Col xs={24}>
+                                <Form.Item>
+                                    <Space className="button-group">
+                                        <Button type="primary" htmlType="submit" loading={loading} disabled={loading}>
+                                            {'Ajouter'}
+                                        </Button>
+                                        <Button htmlType="reset">
+                                            Réinitialiser
+                                        </Button>
+                                    </Space>
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Card>
             </div>
         </div>
 
