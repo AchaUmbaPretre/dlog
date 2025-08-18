77f956134f3f5a1f3d28d0e2176e76df9879a002
AchaUmbaPretre <achandambi@gmail.com>
2025-08-18 14:17:55 +0100

    commit

--- src/pages/charroi/affectationDemande/bandeSortie/rapportBs/performance_op/Performance_op.jsx
+++ src/pages/charroi/affectationDemande/bandeSortie/rapportBs/performance_op/Performance_op.jsx
@@ -1,9 +1,87 @@
-import { Card, Divider, Tabs } from 'antd'
+import { Card, Typography, Space, Table } from 'antd'
 import { useState } from 'react';
+
+const { Text } = Typography;
 
 const Performance_op = () => {
   const [data, setData] = useState([]);
-  
+
+  const columns = [
+    { title: '#', dataIndex: 'id', key: 'id', render: (text, record, index) => index + 1, width: "3%" },
+    {
+      title: 'Immatriculation', key: 'immatriculation', 
+      render: (text, record) => (
+        <Space>
+          <Text type='secondary'>{text}</Text>
+        </Space>
+      )
+    },
+    {
+      title: 'Marque', key: 'nom_marque', 
+      render: (text, record) => (
+        <Space>
+          <Text type='secondary'>{text}</Text>
+        </Space>
+      )
+    },
+    {
+      title: 'Nbre moyen', key: 'nbre_moyen', 
+      render: (text, record) => (
+        <Space>
+          <Text type='secondary'>{text}</Text>
+        </Space>
+      )
+    }
+  ]
+
+  const ChauffeurColumns = [
+    { title: '#', dataIndex: 'id', key: 'id', render: (text, record, index) => index + 1, width: "3%" },
+    {
+      title: 'Nom', dataIndex: 'nom_chauffeur', key: 'nom_chauffeur', 
+      render: (text, record) => (
+        <Space>
+          <Text type='secondary'>{text}</Text>
+        </Space>
+      )
+    },
+    {
+      title: 'Nbre moyen', key: 'nbre_moyen', 
+      render: (text, record) => (
+        <Space>
+          <Text type='secondary'>{text}</Text>
+        </Space>
+      )
+    }
+  ]
+
+  const MoyenDureeColumns = [
+    { title: '#', dataIndex: 'id', key: 'id', render: (text, record, index) => index + 1, width: "3%" },
+    {
+      title: 'Destination', dataIndex: 'nom_destination', key: 'nom_destination', 
+      render: (text, record) => (
+        <Space>
+          <Text type='secondary'>{text}</Text>
+        </Space>
+      )
+    },
+    {
+      title: 'Date', dataIndex: 'Date', key: 'date',
+      render: (text, record) => (
+        <Space>
+          <Text type='secondary'>{text}</Text>
+        </Space>
+      )
+    },
+    {
+      title: 'Nbre moyen', key: 'nbre_moyen', 
+      render: (text, record) => (
+        <Space>
+          <Text type='secondary'>{text}</Text>
+        </Space>
+      )
+    }
+  ]
+
   return (
     <>
       <div className="rapport_bs">
@@ -11,19 +89,19 @@ const Performance_op = () => {
           <Card  type="inner" title="Performance opérationnelle" className="rapport_bs_globals">
             <div className="rapport_bs_global">
               <Card type="inner" title="Nombre moyen de sorties par véhicule">
-                  
+                <Table dataSource={data} columns={columns} />
               </Card>
 
               <Card type="inner" title="Nombre moyen de sorties par chauffeur">
-                  
+                <Table dataSource={data} columns={ChauffeurColumns} />
               </Card>
 
               <Card type="inner" title="Durée moyenne d’une course">
-                  
+                <Table dataSource={data} columns={columns} />
               </Card>
 
               <Card type="inner" title="Durée totale cumulée des courses">
-                  
+                <Table dataSource={data} columns={columns} />
               </Card>
             </div>
           </Card>
