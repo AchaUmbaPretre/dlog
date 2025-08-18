6308e3362291a3b31e7bb8df36ec90c4f1c8cdd2
AchaUmbaPretre <achandambi@gmail.com>
2025-08-18 13:55:41 +0100

    commit

--- src/pages/charroi/affectationDemande/bandeSortie/rapportBs/performance_op/Performance_op.jsx
+++ src/pages/charroi/affectationDemande/bandeSortie/rapportBs/performance_op/Performance_op.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { Card, Divider, Tabs } from 'antd'
+import { useState } from 'react';
 
 const Performance_op = () => {
+  const [data, setData] = useState([]);
+  
   return (
-    <div>Performance_op</div>
+    <>
+      <div className="rapport_bs">
+        <div className="rapport_bs_wrapper">
+          <Card  type="inner" title="Performance opérationnelle" className="rapport_bs_globals">
+            <div className="rapport_bs_global">
+              <Card type="inner" title="Nombre moyen de sorties par véhicule">
+                  
+              </Card>
+
+              <Card type="inner" title="Nombre moyen de sorties par chauffeur">
+                  
+              </Card>
+
+              <Card type="inner" title="Durée moyenne d’une course">
+                  
+              </Card>
+
+              <Card type="inner" title="Durée totale cumulée des courses">
+                  
+              </Card>
+            </div>
+          </Card>
+        </div>
+      </div>
+    </>
   )
 }
 
