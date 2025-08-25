34c0642061f9676760294ec878d4f5c41606f637
AchaUmbaPretre <achandambi@gmail.com>
2025-08-25 14:36:57 +0100

    commit

--- src/pages/charroi/affectationDemande/bandeSortie/rapportBs/mouvementVehicule/MouvementVehicule.jsx
+++ src/pages/charroi/affectationDemande/bandeSortie/rapportBs/mouvementVehicule/MouvementVehicule.jsx
@@ -1,38 +1,89 @@
-import { Select } from 'antd';
-import './mouvementVehicule.scss'
-import { useState } from 'react';
-import MouvementFilter from './mouvementFilter/MouvementFilter';
-const { Option } = Select;
-
+import { Card, Row, Col, Statistic, Tooltip } from "antd";
+import {
+  CarOutlined,
+  FileTextOutlined,
+  CheckCircleOutlined,
+} from "@ant-design/icons";
+import MouvementFilter from "./mouvementFilter/MouvementFilter";
+import "./mouvementVehicule.scss";
 
 const MouvementVehicule = () => {
-
-
   return (
-    <>
-      <div className="mouvement_vehicule">
-        <div className="mouv_vehicule_wrapper">
-          <MouvementFilter/>
-          <div className="mouv_vehicule_row">
-            <div className="mouv_vehicule_card">
-              <span>5</span>
-              <span>Bons en attente</span>
-            </div>
+    <div className="mouvement_vehicule">
+      <div className="mouv_vehicule_wrapper">
+        {/* Filtres */}
+        <MouvementFilter />
 
-            <div className="mouv_vehicule_card">
-              <span>4</span>
-              <span>Véhicules hors site</span>
-            </div>
+        {/* Statistiques */}
+        <Row gutter={16} className="mouv_vehicule_row">
+          <Col xs={24} sm={8}>
+            <Tooltip title="Nombre total de bons en attente de validation">
+              <Card bordered={false} className="mouv_vehicule_card attente">
+                <Statistic
+                  title="Bons en attente"
+                  value={5}
+                  prefix={<FileTextOutlined />}
+                />
+              </Card>
+            </Tooltip>
+          </Col>
 
-            <div className="mouv_vehicule_card">
-              <span>2</span>
-              <span>Véhicules disponibles</span>
-            </div>
+          <Col xs={24} sm={8}>
+            <Tooltip title="Véhicules actuellement hors site">
+              <Card bordered={false} className="mouv_vehicule_card hors_site">
+                <Statistic
+                  title="Véhicules hors site"
+                  value={4}
+                  prefix={<CarOutlined />}
+                />
+              </Card>
+            </Tooltip>
+          </Col>
+
+          <Col xs={24} sm={8}>
+            <Tooltip title="Véhicules disponibles sur site">
+              <Card bordered={false} className="mouv_vehicule_card dispo">
+                <Statistic
+                  title="Disponibles"
+                  value={2}
+                  prefix={<CheckCircleOutlined />}
+                />
+              </Card>
+            </Tooltip>
+          </Col>
+        </Row>
+
+        <h3 className="mouv_h3">Compteur absolu</h3>
+        <div className="cards">
+          <div className="card">
+            <span>10</span>
+            <span>Bons validé</span>
           </div>
+
+          <div className="card">
+            <span>10</span>
+            <span>Départs effectués</span>
+          </div>
+
+          <div className="card">
+            <span>10</span>
+            <span>Retours confirmés</span>
+          </div>
+
+          <div className="card">
+            <span>10</span>
+            <span>Départs hors timing</span>
+          </div>
+
+          <div className="card">
+            <span>10</span>
+            <span>Retour hors timing</span>
+          </div>
+
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default MouvementVehicule
+export default MouvementVehicule;
