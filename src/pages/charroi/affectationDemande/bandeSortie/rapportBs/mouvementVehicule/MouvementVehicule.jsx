b49d6dc86880d3fb4b48c6d9a4db6cea919d1843
AchaUmbaPretre <achandambi@gmail.com>
2025-08-25 14:04:30 +0100

    commit

--- src/pages/charroi/affectationDemande/bandeSortie/rapportBs/mouvementVehicule/MouvementVehicule.jsx
+++ src/pages/charroi/affectationDemande/bandeSortie/rapportBs/mouvementVehicule/MouvementVehicule.jsx
@@ -13,6 +13,22 @@ const MouvementVehicule = () => {
       <div className="mouvement_vehicule">
         <div className="mouv_vehicule_wrapper">
           <MouvementFilter/>
+          <div className="mouv_vehicule_row">
+            <div className="mouv_vehicule_card">
+              <span>5</span>
+              <span>Bons en attente</span>
+            </div>
+
+            <div className="mouv_vehicule_card">
+              <span>4</span>
+              <span>Véhicules hors site</span>
+            </div>
+
+            <div className="mouv_vehicule_card">
+              <span>2</span>
+              <span>Véhicules disponibles</span>
+            </div>
+          </div>
         </div>
       </div>
     </>
