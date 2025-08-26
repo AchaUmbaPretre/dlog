e2a44d84e5abccf9f00e89e3d55fefd50978c105
AchaUmbaPretre <achandambi@gmail.com>
2025-08-26 16:22:31 +0100

    commit

--- src/pages/inspectionGen/inspectionRapport/volumeGlobal/VolumeGlobal.jsx
+++ src/pages/inspectionGen/inspectionRapport/volumeGlobal/VolumeGlobal.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { getRapportInspectionRep } from '../../../../services/rapportService';
 
 const VolumeGlobal = () => {
     const [data, setData] = useState([]);
 
     const fetchData = () => {
         try {
-            
+            const { data } = await getRapportInspectionRep()
+            setData(data)
         } catch (error) {
             console.log(error)
         }
-    }
+    };
+
+    useEffect(()=> {
+        fetchData();
+    }, [])
+
+    
 
   return (
     <>
