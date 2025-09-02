a705f76b4d4e781c97e3e79ff5c33830d48bea8c
AchaUmbaPretre <achandambi@gmail.com>
2025-09-02 14:56:37 +0100

    commit

--- src/pages/modeTv/modeTvCardPonct/ModeTvCardPonct.jsx
+++ src/pages/modeTv/modeTvCardPonct/ModeTvCardPonct.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Progress } from 'antd';
 import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
 import './modeTvCardPonct.scss';
