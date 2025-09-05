d8c4bdefce2aa41dee3b8518e85eca9662f87d0a
AchaUmbaPretre <achandambi@gmail.com>
2025-09-05 11:27:16 +0100

    commit

--- src/pages/charroi/rapportCharroi/RapportCharroi.jsx
+++ src/pages/charroi/rapportCharroi/RapportCharroi.jsx
@@ -1,8 +1,62 @@
+import { useState } from 'react';
 import './rapportCharroi.scss'
+import { Tabs } from 'antd';
+import { ApartmentOutlined } from '@ant-design/icons';
 
 const RapportCharroi = () => {
+    const [activeKey, setActiveKey] = useState(['1', '2']);
+    
+    const handleTabChange = (key) => {
+        setActiveKey(key);
+    };
+
   return (
-    <div>RapportCharroi</div>
+    <>
+        <div className="rapport_charroi">
+            <div className="rapport_charroi_wrapper">
+                <Tabs
+                    activeKey={activeKey[0]}
+                    onChange={handleTabChange}
+                    type="card"
+                    tabPosition="top"
+                    renderTabBar={(props, DefaultTabBar) => <DefaultTabBar {...props} />}
+                >
+                    <Tabs.TabPane
+                        tab={
+                        <span>
+                            <ApartmentOutlined style={{ color: '#52c41a' }} /> Liste des bureaux
+                        </span>
+                        }
+                        key="1"
+                    >
+                        BS VALIDE
+                    </Tabs.TabPane>
+
+                    <Tabs.TabPane
+                        tab={
+                        <span>
+                            <ApartmentOutlined style={{ color: '#52c41a' }} /> Liste des bureaux
+                        </span>
+                        }
+                        key="2"
+                    >
+                        VEHICULE EN COURSE
+                    </Tabs.TabPane>
+
+                    <Tabs.TabPane
+                        tab={
+                        <span>
+                            <ApartmentOutlined style={{ color: '#52c41a' }} /> Liste des bureaux
+                        </span>
+                        }
+                        key="3"
+                    >
+                        UTILITAIRE
+                    </Tabs.TabPane>
+                </Tabs>
+            </div>
+        </div>
+    </>
   )
 }
 
