3f9564fe1f85fa09c8b0646ea47fde4fb9da5ed4
AchaUmbaPretre <achandambi@gmail.com>
2025-09-05 12:08:30 +0100

    commit

---
+++ src/pages/charroi/rapportCharroi/rapportVehiculeValide/RapportVehiculeValide.jsx
@@ -0,0 +1,13 @@
+import './rapportVehiculeValide.scss'
+
+const RapportVehiculeValide = () => {
+  return (
+    <>
+        <div className="rapportVehiculeValide">
+            rapportVehiculeValide
+        </div>
+    </>
+  )
+}
+
+export default RapportVehiculeValide
