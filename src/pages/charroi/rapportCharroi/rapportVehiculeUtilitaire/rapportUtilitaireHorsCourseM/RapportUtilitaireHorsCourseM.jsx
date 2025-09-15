3ff07b7f129ccef57a0d33f3b03facfee124b579
AchaUmbaPretre <achandambi@gmail.com>
2025-09-15 11:11:54 +0100

    commit

---
+++ src/pages/charroi/rapportCharroi/rapportVehiculeUtilitaire/rapportUtilitaireHorsCourseM/RapportUtilitaireHorsCourseM.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RapportUtilitaireHorsCourseM = () => {
+  return (
+    <div>RapportUtilitaireHorsCourseM</div>
+  )
+}
+
+export default RapportUtilitaireHorsCourseM
