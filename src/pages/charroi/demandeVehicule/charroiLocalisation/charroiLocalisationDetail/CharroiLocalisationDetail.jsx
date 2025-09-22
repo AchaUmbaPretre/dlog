99bf94b5bd0ea88ab5510dadaaf2469929e6faff
AchaUmbaPretre <achandambi@gmail.com>
2025-09-22 12:50:24 +0100

    commit

--- src/pages/charroi/demandeVehicule/charroiLocalisation/charroiLocalisationDetail/CharroiLocalisationDetail.jsx
+++ src/pages/charroi/demandeVehicule/charroiLocalisation/charroiLocalisationDetail/CharroiLocalisationDetail.jsx
@@ -3,7 +3,28 @@ import './charroiLocalisationDetail.scss'
 
 const CharroiLocalisationDetail = ({id}) => {
   return (
-    <div>CharroiLocalisationDetail {id}</div>
+    <>
+        <div className="charroi_local_detail">
+            <div className="charroi_top">
+                <div className="charroi_top_left">
+                    <h3 className="charroi_h3">Toyota Hilux 001</h3>
+                    <span className="charroi_desc">Av, colonel Mondjiba, Kintambo</span>
+                </div>
+                <div className="charroi_top_right">
+                    <h3 className="statut_h3">ONLINE</h3>
+                    <span className="charroi_desc">Dernier signal :  il y a 35 sec</span>
+                </div>
+            </div>
+            <div className="charroi_local">
+                <div className="charroi_local_left">
+
+                </div>
+                <div className="charroi_local_right">
+
+                </div>
+            </div>
+        </div>
+    </>
   )
 }
 
