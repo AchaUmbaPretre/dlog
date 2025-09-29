c81480555abcc8ee599ae63c5a6ad5519eecad19
AchaUmbaPretre <achandambi@gmail.com>
2025-09-29 12:52:04 +0100

    commit

--- src/config.js
+++ src/config.js
@@ -8,7 +8,9 @@ const config = {
     
 REACT_APP_SERVER_DOMAIN : 'https://apidlog.loginsmart-cd.com' ,
 
-API_KEY : 'f7c5292b587d4fff9fb1d00f3b6f3f73'
+API_KEY : 'f7c5292b587d4fff9fb1d00f3b6f3f73',
+
+api_hash : '$2y$10$FbpbQMzKNaJVnv0H2RbAfel1NMjXRUoCy8pZUogiA/bvNNj1kdcY.'
 
 /*   REACT_APP_SERVER_DOMAIN : 'https://apidlog.loginsmart-cd.com'
  */ };
--- src/pages/charroi/demandeVehicule/charroiLocalisation/getEventLocalisation/GetEventLocalisation.jsx
+++ src/pages/charroi/demandeVehicule/charroiLocalisation/getEventLocalisation/GetEventLocalisation.jsx
@@ -1,12 +1,26 @@
 import './getEventLocalisation.scss'
 import { useEffect, useState } from 'react';
 import { Select, DatePicker } from 'antd';
+import config from '../../../../../config';
 
 const { RangePicker } = DatePicker;
 
 const GetEventLocalisation = () => {
   const [dateRange, setDateRange] = useState([]);
   const [pagination, setPagination] = useState({ current: 1, pageSize: 20 });
+  const apiHash = config.api_hash;
+
+  const fetchData = async() => {
+    try {
+        const { data } = axios.get(`http://falconeyesolutions.com/api/get_events?date_from=2025-09-28%2000:00:00&date_to=2025-09-29%2023:59:59&lang=fr&limit=1000&user_api_hash=${apiHash  }`)
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+  useEffect(()=> {
+    fetchData()
+  }, []);
 
   const column = [
     {
