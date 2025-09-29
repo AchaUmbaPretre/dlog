d71d1d9d19ae72a5e20a274a3b36297c9a43696f
AchaUmbaPretre <achandambi@gmail.com>
2025-09-29 12:20:58 +0100

    commit

--- src/pages/charroi/demandeVehicule/charroiLocalisation/getEventLocalisation/GetEventLocalisation.jsx
+++ src/pages/charroi/demandeVehicule/charroiLocalisation/getEventLocalisation/GetEventLocalisation.jsx
@@ -5,29 +5,31 @@ import { Select, DatePicker } from 'antd';
 const { RangePicker } = DatePicker;
 
 const GetEventLocalisation = () => {
-    const [dateRange, setDateRange] = useState([]);
-    
+  const [dateRange, setDateRange] = useState([]);
+
   return (
     <>
-        <div className="event_container">
-            <div className="event_wrapper">
-                <div className="event_top">
-                <RangePicker
-                    style={{ width: '100%' }}
-                    value={dateRange}
-                    onChange={setDateRange}
-                    allowClear
-                    format="DD/MM/YYYY"
-                    placeholder={['Date début', 'Date fin']}
-                />
-                </div>
-                <div className="event_bottom">
-
-                </div>
-            </div>
+      <div className="event_container">
+        <div className="event_wrapper">
+          <div className="event_top">
+            <RangePicker
+              style={{ width: '100%' }}
+              value={dateRange}
+              onChange={setDateRange}
+              allowClear
+              // ✅ Affiche les heures + minutes
+              showTime={{ format: 'HH:mm' }}
+              format="DD/MM/YYYY HH:mm"
+              placeholder={['Date et heure début', 'Date et heure fin']}
+            />
+          </div>
+          <div className="event_bottom">
+            {/* Tu pourras afficher la sélection ici */}
+          </div>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default GetEventLocalisation
+export default GetEventLocalisation;
