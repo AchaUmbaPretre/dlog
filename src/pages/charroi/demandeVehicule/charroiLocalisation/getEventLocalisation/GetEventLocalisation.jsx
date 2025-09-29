59a970fd84ac67886b8a20e658c85eaca19475cf
AchaUmbaPretre <achandambi@gmail.com>
2025-09-29 12:19:33 +0100

    commit

--- src/pages/charroi/demandeVehicule/charroiLocalisation/getEventLocalisation/GetEventLocalisation.jsx
+++ src/pages/charroi/demandeVehicule/charroiLocalisation/getEventLocalisation/GetEventLocalisation.jsx
@@ -1,12 +1,25 @@
 import './getEventLocalisation.scss'
+import { useEffect, useState } from 'react';
+import { Select, DatePicker } from 'antd';
+
+const { RangePicker } = DatePicker;
 
 const GetEventLocalisation = () => {
+    const [dateRange, setDateRange] = useState([]);
+    
   return (
     <>
         <div className="event_container">
             <div className="event_wrapper">
                 <div className="event_top">
-                    
+                <RangePicker
+                    style={{ width: '100%' }}
+                    value={dateRange}
+                    onChange={setDateRange}
+                    allowClear
+                    format="DD/MM/YYYY"
+                    placeholder={['Date début', 'Date fin']}
+                />
                 </div>
                 <div className="event_bottom">
 
