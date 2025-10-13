691ff71952e80c86de662e5f544fa585d6b28734
AchaUmbaPretre <achandambi@gmail.com>
2025-10-13 14:46:45 +0100

    commit

--- src/pages/charroi/monitoring/Monitoring.jsx
+++ src/pages/charroi/monitoring/Monitoring.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Table, Button, Image, Tabs, Input, message, Dropdown, Menu, Space, Tooltip, Popconfirm, Tag, Modal, notification } from 'antd';
+import RapportEvent from '../demandeVehicule/charroiLocalisation/rapportEvent/RapportEvent';
+
 
 const Monitoring = () => {
-    
-  return (
-    <div>
+    const [activeKey, setActiveKey] = useState(['1', '2']);
 
-    </div>
+    const handleTabChange = (key) => {
+        setActiveKey(key);
+    }
+
+  return (
+    <>
+        <Tabs
+            activeKey={activeKey[0]}
+            onChange={handleTabChange}
+            type="card"
+            tabPosition="top"
+            renderTabBar={(props, DefaultTabBar) => <DefaultTabBar {...props} />}
+        >
+            <Tabs.TabPane
+                tab={
+                    <span>
+                        Position
+                    </span>
+                }
+                key="1"
+            >
+{/*                 <BonSortiePerso/>
+ */}        </Tabs.TabPane>
+
+            <Tabs.TabPane
+                tab={
+                        <span>
+                            Evénements
+                        </span>
+                    }
+                    key="2"
+                >
+            </Tabs.TabPane>
+
+            <Tabs.TabPane
+                tab={
+                    <span>
+                        Rapport véhicule
+                    </span>
+                    }
+                    key="3"
+                >
+                <RapportEvent/>
+            </Tabs.TabPane>
+        </Tabs>
+    </>
   )
 }
 
