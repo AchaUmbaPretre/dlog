52da248931a0eb8d38979ea6665954842e3ce051
AchaUmbaPretre <achandambi@gmail.com>
2025-10-21 12:20:08 +0100

    commit

--- src/pages/charroi/monitoring/rapportMoniUtilitaire/RapportMoniUtilitaire.jsx
+++ src/pages/charroi/monitoring/rapportMoniUtilitaire/RapportMoniUtilitaire.jsx
@@ -9,19 +9,19 @@ const RapportMoniUtilitaire = () => {
 
 
         const fetchData = async () => {
-        try {
-            const falconData = await getFalcon();
-            const items = falconData.data[0].items || [];
-            setData(items);
-            setLoading(false);
-        } catch (error) {
-            console.error("Erreur fetchData:", error);
-            notification.error({
-            message: 'Erreur de chargement',
-            description: 'Impossible de charger les données véhicules.',
-            });
-            setLoading(false);
-        }
+            try {
+                const falconData = await getFalcon();
+                const items = falconData.data[0].items || [];
+                setData(items);
+                setLoading(false);
+            } catch (error) {
+                console.error("Erreur fetchData:", error);
+                notification.error({
+                message: 'Erreur de chargement',
+                description: 'Impossible de charger les données véhicules.',
+                });
+                setLoading(false);
+            }
         };
     
         useEffect(() => {
