2709f90c47bb3f5aa6368c275fed9d8df97c6379
AchaUmbaPretre <achandambi@gmail.com>
2025-10-21 12:19:38 +0100

    commit

--- src/pages/charroi/monitoring/rapportMoniUtilitaire/RapportMoniUtilitaire.jsx
+++ src/pages/charroi/monitoring/rapportMoniUtilitaire/RapportMoniUtilitaire.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './rapportMoniUtilitaire.scss'
+import { getFalcon } from '../../../../services/rapportService';
+import { notification } from 'antd';
 
 const RapportMoniUtilitaire = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+
+        const fetchData = async () => {
+        try {
+            const falconData = await getFalcon();
+            const items = falconData.data[0].items || [];
+            setData(items);
+            setLoading(false);
+        } catch (error) {
+            console.error("Erreur fetchData:", error);
+            notification.error({
+            message: 'Erreur de chargement',
+            description: 'Impossible de charger les données véhicules.',
+            });
+            setLoading(false);
+        }
+        };
+    
+        useEffect(() => {
+            fetchData();
+        }, []);
+
   return (
     <div>RapportMoniUtilitaire</div>
   )
