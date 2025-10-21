932b40710a4ac11d07642af18b714a5316bc2a9a
AchaUmbaPretre <achandambi@gmail.com>
2025-10-21 10:57:29 +0100

    commit

---
+++ src/pages/charroi/monitoring/rapportMoniUtilitaire/RapportMoniUtilitaire.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './rapportMoniUtilitaire.scss'
+
+const RapportMoniUtilitaire = () => {
+  return (
+    <div>RapportMoniUtilitaire</div>
+  )
+}
+
+export default RapportMoniUtilitaire
