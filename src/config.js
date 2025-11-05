d4072ae7b652dd713c174337b886647dd51f35b7
AchaUmbaPretre <achandambi@gmail.com>
2025-11-05 12:29:14 +0100

    commit

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const TOKEN = currentUser?.accessToken;
 
 const config = {
     
-REACT_APP_SERVER_DOMAIN : 'https://apidlog.loginsmart-cd.com',
+REACT_APP_SERVER_DOMAIN : 'http://localhost:8080',
 
 API_KEY : 'f7c5292b587d4fff9fb1d00f3b6f3f73',
 
@@ -14,7 +14,7 @@ api_hash : '$2y$10$FbpbQMzKNaJVnv0H2RbAfel1NMjXRUoCy8pZUogiA/bvNNj1kdcY.'
 
 /*   REACT_APP_SERVER_DOMAIN : 'https://apidlog.loginsmart-cd.com'
  */ };
-  
+
 export default config;
 
 export const userRequest = axios.create({
