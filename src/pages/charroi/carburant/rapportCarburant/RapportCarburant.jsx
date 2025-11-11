4ffb130c630d47fbb14777b8833378b26e5e3f5d
AchaUmbaPretre <achandambi@gmail.com>
2025-11-11 15:26:59 +0100

    commit

--- src/pages/charroi/carburant/rapportCarburant/RapportCarburant.jsx
+++ src/pages/charroi/carburant/rapportCarburant/RapportCarburant.jsx
@@ -6,9 +6,11 @@ const RapportCarburant = () => {
         <div className="rapport__carburant">
             <div className="rapport__carburant__container">
                 <h1 className="rapport__carburant__title">Rapport de gestion du carburant</h1>
-                <div className="rapport__resume">
-                    <div className="rapport__resume_rows">
-                        <div className="rapport__resume_container">
+                <div className="rapport__carburant__wrapper">
+                    <div className="rapport__resume">
+                        <h2 className="rapport_title">Résume</h2>
+                        <div className="rapport__resume_rows">
+                            <div className="rapport__resume_container"></div>
                             <div className="rapport__resume_row">
                                 <span className="rapport_carburant_desc">Total des pleins enregistrés</span>
                                 <strong>50</strong>
@@ -34,6 +36,27 @@ const RapportCarburant = () => {
                                 <strong>28,5 L/100km</strong>
                             </div>
                         </div>
+                    </div>
+
+                    <div className="rapport__carburant_graphique">
+                        <h2 className="rapport_title">Graphiques</h2>
+                        <div className="rapport_graphique_rows">
+                            <div className="rapport_graphique_row">
+                                <h3 className="rapport__soustitle">Consommation par vehicule</h3>
+                            </div>
+
+                            <div className="rapport_graphique_row">
+                                <h3 className="rapport__soustitle">Evolution du cout par semaine</h3>
+                            </div>
+
+                            <div className="rapport_graphique_row">
+                                <h3 className="rapport__soustitle">Repartition du carburant par cat. vehicule</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="rapport__detail_vehicule">
+                        <h2 className="rapport_title">Détail par véhicule</h2>
                     </div>
                 </div>
             </div>
