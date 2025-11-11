5db3f4ea5ab7cefb5cd62628323b0ddf0e8e069f
AchaUmbaPretre <achandambi@gmail.com>
2025-11-11 10:30:23 +0100

    commit

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const TOKEN = currentUser?.accessToken;
 
 const config = {
     
-REACT_APP_SERVER_DOMAIN : 'http://localhost:8080',
+REACT_APP_SERVER_DOMAIN : 'https://apidlog.loginsmart-cd.com ',
 
 API_KEY : 'f7c5292b587d4fff9fb1d00f3b6f3f73',
 
