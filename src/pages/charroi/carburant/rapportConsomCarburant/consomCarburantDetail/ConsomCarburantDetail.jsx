0b0a9afd627890fdd422b8edf5c54b65bca9607a
AchaUmbaPretre <achandambi@gmail.com>
2025-11-14 13:13:00 +0100

    commit

--- src/pages/charroi/carburant/rapportConsomCarburant/consomCarburantDetail/ConsomCarburantDetail.jsx
+++ src/pages/charroi/carburant/rapportConsomCarburant/consomCarburantDetail/ConsomCarburantDetail.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import ConsomDetailSite from './consomDetailSite/ConsomDetailSite';
 import ConsomDetailVehicule from './consomDetailVehicule/ConsomDetailVehicule';
 
+const views = {
+  mesSites: <ConsomDetailSite />,
+  mesVehicules: <ConsomDetailVehicule />,
+};
+
 const ConsomCarburantDetail = ({ spectreValue }) => {
   return (
     <div className="consomCarburantDetail">
-      {spectreValue === 'mesSites' ? (
-        <ConsomDetailSite />
-      ) : (
-        <ConsomDetailVehicule />
-      )}
+      {views[spectreValue] || <ConsomDetailVehicule />}
     </div>
   );
 };
