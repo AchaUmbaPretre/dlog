b24f9dc81b3cd22eecec946ae4a03f35ba5bfa09
AchaUmbaPretre <achandambi@gmail.com>
2025-11-14 11:29:17 +0100

    commit

--- src/pages/charroi/carburant/rapportConsomCarburant/consomCarburantChart/ConsomCarburantChart.jsx
+++ src/pages/charroi/carburant/rapportConsomCarburant/consomCarburantChart/ConsomCarburantChart.jsx
@@ -3,11 +3,19 @@ import { Divider, Card, Table, Tag, Tooltip } from 'antd';
 import './consomCarburantChart.scss';
 
 const ConsomCarburantChart = () => {
-    
+
   return (
     <>
         <div className="consomCarburantChart">
             <Divider>Consommation mensuelle</Divider>
+            <div className="consomCarburant_chart_wrapper">
+                <div className="consom_line">
+
+                </div>
+                <div className="consom_bar">
+                    
+                </div>
+            </div>
         </div>
     </>
   )
