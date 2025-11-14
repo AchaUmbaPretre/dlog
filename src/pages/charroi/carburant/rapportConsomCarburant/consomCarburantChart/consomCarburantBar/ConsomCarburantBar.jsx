e2f90729c8d23fab63a591b5b6b80d3e5281ac07
AchaUmbaPretre <achandambi@gmail.com>
2025-11-14 12:18:03 +0100

    commit

--- src/pages/charroi/carburant/rapportConsomCarburant/consomCarburantChart/consomCarburantBar/ConsomCarburantBar.jsx
+++ src/pages/charroi/carburant/rapportConsomCarburant/consomCarburantChart/consomCarburantBar/ConsomCarburantBar.jsx
@@ -1,8 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Bar } from 'react-chartjs-2';
+import { Card } from 'antd';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+// Enregistrer les composants nécessaires pour Chart.js
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const ConsomCarburantBar = () => {
+    const [datas, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    // Organiser les données récupérées pour les afficher sur le graphique
+    const prepareChartData = () => {
+    const dieselData = Array(12).fill(0); // Initialiser un tableau pour Diesel
+    const essenceData = Array(12).fill(0); // Initialiser un tableau pour Essence
+
+    // Remplir les données par type de carburant et par mois
+    datas.forEach(({ annee, mois, total_conso, nom_type_carburant }) => {
+      const moisIndex = mois - 1; // Pour avoir un index de 0 à 11 (janvier = index 0, décembre = index 11)
+      if (nom_type_carburant === 'Diesel') {
+        dieselData[moisIndex] += total_conso;
+      } else if (nom_type_carburant === 'Essence') {
+        essenceData[moisIndex] += total_conso;
+      }
+    });
+
+    return {
+      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], // Mois de l'année
+      datasets: [
+        {
+          label: 'Diesel',
+          data: dieselData,
+          backgroundColor: '#1a73e8',
+          borderColor: '#1a73e8',
+          borderWidth: 1,
+        },
+        {
+          label: 'Essence',
+          data: essenceData,
+          backgroundColor: 'rgb(255, 99, 132)', // Couleur des barres pour l'essence
+          borderColor: '#fbbc04',
+          borderWidth: 1,
+        },
+      ],
+    };
+  };
+
+  const data = prepareChartData();
+
+  // Options du diagramme
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top', // Position de la légende
+      },
+      title: {
+        display: true,
+        text: 'Consommation Mensuelle: Diesel vs Essence', // Titre du diagramme
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false, // Supprime les lignes de la grille horizontale
+        },
+      },
+      y: {
+        grid: {
+          color: '#e0e0e0', // Couleur des lignes de la grille verticale
+        },
+      },
+    },
+  };
+
   return (
-    <div>ConsomCarburantBar</div>
+    <>
+        <Card>
+            <div className="consomCarburantBar">
+                <Bar data={data} options={options} />
+            </div>
+        </Card>
+    </>
   )
 }
 
--- src/pages/charroi/carburant/rapportConsomCarburant/consomCarburantChart/consomCarburantLine/ConsomCarburantLine.jsx
+++ src/pages/charroi/carburant/rapportConsomCarburant/consomCarburantChart/consomCarburantLine/ConsomCarburantLine.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Card } from 'antd';
 
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 const ConsomCarburantLine = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+      // Transformation des données pour le graphique
+    const chartData = () => {
+        const labels = [...new Set(data.map((item) => item.nom_type_carburant))];
+        const dataValues = labels.map((label) =>
+        data
+            .filter((item) => item.nom_type_carburant === label)
+            .reduce((sum, item) => sum + item.total_conso_litres, 0)
+        );
+
+        return {
+        labels,
+        datasets: [
+            {
+            label: 'Consommation',
+            data: dataValues,
+            backgroundColor: ['#1a73e8', 'rgb(255, 99, 132)', '#ffc107', '#4caf50'], // Ajoutez plus de couleurs si nécessaire
+            hoverOffset: 4,
+            },
+        ],
+        };
+    };
+
+    // Options pour le Doughnut Chart
+    const options = {
+        responsive: true,
+        plugins: {
+        legend: {
+            position: 'top', // Position de la légende
+        },
+        title: {
+            display: true,
+            text: 'Répartition de la Consommation', // Titre du diagramme
+        },
+        tooltip: {
+            callbacks: {
+            label: function (context) {
+                const value = context.raw;
+                const total = context.dataset.data.reduce((a, b) => a + b, 0);
+                const percentage = ((value / total) * 100).toFixed(2) + '%';
+                return `${context.label}: ${percentage}`;
+            },
+            },
+        },
+        },
+    };
+
+
   return (
-    <div>ConsomCarburantLine</div>
+    <>
+        <Card>
+            <div className="consomCarburantLine">
+                <Doughnut data={chartData()} options={options} />
+            </div>
+        </Card>
+    </>
   )
 }
 
