782c0767f729a74112345812147897981b82cf69
AchaUmbaPretre <achandambi@gmail.com>
2025-12-03 16:56:03 +0100

    commit

--- src/pages/charroi/generateur/Generateur.jsx
+++ src/pages/charroi/generateur/Generateur.jsx
@@ -1,50 +1,65 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 import { Tabs } from 'antd';
-import { getTabStyle, iconStyle } from '../../../utils/tabStyles';
 import {
-  ThunderboltOutlined
+  ThunderboltOutlined,
+  DatabaseOutlined,
+  AppstoreOutlined
 } from '@ant-design/icons';
+import { getTabStyle, iconStyle } from '../../../utils/tabStyles';
 import ListGenerateur from './listGenerateur/ListGenerateur';
 import ListTypeGenerateur from './TypeGenerateur/ListTypeGenerateur';
+import PleinGenerateur from './composant/pleinGenerateur/PleinGenerateur';
+
+const tabConfig = [
+  {
+    key: '1',
+    label: 'Liste des générateurs',
+    icon: <ThunderboltOutlined />,
+    component: <ListGenerateur />
+  },
+  {
+    key: '2',
+    label: 'Liste des pleins générateurs',
+    icon: <DatabaseOutlined />,
+    component: <PleinGenerateur />
+  },
+  {
+    key: '3',
+    label: 'Les types',
+    icon: <AppstoreOutlined />,
+    component: <ListTypeGenerateur />
+  }
+];
 
 const Generateur = () => {
-    const [activeKey, setActiveKey] = useState('1');
+  const [activeKey, setActiveKey] = useState('1');
 
   return (
     <div className="carburant_all">
-        <Tabs
-            activeKey={activeKey}
-            onChange={setActiveKey}
-            type="card"
-            tabPosition="top"
-            destroyInactiveTabPane
-            animated
-        >
-            <Tabs.TabPane
-                key="1"
-                tab={
-                    <span style={getTabStyle('1', activeKey)}>
-                    <ThunderboltOutlined style={iconStyle('1', activeKey)} />
-                    Liste des générateurs
-                    </span>
-                }
-            >
-                <ListGenerateur />  
-            </Tabs.TabPane>
-                <Tabs.TabPane
-                    key="2"
-                    tab={
-                        <span style={getTabStyle('2', activeKey)}>
-                            <ThunderboltOutlined style={iconStyle('2', activeKey)} />
-                            Les types
-                        </span>
-                    }
-                >
-                    <ListTypeGenerateur />           
-                </Tabs.TabPane>
-        </Tabs>
+      <Tabs
+        activeKey={activeKey}
+        onChange={setActiveKey}
+        type="card"
+        tabPosition="top"
+        destroyInactiveTabPane
+        animated
+      >
+        {tabConfig.map(tab => (
+          <Tabs.TabPane
+            key={tab.key}
+            tab={
+              <span style={getTabStyle(tab.key, activeKey)}>
+                {React.cloneElement(tab.icon, { style: iconStyle(tab.key, activeKey) })}
+                {tab.label}
+              </span>
+            }
+          >
+            {tab.component}
+          </Tabs.TabPane>
+        ))}
+      </Tabs>
     </div>
-  )
-}
+  );
+};
 
-export default Generateur
+export default Generateur;
---
+++ src/pages/charroi/generateur/composant/pleinGenerateur/PleinGenerateur.jsx
@@ -0,0 +1,226 @@
+import { useEffect, useState, useMemo } from "react";
+import {
+  Table,
+  Button,
+  Dropdown,
+  Input,
+  Space,
+  Typography,
+  Card,
+  Empty,
+  Checkbox,
+  Tooltip
+} from "antd";
+import {
+  EditOutlined,
+  FireOutlined,
+  PlusCircleOutlined,
+  ReloadOutlined,
+  DownOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+
+const { Search } = Input;
+const { Text, Title } = Typography;
+
+const PleinGenerateur = () => {
+ const [pagination, setPagination] = useState({ current: 1, pageSize: 10 });
+  const [loading, setLoading] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [modalType, setModalType] = useState(null);
+  const [data, setData] = useState([]);
+  const [columnsVisibility, setColumnsVisibility] = useState({
+    "#": true,
+    Marque: true,
+    "Code groupe": false,
+    "Type gen.": true,
+    "Type carburant": true,
+    "Date operation": false,
+    "Qté" : true,
+    "Crée par" : false
+  });
+
+/*     const fetchData = async() => {
+        setLoading(true);
+
+        try {
+            const response = await getGenerateur();
+            setData(response?.data || []);
+
+        } catch (error) {
+            notification.error({
+                message: "Erreur de chargement",
+                description: "Impossible de récupérer les données du générateur.",
+                placement: "topRight",
+            });
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []); */
+
+  const closeAllModals = () => setModalType(null);
+  const openModal = (type) => setModalType(type);
+
+  const columnMenu = (
+        <div style={{ padding: 10, background:'#fff' }}>
+        {Object.keys(columnsVisibility).map((colName) => (
+            <div key={colName}>
+            <Checkbox
+                checked={columnsVisibility[colName]}
+                onChange={() =>
+                setColumnsVisibility((prev) => ({
+                    ...prev,
+                    [colName]: !prev[colName],
+                }))
+                }
+            >
+                {colName}
+            </Checkbox>
+            </div>
+        ))}
+        </div>
+    );
+  
+  const columns = useMemo(() => {
+    const allColumns = [
+        {
+            title: "#",
+            key: "index",
+            width: 40,
+            align: "center",
+            render: (_, __, index) => (pagination.current - 1) * pagination.pageSize + index + 1,
+        },
+        {   title:"Code groupe", dataIndex: "code_groupe", key: "code_groupe"},
+        {   title:"Marque", dataIndex: "nom_marque", key: "nom_marque"},
+        {   title:"Modèle", dataIndex: "nom_modele", key: "nom_modèle"},
+        {   title:"Type gen.", dataIndex: "nom_type_gen", key: "nom_type_gen"},
+        {   title:"Type carburant", dataIndex: "nom_type_carburant", key: "nom_type_carburant"},
+        {   title:"Date operation", dataIndex: "date_operation", key: "date_operation"},
+        {   title:"Qté", dataIndex: "quantite_litres", key: "quantite_litres"},
+        {   title:"Crée par", dataIndex: "user_cr", key: "user_cr"},
+        {
+            title: "Actions",
+            key: 'action',
+            width: '10%',
+            render: (text, record) => (
+                <Space size="middle">
+                    <Tooltip title="Modifier">
+                        <Button
+                            icon={<EditOutlined />}
+                            style={{ color: 'green' }}
+                            onClick={() => handleEdit(record.id_tache)}
+                            aria-label="Edit generateur"
+                        />
+                    </Tooltip>   
+                </Space>
+            )
+        }
+
+    ];
+    return allColumns.filter((col) => columnsVisibility[col.title] !== false);
+  }, [pagination, columnsVisibility]);
+
+  const filteredData = useMemo(() => {
+    const search = searchValue.toLowerCase().trim();
+    if(!search) return data;
+    return data.filter(
+        (item) =>
+            item.nom_marque?.toLowerCase().includes(search) || 
+            item.nom_modele?.toLowerCase().includes(search) || 
+            item.nom_type_gen?.toLowerCase().includes(search) || 
+            item.nom_type_carburant?.toLowerCase().includes(search)
+    );
+  }, [data, searchValue]);
+
+  const handleEdit = () => {
+
+  }
+
+  return (
+    <div className="carburant-page">
+        <Card
+        title={
+          <Space>
+            <FireOutlined style={{ color: "#fa541c", fontSize: 22 }} />
+            <Title level={4} style={{ margin: 0 }}>
+              Gestion des pleins générateurs
+            </Title>
+          </Space>
+        }
+        bordered={false}
+        className="shadow-sm rounded-2xl"
+        extra={
+          <Space wrap>
+            <Search
+              placeholder="Recherche..."
+              allowClear
+              onChange={(e) => setSearchValue(e.target.value)}
+              style={{ width: 260 }}
+            />
+            <Button
+              icon={<ReloadOutlined />}
+              loading={loading}
+            >
+              Actualiser
+            </Button>
+            <Button
+              type="primary"
+              icon={<PlusCircleOutlined />}
+              onClick={() => openModal("Add")}
+            >
+              Nouveau
+            </Button>
+            <Dropdown overlay={columnMenu} trigger={["click"]}>
+              <Button icon={<MenuOutlined />}>
+                Colonnes <DownOutlined />
+              </Button>
+            </Dropdown>
+          </Space>
+        }
+      >
+        <Table
+          columns={columns}
+          dataSource={filteredData}
+          rowKey={(record) => record.id || record.key}
+          size="middle"
+          loading={loading}
+          pagination={{
+            ...pagination,
+            showSizeChanger: true,
+            showQuickJumper: true,
+            showTotal: (total) => `${total} enregistrements`,
+          }}
+          onChange={(pagination) => setPagination(pagination)}
+          scroll={{ x: 1100 }}
+          rowClassName={(record, index) => (index % 2 === 0 ? 'odd-row' : 'even-row')}
+          locale={{
+            emptyText: (
+              <Empty
+                description="Aucune donnée disponible"
+                image={Empty.PRESENTED_IMAGE_SIMPLE}
+              />
+            ),
+          }}
+          bordered
+        />
+      </Card>
+
+{/*       <Modal
+        open={modalType === "Add"}
+        onCancel={closeAllModals}
+        footer={null}
+        width={1000}
+        centered
+        destroyOnClose
+      >
+        <GenerateurForm closeModal={closeAllModals} fetchData={fetchData} />
+      </Modal> */}
+    </div>
+  )
+}
+
+export default PleinGenerateur;
