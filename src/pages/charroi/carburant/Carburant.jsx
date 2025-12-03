b0cee87c8648bf8f608b24d4bf7f3a9861c56852
AchaUmbaPretre <achandambi@gmail.com>
2025-12-03 14:25:01 +0100

    commit

--- src/pages/charroi/carburant/Carburant.jsx
+++ src/pages/charroi/carburant/Carburant.jsx
@@ -77,21 +77,21 @@ const Carburant = () => {
 
 const totalKmActuel = useMemo(() => {
   return data.reduce((sum, item) => sum + (item.compteur_km || 0), 0);
-}, [data]);
+}, [data, filteredDatas]);
 
 const totalConsommation = useMemo(() => {
   return data.reduce((sum, item) => sum + (item.consommation || 0), 0);
-}, [data]);
+}, [data, filteredDatas]);
 
 const distanceMoyenne = useMemo(() => {
   if (data.length === 0) return 0;
   const totalDistance = data.reduce((sum, item) => sum + (item.distance || 0), 0);
   return totalDistance / data.length;
-}, [data]);
+}, [data, filteredDatas]);
 
 const montantTotalUsd = useMemo(() => {
   return data.reduce((sum, item) => sum + (item.montant_total_usd || 0), 0);
-}, [data]);
+}, [data, filteredDatas]);
 
 
   const fetchData = async (filteredDatas) => {
