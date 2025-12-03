0460b565cb41434705f0d2dd734f0f59dc17622c
AchaUmbaPretre <achandambi@gmail.com>
2025-12-03 12:48:21 +0100

    commit

--- src/pages/charroi/carburant/carburantDetail/CarburantDetail.jsx
+++ src/pages/charroi/carburant/carburantDetail/CarburantDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { notification, Tooltip, Skeleton, Alert, Button, Divider, Tag } from 'antd';
 import {
   CarOutlined,
