7ab1f5e2aec86cd0cd2c585818cbac52f862d225
AchaUmbaPretre <achandambi@gmail.com>
2025-12-15 14:09:43 +0100

    commit

--- src/pages/charroi/generateur/reparationGenerat/hook/useReparateurGenLoader.js
+++ src/pages/charroi/generateur/reparationGenerat/hook/useReparateurGenLoader.js
@@ -1,11 +1,11 @@
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { getGenerateur } from "../../../../../services/generateurService";
 import { getFournisseur } from "../../../../../services/fournisseurService";
 
 
 export function useReparateurGenLoader() {
-    [loading, setLoading] = useState({ lists: false});
-    [lists, setLists] = useState({ generateurs: [], fournisseur: []});
+    const [loading, setLoading] = useState({ lists: false});
+    const [lists, setLists] = useState({ generateurs: [], fournisseur: []});
     const [error, setError] = useState(null);
     
     const loadLists = useCallback(async () => {
@@ -20,7 +20,7 @@ export function useReparateurGenLoader() {
                 generateurs: geneData?.data || [],
                 fournisseurs: fourniData?.data || []
             })
-        } catch (error) {
+        } catch (err) {
             console.error('useRepGenerateurLoaders.loadLists', err);
             setError(err);
         } finally {
--- src/pages/charroi/generateur/reparationGenerat/reparationGeneratForm/ReparationGeneratForm.jsx
+++ src/pages/charroi/generateur/reparationGenerat/reparationGeneratForm/ReparationGeneratForm.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { Col, DatePicker, Form, Modal, Card, notification, Input, InputNumber, Row, Select, Skeleton, Button, Divider, message } from 'antd';
+import { useReparateurGenLoader } from '../hook/useReparateurGenLoader';
+import { useReparateurGenForm } from '../hook/useReparateurGenForm';
 
 
 const ReparationGeneratForm = ({closeModal, fetchData}) => {
     const [form] = Form.useForm();
     const [loadingData, setLoadingData] = useState(false);
     const [fournisseur, setFournisseur] = useState([]);
-        
+    const { loading, lists } = useReparateurGenForm()
+
     const onFinish = async (values) => {
 
-        }
+    }
+
+    const generateurOptions = useMemo(()=> lists.generateurs.map(v => ({ value: v.id_generateur, label: `${v.nom_marque} / ${v.nom_modele} / ${v.code_groupe}` })), [lists.generateurs])
   return (
     <>
         <div className="controle_form">
