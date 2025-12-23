54e39861a5d4579f9fc034d614ed78905481ddbd
AchaUmbaPretre <achandambi@gmail.com>
2025-12-23 11:19:26 +0100

    commit

--- src/services/sortieEamFmp.js
+++ src/services/sortieEamFmp.js
@@ -5,9 +5,14 @@ const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
 
 export const getSortieEam = async () => {
     return axios.get(`${DOMAIN}/api/sortieEamFmp/eam`);
-  };
+};
 
 export const getSortieFmp = async () => {
     return axios.get(`${DOMAIN}/api/sortieEamFmp/fmp`);
+};
+
+
+export const getReconciliation = async () => {
+    return axios.get(`${DOMAIN}/api/sortieEamFmp/reconciliation`);
 };
 
