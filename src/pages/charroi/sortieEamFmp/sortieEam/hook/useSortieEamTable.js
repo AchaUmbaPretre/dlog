e95a872090a77894df822c32514f0ed1ad55d456
AchaUmbaPretre <achandambi@gmail.com>
2025-12-24 10:20:36 +0100

    commit

--- src/pages/charroi/sortieEamFmp/sortieEam/hook/useSortieEamTable.js
+++ src/pages/charroi/sortieEamFmp/sortieEam/hook/useSortieEamTable.js
@@ -260,8 +260,8 @@ export const useSortieEamTable = ({
             { title: "Bulk issue", dataIndex: "bulk_issue", key: "bulk_issue" },
             { 
               title: "Date trans.", 
-              dataIndex: "transanction_date", 
-              key: "transanction_date",
+              dataIndex: "last_transaction_date", 
+              key: "last_transaction_date",
               sorter: (a,b) => moment(a.transanction_date).unix() - moment(b.transanction_date).unix(),
                 render: (text) => (
                   <Tag icon={<CalendarOutlined />} color="red">
