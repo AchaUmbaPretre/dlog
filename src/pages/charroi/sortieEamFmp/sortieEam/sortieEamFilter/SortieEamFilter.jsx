d3e624e9247958674285aa6d6646696e766c8424
AchaUmbaPretre <achandambi@gmail.com>
2025-12-26 15:18:17 +0100

    commit

--- src/pages/charroi/sortieEamFmp/sortieEam/sortieEamFilter/SortieEamFilter.jsx
+++ src/pages/charroi/sortieEamFmp/sortieEam/sortieEamFilter/SortieEamFilter.jsx
@@ -1,12 +1,52 @@
-import React, { useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { Select, DatePicker, Skeleton, notification } from "antd";
+import { getSortieEam } from '../../../../../services/sortieEamFmp';
 const { RangePicker } = DatePicker;
 
 
 const SortieEamFilter = () => {
       const [dateRange, setDateRange] = useState(null);
       const [loading, setLoading] = useState(false);
+      const [selectedSmr, setSelectedSmr] = useState([]);
+      const [selectedPart, setSelectedPart] = useState([]);
+      const [smr, setSmr] = useState([]);
+
+      useEffect(()=> {
+        const fetchData = async() => {
+            try {
+                const res = await getSortieEam();
+                setSmr(res?.data)
+            } catch (err) {
+                notification.error({
+                    message: "Erreur lors du chargement",
+                    description: err?.response?.data?.message || "Impossible de charger les sorties EAM.",
+                });
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchData();
+      }, []);
     
+    const smrOptions = useMemo(() => {
+    const uniqueSmr = [...new Set(smr.map(item => item.smr_ref))];
+
+    return uniqueSmr.map(smr_ref => ({
+        value: smr_ref,
+        label: smr_ref
+    }));
+    }, [smr]);
+
+    const partOptions = useMemo(() => {
+        const uniqueSmr = [...new Set(smr.map(item => item.part))];
+
+        return uniqueSmr.map(part => ({
+            value: part,
+            label: part 
+        }));
+    }, [smr]);
+
   return (
     <>
         <div className="filterTache" style={{marginBottom:'20px'}}>
@@ -20,9 +60,27 @@ const SortieEamFilter = () => {
 
             <div className="filter_row">
                 <label>SMR :</label>
-                <RangePicker
+                <Select
+                    mode="multiple"
+                    showSearch
+                    optionFilterProp="label"
                     style={{ width: "100%" }}
-                    onChange={setDateRange}
+                    options={smrOptions}
+                    placeholder="Sélectionnez..."
+                    onChange={setSelectedSmr}
+                />
+            </div>
+
+            <div className="filter_row">
+                <label>PART :</label>
+                <Select
+                    mode="multiple"
+                    showSearch
+                    optionFilterProp="label"
+                    style={{ width: "100%" }}
+                    options={partOptions}
+                    placeholder="Sélectionnez..."
+                    onChange={setSelectedSmr}
                 />
             </div>
 
