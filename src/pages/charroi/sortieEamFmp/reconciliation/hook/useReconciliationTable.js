3f1ef595db6e0278edf75e91aeaf2c9a3baaabe8
AchaUmbaPretre <achandambi@gmail.com>
2025-12-29 14:41:02 +0100

    commit

--- src/pages/charroi/sortieEamFmp/reconciliation/hook/useReconciliationTable.js
+++ src/pages/charroi/sortieEamFmp/reconciliation/hook/useReconciliationTable.js
@@ -4,82 +4,71 @@ import getColumnSearchProps from "../../../../../utils/columnSearchUtils";
 
 const { Text } = Typography;
 
-export const useReconciliationTable = ({ pagination, columnsVisibility }) => {
-    const [searchText, setSearchText] = useState('');
-    const [searchedColumn, setSearchedColumn] = useState('');
-    const searchInput = useRef(null);
+export const useReconciliationTable = ({ columnsVisibility, searchText, setSearchText }) => {
+  const searchInput = useRef(null);
 
-    return useMemo(() => {
-        const allColumns = [
-            {
-                title: "Code article",
-                dataIndex: "code_article",
-                key: "code_article",
-                fixed: "left",
-                width: 150,
-                ...getColumnSearchProps('code_article', searchText, setSearchText, setSearchedColumn, searchInput),
-                render: (value) => <Text strong>{value}</Text>,
-            },
-            {
-                title: "Description",
-                dataIndex: "description",
-                key: "description",
-                ellipsis: true,
-                width: 250,
-                ...getColumnSearchProps('description', searchText, setSearchText, setSearchedColumn, searchInput),
-            },
-            {
-                title: "Qté EAM",
-                dataIndex: "qte_eam",
-                key: "qte_eam",
-                align: "right",
-                width: 120,
-                sorter: (a,b) => a.qte_eam - b.qte_eam,
-            },
-            {
-                title: "Qté FMP",
-                dataIndex: "qte_fmp",
-                key: "qte_fmp",
-                align: "right",
-                width: 120,
-                sorter: (a,b) => a.qte_fmp - b.qte_fmp,
-            },
-            {
-                title: "Écart",
-                dataIndex: "ecart",
-                key: "ecart",
-                align: "center",
-                width: 120,
-                sorter: (a,b) => a.ecart - b.ecart,
-                render: (value) => {
-                    if (value === 0) return <Tag color="green">0</Tag>;
-                    if (value > 0) return <Tag color="blue">+{value}</Tag>;
-                    return <Tag color="red">{value}</Tag>;
-                },
-            },
-            {
-                title: "SMR",
-                dataIndex: "type_smr",
-                key: "type_smr",
-                align: "center",
-                width: 120,
-                render: (value) =>
-                    value === "SANS_SMR"
-                        ? <Tag color="orange">Sans SMR</Tag>
-                        : <Tag color="green">Avec SMR</Tag>,
-                filters: [
-                    { text: "Avec SMR", value: "AVEC_SMR" },
-                    { text: "Sans SMR", value: "SANS_SMR" },
-                ],
-                onFilter: (value, record) => record.type_smr === value,
-            },
-        ];
+  return useMemo(() => {
+    const allColumns = [
+      {
+        title: "Code article",
+        dataIndex: "code_article",
+        key: "code_article",
+        fixed: "left",
+        width: 150,
+        ...getColumnSearchProps('code_article', searchText, setSearchText, null, searchInput),
+        render: (value) => <Text strong>{value}</Text>,
+      },
+      {
+        title: "Description",
+        dataIndex: "description",
+        key: "description",
+        ellipsis: true,
+        width: 250,
+        ...getColumnSearchProps('description', searchText, setSearchText, null, searchInput),
+      },
+      {
+        title: "Qté EAM",
+        dataIndex: "qte_eam",
+        key: "qte_eam",
+        align: "right",
+        width: 120,
+        sorter: (a,b) => a.qte_eam - b.qte_eam,
+      },
+      {
+        title: "Qté FMP",
+        dataIndex: "qte_fmp",
+        key: "qte_fmp",
+        align: "right",
+        width: 120,
+        sorter: (a,b) => a.qte_fmp - b.qte_fmp,
+      },
+      {
+        title: "Écart",
+        dataIndex: "ecart",
+        key: "ecart",
+        align: "center",
+        width: 120,
+        sorter: (a,b) => a.ecart - b.ecart,
+        render: (value) => {
+          if (value === 0) return <Tag color="green">0</Tag>;
+          if (value > 0) return <Tag color="blue">+{value}</Tag>;
+          return <Tag color="red">{value}</Tag>;
+        },
+      },
+      {
+        title: "SMR",
+        dataIndex: "type_smr",
+        key: "type_smr",
+        align: "center",
+        width: 120,
+        render: (value) =>
+          value === "SANS_SMR" ? <Tag color="orange">Sans SMR</Tag> : <Tag color="green">Avec SMR</Tag>,
+      },
+    ];
 
-        const visibleColumns = columnsVisibility
-            ? allColumns.filter(col => columnsVisibility[col.key] !== false)
-            : allColumns;
+    // Appliquer la visibilité des colonnes
+    const visibleColumns = allColumns.filter(col => columnsVisibility[col.key] !== false);
 
-        return visibleColumns;
-
-    }, [pagination, columnsVisibility, searchText]);
+    return visibleColumns;
+  }, [columnsVisibility, searchText]);
 };
