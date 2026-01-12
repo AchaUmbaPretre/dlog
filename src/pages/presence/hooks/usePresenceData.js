bfba6a8fd885938735daf48598cc1f34dc89e0b7
AchaUmbaPretre <achandambi@gmail.com>
2026-01-12 15:55:33 +0100

    commit

---
+++ src/pages/presence/hooks/usePresenceData.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import config from '../config';
+
+const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
+
+export const getPresence = async () => {
+  return axios.get(`${DOMAIN}/api/presence`);
+};
+
+export const getPresenceById = async () => {
+  return axios.get(`${DOMAIN}/api/presence/presenceById`);
+};
+
+export const postPresence = async (data) => {
+  return axios.post(`${DOMAIN}/api/presence`, data);
+};
+
+
+//Congé
+export const getConge = async () => {
+  return axios.get(`${DOMAIN}/api/presence/conge`);
+};
+
+export const postConge = async (data) => {
+  return axios.post(`${DOMAIN}/api/presence/conge`, data);
+};
