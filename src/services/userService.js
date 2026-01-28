8b03529d22fa7cdf97acd2e7d2512c7752cc9c85
AchaUmbaPretre <achandambi@gmail.com>
2026-01-28 09:52:02 +0100

    commit

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,17 +1,16 @@
-// src/services/authService.js
 import axios from 'axios';
 import config from '../config';
+import { userRequest } from '../requestMethods';
 
 const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
 
 export const getUser = async (user) => {
-    return axios.get(`${DOMAIN}/api/user`);
-  };
+  return axios.get(`${DOMAIN}/api/user`);
+};
 
 export const getUserOne = async (id) => {
-    return axios.get(`${DOMAIN}/api/user/one?id_user=${id}`);
-  };
-
+  return axios.get(`${DOMAIN}/api/user/one?id_user=${id}`);
+};
 
 export const postUser = async (data) => {
   return axios.post(`${DOMAIN}/api/user`, data);
@@ -27,8 +26,8 @@ export const putUserOne = async (id, data) => {
 
 //Signature
 export const getSignature = async (id) => {
-    return axios.get(`${DOMAIN}/api/user/signature?userId=${id}`);
-  };
+  return axios.get(`${DOMAIN}/api/user/signature?userId=${id}`);
+};
 
 export const postSignature = async (data) => {
   return axios.post(`${DOMAIN}/api/user/signature`, data);
