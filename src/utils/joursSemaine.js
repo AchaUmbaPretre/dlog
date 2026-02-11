7e429b9557a1b6e98b5e3652e5eaa1cbfb408a2a
AchaUmbaPretre <achandambi@gmail.com>
2026-02-11 11:08:04 +0100

    commit

--- src/utils/joursSemaine.js
+++ src/utils/joursSemaine.js
@@ -1,9 +1,9 @@
 export const joursSemaine = [
-  "LUNDI",
-  "MARDI",
-  "MERCREDI",
-  "JEUDI",
-  "VENDREDI",
-  "SAMEDI",
-  "DIMANCHE",
+  { label: "Lundi", value: "lundi" },
+  { label: "Mardi", value: "mardi" },
+  { label: "Mercredi", value: "mercredi" },
+  { label: "Jeudi", value: "jeudi" },
+  { label: "Vendredi", value: "vendredi" },
+  { label: "Samedi", value: "samedi" },
+  { label: "Dimanche", value: "dimanche" },
 ];
