d88ed5f711b8ef49e707dfea1a044d1952155c28
AchaUmbaPretre <achandambi@gmail.com>
2026-02-24 13:06:55 +0100

    commit

---
+++ src/pages/presence/dashboardPresence/controleDashboard/ControleDashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './controleDashboard.scss'
+
+const ControleDashboard = () => {
+  return (
+    <div>ControleDashboard</div>
+  )
+}
+
+export default ControleDashboard
