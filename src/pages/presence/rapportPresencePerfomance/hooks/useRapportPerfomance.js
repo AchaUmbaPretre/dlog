0d47b90bd2a70fc2f78922c74ef3957d295ec988
AchaUmbaPretre <achandambi@gmail.com>
2026-02-25 11:20:02 +0100

    commit

--- src/pages/presence/rapportPresencePerfomance/hooks/useRapportPerfomance.js
+++ src/pages/presence/rapportPresencePerfomance/hooks/useRapportPerfomance.js
@@ -1,10 +1,16 @@
-import { useCallback, useEffect } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { notifyWarning } from "../../../../utils/notifyWarning";
+import { getPresenceDashboardPerformance } from "../../../../services/presenceService";
 
 export const useRapportPerfomance = () => {
+    const [ data, setData ] = useState([]);
+    const [ loading, setLoading ] = useState(false)
+
     const load = useCallback(async() => {
         try {
-            
+            const res = await getPresenceDashboardPerformance();
+            setData(res.data)
+
         } catch (error) {
             console.error("Erreur chargement planning", error);
             notifyWarning('Erreur chargement Performance', 'Chargement depuis le cache local…');      
@@ -15,4 +21,9 @@ export const useRapportPerfomance = () => {
         load()
     }, [])
 
+    return {
+        data,
+        loading,
+        reload: load
+    }
 }
